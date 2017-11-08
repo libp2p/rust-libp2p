32f3ba356be213558cf72b483118aec2eaaec7a8
Pierre Krieger <pierre.krieger1708@gmail.com>
2017-11-08 13:13:06 +0100

    Add Swarm::dial

--- libp2p-swarm/src/lib.rs
+++ libp2p-swarm/src/lib.rs
@@ -10,7 +10,7 @@ use std::io::Error as IoError;
 use std::mem;
 use std::sync::{Arc, Mutex};
 use bytes::Bytes;
-use futures::{Future, Sink, Stream};
+use futures::{IntoFuture, Future, Sink, Stream};
 use futures::future::join_all;
 use multiaddr::Multiaddr;
 use tokio_io::{AsyncRead, AsyncWrite};
@@ -31,6 +31,7 @@ pub use self::transport::Transport;
 pub struct Swarm<T> {       // TODO: provide a default for T
     transports: Mutex<T>,
     listeners: Mutex<Vec<Box<Future<Item = (), Error = IoError>>>>,
+    dialers: Mutex<Vec<Box<Future<Item = (), Error = IoError>>>>,
     security_protocols: Vec<(Bytes, fn(&Bytes,&Bytes)->bool, Arc<AbstractConnUpgr>)>,
 }
 
@@ -65,18 +66,59 @@ impl<T> Swarm<T> {
         Swarm {
             transports: Mutex::new(transports),
             listeners: Mutex::new(Vec::new()),      // TODO: with_capacity with the number of multiaddrs
+            dialers: Mutex::new(Vec::new()),
             security_protocols: security_protocols,
         }
     }
 
     // TODO: get more thoughts into this API
     pub fn run(self) -> Box<Future<Item = (), Error = IoError>> {
-        Box::new(join_all(mem::replace(&mut *self.listeners.lock().unwrap(), Vec::new())).map(|_| ()))
+        let listeners = join_all(mem::replace(&mut *self.listeners.lock().unwrap(), Vec::new())).map(|_| ());
+        let dialers = join_all(mem::replace(&mut *self.dialers.lock().unwrap(), Vec::new())).map(|_| ());
+        Box::new(listeners.join(dialers).map(|_| ()))
+    }
+}
+
+impl<T> Swarm<T> where T: Transport, T::Dial: 'static, T::RawConn: 'static {        // TODO: 'static?
+    /// Tries to dial on all added multiaddresses of the peer info.
+    // TODO: produce a future that is signalled when listening starts, like the JS API
+    #[inline]
+    pub fn dial(&self, addr: Multiaddr) {
+        let security_protocols = self.security_protocols.clone().into_iter();
+
+        let multiplex_protocols = vec![
+            (Bytes::from("/multiplex/1.0.0"), <Bytes as PartialEq>::eq, 0)
+        ].into_iter();
+
+        let finished = self.transports.lock().unwrap()
+            .dial(addr).unwrap()       // TODO: don't panic
+            .into_future()
+            .map_err(|_| panic!())       // TODO:
+            // Negociate the security protocol.
+            .and_then(move |connection| {
+                multistream_select::dialer_select_proto(connection, security_protocols.clone())
+                    .map_err(|err| panic!("{:?}", err))      // TODO:
+            })
+            .and_then(|(chosen_sec, connection)| {
+                Ok(AbstractConnUpgr::upgrade(&*chosen_sec, Box::new(connection) as Box<_>))
+            })
+            // Negociate the multiplex.
+            .and_then(move |connection| {
+                multistream_select::dialer_select_proto(connection, multiplex_protocols.clone())
+                    .map_err(|err| panic!("{:?}", err))      // TODO:
+            })
+            .and_then(|(chosen_multiplex, connection)| {
+                Ok(())
+            })
+            .map_err(|_| panic!());     // TODO:
+
+        self.dialers.lock().unwrap().push(Box::new(finished));
     }
 }
 
 impl<T> Swarm<T> where T: Transport, T::Listener: 'static, T::RawConn: 'static {        // TODO: 'static?
-    /// Start listening on all added multiaddresses of the peer info.
+    /// Start listening on the multiaddr.
+    // TODO: should take multiple multiaddrs (through a peerinfo).
     // TODO: produce a future that is signalled when listening starts, like the JS API
     #[inline]
     pub fn listen(&self, addr: Multiaddr) {
