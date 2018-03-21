6fb14f241c0114f83673ec5f4148182c58706cc9
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-03-21 10:51:20 +0100

    Rename multiplex to mplex

--- swarm/tests/multiplex.rs
+++ swarm/tests/multiplex.rs
@@ -22,7 +22,7 @@ extern crate bytes;
 extern crate futures;
 extern crate libp2p_swarm;
 extern crate libp2p_tcp_transport;
-extern crate libp2p_multiplex as multiplex;
+extern crate libp2p_mplex as multiplex;
 extern crate tokio_core;
 extern crate tokio_io;
 
