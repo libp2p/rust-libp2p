377063355650bd0c4198b868d7fec7d68920ae78
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-05-22 17:10:48 +0200

    Remove dial_custom_handler

--- core/src/swarm.rs
+++ core/src/swarm.rs
@@ -134,37 +134,6 @@ where
         }
     }
 
-    /// Asks the swarm to dial the node with the given multiaddress. The connection is then
-    /// upgraded using the `upgrade`, and the output is then passed to `and_then`.
-    ///
-    /// Contrary to `dial_to_handler`, the output of the upgrade is not given to the handler that
-    /// was passed at initialization.
-    // TODO: consider returning a future so that errors can be processed?
-    pub fn dial_custom_handler<Du, Df, Dfu>(
-        &self,
-        multiaddr: Multiaddr,
-        transport: Du,
-        and_then: Df,
-    ) -> Result<(), Multiaddr>
-    where
-        Du: Transport + 'static,                               // TODO: 'static :-/
-        Df: FnOnce(Du::Output, Multiaddr) -> Dfu + 'static,    // TODO: 'static :-/
-        Dfu: IntoFuture<Item = (), Error = IoError> + 'static, // TODO: 'static :-/
-    {
-        trace!("Swarm dialing {} with custom handler", multiaddr);
-
-        match transport.dial(multiaddr) {
-            Ok(dial) => {
-                let dial = Box::new(dial.and_then(|(d, m)| and_then(d, m))) as Box<_>;
-                // Ignoring errors if the receiver has been closed, because in that situation
-                // nothing is going to be processed anyway.
-                let _ = self.new_toprocess.unbounded_send(dial);
-                Ok(())
-            }
-            Err((_, multiaddr)) => Err(multiaddr),
-        }
-    }
-
     /// Adds a multiaddr to listen on. All the incoming connections will use the `upgrade` that
     /// was passed to `swarm`.
     pub fn listen_on(&self, multiaddr: Multiaddr) -> Result<Multiaddr, Multiaddr> {
