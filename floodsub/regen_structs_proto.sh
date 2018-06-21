02bf7604d023b665b3936e706c2685ba417d50c6
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-06-21 16:53:23 +0200

    Fix protobuf again (#269)

--- floodsub/regen_structs_proto.sh
+++ floodsub/regen_structs_proto.sh
@@ -5,7 +5,7 @@
 docker run --rm -v `pwd`:/usr/code:z -w /usr/code rust /bin/bash -c " \
     apt-get update; \
     apt-get install -y protobuf-compiler; \
-    cargo install protobuf; \
+    cargo install --version 1 protobuf; \
     protoc --rust_out . rpc.proto"
 
 mv -f rpc.rs ./src/rpc_proto.rs
