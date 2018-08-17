c7b0b3f62250dbc878046642d746e1732e504d21
James Ray <16969914+jamesray1@users.noreply.github.com>
2018-08-17 19:31:26 +1000

    gossipsub: Cargo.toml, unlicense, rpc_proto.rs from Go impl, regen_structs_proto from floodsublib with floosub import and overlay parameters

---
+++ gossipsub/src/constants.rs
@@ -0,0 +1,6 @@
+git push --set-upstream origin gossipsub-v-6// Overlay parameters
+pub const TARGET_MESH_DEGREE: u32 = 6;
+pub const LOW_WM_MESH_DEGREE: u32 = 4;      // low water mark for mesh degree
+pub const HIGH_WM_MESH_DEGREE: u32 = 12;    // high water mark for mesh degree
+pub const GOSSIP_HIST_LEN u32 = 6;          // length of gossip history
+pub const MSG_HIST_LEN: u32 = 120;          // length of total message history
