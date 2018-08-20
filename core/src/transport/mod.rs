d863c4f349dbf1fa35d348c11c4a83877b89b811
Benjamin Kampmann <ben@gnunicorn.org>
2018-08-20 17:44:06 +0200

    remove cruft

--- core/src/transport/mod.rs
+++ core/src/transport/mod.rs
@@ -55,11 +55,6 @@ pub use self::muxed::MuxedTransport;
 pub use self::upgrade::UpgradedNode;
 
 
-// This is a new error type that you've created. It represents the ways a
-// toolchain could be invalid.
-//
-// The custom derive for Fail derives an impl of both Fail and Display.
-// We don't do any other magic like creating new types.
 #[derive(Debug, Fail)]
 pub enum TransportError {
     #[fail(display= "Error parsing Address {}: {}", addr, err)]
