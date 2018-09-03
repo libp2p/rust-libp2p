9a0bd815372c9ccd8ec7837787914694ea2ad9f2
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-09-03 13:59:05 +0200

    Fix concern

--- examples/kademlia.rs
+++ examples/kademlia.rs
@@ -18,6 +18,8 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
+// Libp2p's code unfortunately produces very large types. Rust's default length limit for type
+// names is not large enough, therefore we need this attribute.
 #![type_length_limit = "2097152"]
 
 extern crate bigint;
