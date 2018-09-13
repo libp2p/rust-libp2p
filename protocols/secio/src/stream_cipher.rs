4cc64525c090b8bedad4209a2051c9637da1d3a4
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-09-13 14:08:00 +0200

    Revert "Revert "remove rustc-serialize (#359) (#386)" (#466)"
    This reverts commit 0a3d4cdfade1656d72ca99751e94e179bdaec221.

--- protocols/secio/src/stream_cipher.rs
+++ protocols/secio/src/stream_cipher.rs
@@ -19,14 +19,19 @@
 // DEALINGS IN THE SOFTWARE.
 
 use super::codec::StreamCipher;
-use crypto::{aessafe, blockmodes::CtrModeX8, symmetriccipher::SynchronousStreamCipher};
+use aes_ctr::stream_cipher::generic_array::GenericArray;
+use aes_ctr::stream_cipher::{NewFixStreamCipher, LoopError, StreamCipherCore};
+use aes_ctr::{Aes128Ctr, Aes256Ctr};
+use ctr::Ctr128;
+use twofish::Twofish;
 
 /// Possible encryption ciphers.
 #[derive(Clone, Copy, Debug)]
 pub enum Cipher {
     Aes128,
     Aes256,
-    Null
+    TwofishCtr,
+    Null,
 }
 
 impl Cipher {
@@ -35,7 +40,8 @@ impl Cipher {
         match *self {
             Cipher::Aes128 => 16,
             Cipher::Aes256 => 32,
-            Cipher::Null => 0
+            Cipher::TwofishCtr => 32,
+            Cipher::Null => 0,
         }
     }
 
@@ -43,7 +49,7 @@ impl Cipher {
     #[inline]
     pub fn iv_size(&self) -> usize {
         match self {
-            Cipher::Aes128 | Cipher::Aes256 => 16,
+            Cipher::Aes128 | Cipher::Aes256 | Cipher::TwofishCtr => 16,
             Cipher::Null => 0
         }
     }
@@ -54,25 +60,123 @@ impl Cipher {
 #[derive(Clone, Copy, Debug)]
 pub struct NullCipher;
 
-impl SynchronousStreamCipher for NullCipher {
-    fn process(&mut self, input: &[u8], output: &mut [u8]) {
-        output.copy_from_slice(input)
+impl StreamCipherCore for NullCipher {
+    fn try_apply_keystream(&mut self, _data: &mut [u8]) -> Result<(), LoopError> {
+        Ok(())
     }
 }
 
 /// Returns your stream cipher depending on `Cipher`.
-#[inline]
-pub fn ctr(c: Cipher, key: &[u8], iv: &[u8]) -> StreamCipher {
-    match c {
-        Cipher::Aes128 => {
-            let aes_dec = aessafe::AesSafe128EncryptorX8::new(key);
-            Box::new(CtrModeX8::new(aes_dec, iv))
-        },
-        Cipher::Aes256 => {
-            let aes_dec = aessafe::AesSafe256EncryptorX8::new(key);
-            Box::new(CtrModeX8::new(aes_dec, iv))
-        },
-        Cipher::Null => Box::new(NullCipher)
+#[cfg(not(all(feature = "aes-all", any(target_arch = "x86_64", target_arch = "x86"))))]
+pub fn ctr(key_size: Cipher, key: &[u8], iv: &[u8]) -> StreamCipher {
+    ctr_int(key_size, key, iv)
+}
+ 
+/// Returns your stream cipher depending on `Cipher`.
+#[cfg(all(feature = "aes-all", any(target_arch = "x86_64", target_arch = "x86")))]
+pub fn ctr(key_size: Cipher, key: &[u8], iv: &[u8]) -> StreamCipher {
+    if *aes_alt::AES_NI {
+        aes_alt::ctr_alt(key_size, key, iv)
+    } else {
+        ctr_int(key_size, key, iv)
     }
 }
 
+
+#[cfg(all(feature = "aes-all", any(target_arch = "x86_64", target_arch = "x86")))]
+mod aes_alt {
+    extern crate aesni;
+    use ::codec::StreamCipher;
+    use ctr::Ctr128;
+    use self::aesni::{Aes128, Aes256};
+    use ctr::stream_cipher::NewFixStreamCipher;
+    use ctr::stream_cipher::generic_array::GenericArray;
+    use twofish::Twofish;
+    use super::{Cipher, NullCipher};
+
+    lazy_static! {
+        pub static ref AES_NI: bool = is_x86_feature_detected!("aes")
+            && is_x86_feature_detected!("sse2")
+            && is_x86_feature_detected!("sse3");
+
+   }
+
+    /// AES-128 in CTR mode
+    pub type Aes128Ctr = Ctr128<Aes128>;
+    /// AES-256 in CTR mode
+    pub type Aes256Ctr = Ctr128<Aes256>;
+    /// Returns alternate stream cipher if target functionalities does not allow standard one.
+    /// Eg : aes without sse
+    pub fn ctr_alt(key_size: Cipher, key: &[u8], iv: &[u8]) -> StreamCipher {
+        match key_size {
+            Cipher::Aes128 => Box::new(Aes128Ctr::new(
+                GenericArray::from_slice(key),
+                GenericArray::from_slice(iv),
+            )),
+            Cipher::Aes256 => Box::new(Aes256Ctr::new(
+                GenericArray::from_slice(key),
+                GenericArray::from_slice(iv),
+            )),
+            Cipher::TwofishCtr => Box::new(Ctr128::<Twofish>::new(
+                GenericArray::from_slice(key),
+                GenericArray::from_slice(iv),
+            )),
+            Cipher::Null => Box::new(NullCipher),
+        }
+    }
+
+}
+
+#[inline]
+fn ctr_int(key_size: Cipher, key: &[u8], iv: &[u8]) -> StreamCipher {
+    match key_size {
+        Cipher::Aes128 => Box::new(Aes128Ctr::new(
+            GenericArray::from_slice(key),
+            GenericArray::from_slice(iv),
+        )),
+        Cipher::Aes256 => Box::new(Aes256Ctr::new(
+            GenericArray::from_slice(key),
+            GenericArray::from_slice(iv),
+        )),
+        Cipher::TwofishCtr => Box::new(Ctr128::<Twofish>::new(
+            GenericArray::from_slice(key),
+            GenericArray::from_slice(iv),
+        )),
+        Cipher::Null => Box::new(NullCipher),
+    }
+}
+
+#[cfg(all(
+        feature = "aes-all", 
+        any(target_arch = "x86_64", target_arch = "x86"),
+))]
+#[cfg(test)]
+mod tests {
+    use super::{Cipher, ctr};
+
+    #[test]
+    fn assert_non_native_run() {
+        // this test is for asserting aes unsuported opcode does not break on old cpu
+        let key = [0;16];
+        let iv = [0;16];
+     
+        let mut aes = ctr(Cipher::Aes128, &key, &iv);
+        let mut content = [0;16];
+        assert!(aes
+                .try_apply_keystream(&mut content).is_ok());
+         
+    }
+}
+
+// aesni compile check for aes-all (aes-all import aesni through aes_ctr only if those checks pass)
+#[cfg(all(
+    feature = "aes-all", 
+    any(target_arch = "x86_64", target_arch = "x86"),
+    any(target_feature = "aes", target_feature = "ssse3"),
+))]
+compile_error!(
+    "aes-all must be compile without aes and sse3 flags : currently \
+    is_x86_feature_detected macro will not detect feature correctly otherwhise. \
+    RUSTFLAGS=\"-C target-feature=+aes,+ssse3\" enviromental variable. \
+    For x86 target arch additionally enable sse2 target feature."
+);
