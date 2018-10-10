ddorgan/libp2p-rs

Add some testing infrastructure

core/src/nodes/swarm.rs
@@ -384,6 +384,13 @@ where
             .count()
     }
 
+    /// Returns true if the swarm is connected or attempting to connect to any node.
+    pub fn has_connections_or_pending(&self) -> bool {
+        !self.reach_attempts.out_reach_attempts.is_empty() ||
+            !self.reach_attempts.other_reach_attempts.is_empty() ||
+            !self.reach_attempts.connected_endpoints.is_empty()
+    }
+
     /// Sends an event to all nodes.
     #[inline]
     pub fn broadcast_event(&mut self, event: &TInEvent)

libp2p/rust-libp2p

core/src/nodes/swarm.rs
@@ -377,6 +377,13 @@ where
             .count()
     }
 
+    /// Returns true if the swarm is connected or attempting to connect to any node.
+    pub fn has_connections_or_pending(&self) -> bool {
+        !self.reach_attempts.out_reach_attempts.is_empty() ||
+            !self.reach_attempts.other_reach_attempts.is_empty() ||
+            !self.reach_attempts.connected_multiaddresses.is_empty()
+    }
+
     /// Sends an event to all nodes.
     #[inline]
     pub fn broadcast_event(&mut self, event: &TInEvent)