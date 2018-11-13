b3e43b1caed82e150cad7a34b30003a97e764ece
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-11-13 19:46:56 +0100

    Remove the Send + 'static requirements for floodsub

--- protocols/floodsub/src/handler.rs
+++ protocols/floodsub/src/handler.rs
@@ -99,7 +99,7 @@ where
 
 impl<TSubstream> ProtocolsHandler for FloodsubHandler<TSubstream>
 where
-    TSubstream: AsyncRead + AsyncWrite + 'static,
+    TSubstream: AsyncRead + AsyncWrite,
 {
     type InEvent = FloodsubRpc;
     type OutEvent = FloodsubRpc;
