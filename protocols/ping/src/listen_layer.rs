ac0e4f704b09082cc027309074164af3503cb67e
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-11-16 11:34:53 +0100

    Remove Send/Sync/'static reqs from ping

--- protocols/ping/src/listen_layer.rs
+++ protocols/ping/src/listen_layer.rs
@@ -50,7 +50,7 @@ impl<TSubstream> Default for PingListenBehaviour<TSubstream> {
 
 impl<TSubstream> NetworkBehavior for PingListenBehaviour<TSubstream>
 where
-    TSubstream: AsyncRead + AsyncWrite + Send + Sync + 'static,
+    TSubstream: AsyncRead + AsyncWrite,
 {
     type ProtocolsHandler = PingListenHandler<TSubstream>;
     type OutEvent = Void;
