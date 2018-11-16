fd8b2c23ccf470b5d1fb7b4dc93487f0043b63cd
David <dvdplm@gmail.com>
2018-11-16 10:48:21 +0100

    Apply suggestions from code review
    Co-Authored-By: tomaka <pierre.krieger1708@gmail.com>

--- core/src/muxing.rs
+++ core/src/muxing.rs
@@ -20,9 +20,9 @@
 
 //! Muxing is the process of splitting a connection into multiple substreams.
 //!
-//! The main item of this module is the `StreamMuxer` traits. An implementation of `StreamMuxer`
-//! has ownership of a connection, and lets you open and close substreams, and read/write data
-//! on opened substreams.
+//! The main item of this module is the `StreamMuxer` trait. An implementation of `StreamMuxer`
+//! has ownership of a connection, lets you open and close substreams, and read/write data
+//! on open substreams.
 //!
 //! > **Note**: You normally don't need to use the methods of the `StreamMuxer` directly, as this
 //! >           is managed by the library's internals.
@@ -34,7 +34,7 @@
 //! In the context of libp2p, each substream can use a different protocol. Contrary to opening a
 //! connection, opening a substream is almost free in terms of resources. This means that you
 //! shouldn't hesitate to rapidly open and close substreams, and to design protocols that don't
-//! require maintaining a long-lived channel of communcation.
+//! require maintaining long-lived channels of communication.
 //!
 //! > **Example**: The Kademlia protocol opens a new substream for each request it wants to
 //! >              perform. Multiple requests can be performed simultaneously by opening multiple
