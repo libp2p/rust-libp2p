0bb333561174dd9886f0d900c58448b1aa17cf90
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-11-21 16:39:45 +0100

    More flags fix

--- misc/mdns/src/dns.rs
+++ misc/mdns/src/dns.rs
@@ -156,8 +156,8 @@ pub fn build_service_discovery_response(id: u16, ttl: Duration) -> Vec<u8> {
     let mut out = Vec::with_capacity(69);
 
     append_u16(&mut out, id);
-    // Flags ; 0x80 for an answer.
-    append_u16(&mut out, 0x8000);
+    // Flags ; 0x84 for an answer.
+    append_u16(&mut out, 0x8400);
     // Number of questions, answers, authorities, additionals.
     append_u16(&mut out, 0x0);
     append_u16(&mut out, 0x1);
