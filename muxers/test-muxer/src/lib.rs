05ecc39603b803de55d355964e6fca54a30d9e8d
David Palm <dvdplm@gmail.com>
2018-12-06 11:50:45 +0100

    port over both tests from two_peers

--- muxers/test-muxer/src/lib.rs
+++ muxers/test-muxer/src/lib.rs
@@ -20,7 +20,6 @@
 
 extern crate libp2p_core;
 extern crate tokio;
-extern crate tokio_io;
 extern crate bytes;
 extern crate futures;
 #[macro_use]
@@ -37,8 +36,10 @@ use libp2p_core::{
     upgrade::{InboundUpgrade, OutboundUpgrade, UpgradeInfo}
 };
 
-use tokio::runtime::Runtime;
-use tokio_io::codec::length_delimited::Framed;
+use tokio::{
+    codec::length_delimited::Builder,
+    runtime::current_thread::Runtime
+};
 use futures::prelude::*;
 use std::thread;
 use std::sync::mpsc;
@@ -61,6 +62,91 @@ where
     <O as StreamMuxer>::OutboundSubstream: Send + Sync,
 {
     env_logger::init();
+    client_to_server_inbound(config.clone());
+    client_to_server_outbound(config.clone());
+}
+
+fn client_to_server_inbound<U, O, E>(config: U)
+    where
+        U: OutboundUpgrade<TcpTransStream, Output = O, Error = E> + Send + Clone + 'static,
+        U: InboundUpgrade<TcpTransStream, Output = O, Error = E>,
+        U: Debug, // needed for `unwrap()`
+        <U as UpgradeInfo>::NamesIter: Send,
+        <U as UpgradeInfo>::UpgradeId: Send,
+        <U as InboundUpgrade<TcpTransStream>>::Future: Send,
+        <U as OutboundUpgrade<TcpTransStream>>::Future: Send,
+        E: std::error::Error + Send + Sync + 'static,
+        O: StreamMuxer + Send + Sync + 'static ,
+        <O as StreamMuxer>::Substream: Send + Sync,
+        <O as StreamMuxer>::Substream: Send + Sync,
+        <O as StreamMuxer>::OutboundSubstream: Send + Sync,
+{
+    // Simulate a client sending a message to a server through a multiplex upgrade.
+    let (tx, rx) = mpsc::channel();
+
+    let listener_config = config.clone();
+    let bg_thread = thread::spawn(move || {
+        let transport =
+            TcpConfig::new().with_upgrade(listener_config);
+
+        let (listener, addr) = transport
+            .listen_on("/ip4/127.0.0.1/tcp/0".parse().unwrap())
+            .unwrap();
+        tx.send(addr).unwrap();
+
+        let future = listener
+            .into_future()
+            .map_err(|(err, _)| err)
+            .and_then(|(client, _)| client.unwrap().0)
+            .and_then(|client| muxing::inbound_from_ref_and_wrap(Arc::new(client)))
+            .map(|client| Builder::new().new_read(client.unwrap()))
+            .and_then(|client| {
+                client
+                    .into_future()
+                    .map_err(|(err, _)| err)
+                    .map(|(msg, _)| msg)
+            })
+            .and_then(|msg| {
+                let msg = msg.unwrap();
+                assert_eq!(msg, "hello world");
+                Ok(())
+            });
+
+        let mut rt = Runtime::new().unwrap();
+        let _ = rt.block_on(future).unwrap();
+    });
+
+    let transport = TcpConfig::new().with_upgrade(config);
+
+    let future = transport
+        .dial(rx.recv().unwrap())
+        .unwrap()
+        .and_then(|client| muxing::outbound_from_ref_and_wrap(Arc::new(client)))
+        .map(|server| Builder::new().new_write(server.unwrap()))
+        .and_then(|server| server.send("hello world".into()))
+        .map(|_| ());
+
+    let mut rt = Runtime::new().unwrap();
+    let _ = rt.block_on(future).unwrap();
+    bg_thread.join().unwrap();
+
+}
+
+fn client_to_server_outbound<U, O, E>(config: U)
+where
+    U: OutboundUpgrade<TcpTransStream, Output = O, Error = E> + Send + Clone + 'static,
+    U: InboundUpgrade<TcpTransStream, Output = O, Error = E>,
+    U: Debug, // needed for `unwrap()`
+    <U as UpgradeInfo>::NamesIter: Send,
+    <U as UpgradeInfo>::UpgradeId: Send,
+    <U as InboundUpgrade<TcpTransStream>>::Future: Send,
+    <U as OutboundUpgrade<TcpTransStream>>::Future: Send,
+    E: std::error::Error + Send + Sync + 'static,
+    O: StreamMuxer + Send + Sync + 'static ,
+    <O as StreamMuxer>::Substream: Send + Sync,
+    <O as StreamMuxer>::Substream: Send + Sync,
+    <O as StreamMuxer>::OutboundSubstream: Send + Sync,
+{
     let (tx, rx) = mpsc::channel();
     let listener_config = config.clone();
     let thr = thread::spawn(move || {
@@ -85,11 +171,13 @@ where
                 // a substream we can set up the framed codec on.
                 muxing::outbound_from_ref_and_wrap(Arc::new(muxer))
             })
-            .map(|substream: Option<SubstreamRef<Arc<O>>>| Framed::<_, bytes::BytesMut>::new(substream.unwrap()))
+            .map(|substream| Builder::new().new_read(substream.unwrap() ))
+//            .map(|substream: Option<SubstreamRef<Arc<O>>>| Framed::<_, bytes::BytesMut>::new(substream.unwrap()))
             // substream is a `Framed<SubstreamRef<Arc<O>>>`, and `Framed` is a `Stream`
-            .and_then(|substream: Framed<SubstreamRef<Arc<O>>>| {
+//            .and_then(|substream: Framed<SubstreamRef<Arc<O>>>| {
+            .and_then(|substream| {
                 substream.into_future()
-                    .map_err(|(e, _): (std::io::Error, Framed<SubstreamRef<Arc<O>>>)| e)
+                    .map_err(|(e, _)| e)
                     .map(|(msg, _)| msg)
             })
             .and_then(|msg| {
@@ -107,8 +195,8 @@ where
        .and_then(|muxer: O| {
            muxing::inbound_from_ref_and_wrap(Arc::new(muxer))
        })
-       .map(|server: Option<SubstreamRef<Arc<O>>>| Framed::<_, bytes::BytesMut>::new(server.unwrap()))
-       .and_then(|server: Framed<SubstreamRef<Arc<O>>>| {
+       .map(|server: Option<SubstreamRef<Arc<O>>>| Builder::new().new_write(server.unwrap()))
+       .and_then(|server| {
            server.send("hello".into())
        });
 
