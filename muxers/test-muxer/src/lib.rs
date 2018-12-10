7241dfb59eab532e6d810ab5db77df5cb2385f85
David Palm <dvdplm@gmail.com>
2018-12-10 12:09:47 +0100

    Don't duplicate code

--- muxers/test-muxer/src/lib.rs
+++ muxers/test-muxer/src/lib.rs
@@ -17,24 +17,144 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
+extern crate libp2p_tcp_transport as tcp;
 
 use log::{info, trace};
 use env_logger;
-use tcp::{TcpConfig, TcpTransStream};
+use tcp::{TcpConfig, TcpTransStream, TcpListenStream};
 use libp2p_core::{
     Transport,
     StreamMuxer,
+    transport::{self, upgrade::ListenerStream, upgrade::ListenerUpgradeFuture},
     muxing::{self, SubstreamRef},
-    upgrade::{InboundUpgrade, OutboundUpgrade, UpgradeInfo}
+    upgrade::{InboundUpgrade, OutboundUpgrade, UpgradeInfo},
+    Multiaddr,
 };
 use tokio::{
     codec::length_delimited::Builder,
     runtime::current_thread::Runtime
 };
 use futures::prelude::*;
+use futures::future::Either;
 use std::{thread, sync::{mpsc, Arc}, fmt::Debug};
 
+mod helpers {
+    use super::*;
+    use tokio::codec::{FramedWrite, FramedRead, Framed, LengthDelimitedCodec};
+
+//    pub(crate) fn inbound_fut<T>(
+//        transport: T,
+//        addr: Multiaddr,
+//    ) -> impl Future<
+//        Item = FramedWrite<muxing::SubstreamRef<Arc<T::Output>>, LengthDelimitedCodec>,
+//        Error = std::io::Error,
+//    >
+//        where
+//            T: Transport + Debug,
+//            T::Output: StreamMuxer + Send + Sync + 'static,
+//    {
+//        transport
+//            .dial(addr)
+//            .unwrap()
+//            .and_then(|server| muxing::inbound_from_ref_and_wrap(Arc::new(server)))
+//            .map(|server| Builder::new().new_write(server.unwrap()))
+//    }
+//
+//    pub(crate) fn outbound_fut<T>(
+//        transport: T,
+//        addr: Multiaddr,
+//    ) -> impl Future<
+//        Item = FramedWrite<muxing::SubstreamRef<Arc<T::Output>>, LengthDelimitedCodec>,
+//        Error = std::io::Error,
+//    >
+//        where
+//            T: Transport + Debug,
+//            T::Output: StreamMuxer + Send + Sync + 'static,
+//    {
+//        transport
+//            .dial(addr)
+//            .unwrap()
+//            .and_then(|server| muxing::outbound_from_ref_and_wrap(Arc::new(server)))
+//            .map(|server| Builder::new().new_write(server.unwrap()))
+//    }
+
+    pub(crate) fn dial_fut<T>(
+        transport: T,
+        addr: Multiaddr,
+        inbound: bool,
+    ) -> impl Future<
+        Item = Framed<muxing::SubstreamRef<Arc<T::Output>>, LengthDelimitedCodec>,
+        Error = std::io::Error,
+    >
+        where
+            T: Transport + Debug,
+            T::Output: StreamMuxer + Send + Sync + 'static,
+    {
+        transport
+            .dial(addr)
+            .unwrap()
+            .and_then(move |server| {
+                match inbound {
+                    true => Either::A(muxing::inbound_from_ref_and_wrap(Arc::new(server))),
+                    false => Either::B(muxing::outbound_from_ref_and_wrap(Arc::new(server))),
+                }
+            })
+            .map(|server| Builder::new().new_framed(server.unwrap()))
+    }
+
+    pub(crate) fn framed_listener_fut<U, O, E>(
+        listener: ListenerStream<TcpListenStream, U>,
+        inbound: bool
+    )
+        -> impl Future<
+            Item = Framed<muxing::SubstreamRef<Arc<O>>, LengthDelimitedCodec>,
+            Error = std::io::Error,
+        >
+    where
+        U: InboundUpgrade<TcpTransStream, Output = O, Error = E> + Debug + Send + Clone + 'static,
+        <U as UpgradeInfo>::NamesIter: Send,
+        <U as UpgradeInfo>::UpgradeId: Send,
+        <U as InboundUpgrade<TcpTransStream>>::Future: Send,
+        E: std::error::Error + Send + Sync + 'static,
+        O: StreamMuxer + Send + Sync + 'static,
+        <O as StreamMuxer>::Substream: Send + Sync,
+    {
+        listener
+            .into_future()
+            .map_err(|(e, _)| e)
+            .and_then(|(client, _)| client.unwrap().0)
+            .and_then(move |client| {
+                match inbound {
+                    true => Either::A(muxing::inbound_from_ref_and_wrap(Arc::new(client))),
+                    false => Either::B(muxing::outbound_from_ref_and_wrap(Arc::new(client))),
+                }
+            })
+            .map(|client| Builder::new().new_framed(client.unwrap()))
+    }
+
+}
+
 pub fn test_muxer<U, O, E>(config: U)
+where
+    U: OutboundUpgrade<TcpTransStream, Output = O, Error = E> + Send + Clone + Debug + 'static,
+    U: InboundUpgrade<TcpTransStream, Output = O, Error = E>,
+    <U as UpgradeInfo>::NamesIter: Send,
+    <U as UpgradeInfo>::UpgradeId: Send,
+    <U as InboundUpgrade<TcpTransStream>>::Future: Send,
+    <U as OutboundUpgrade<TcpTransStream>>::Future: Send,
+    E: std::error::Error + Send + Sync + 'static,
+    O: StreamMuxer + Send + Sync + 'static ,
+    <O as StreamMuxer>::Substream: Send + Sync,
+    <O as StreamMuxer>::OutboundSubstream: Send + Sync,
+{
+    env_logger::init();
+    info!("Calling inbound\n");
+    client_to_server_inbound(config.clone());
+    info!("Calling outbound\n");
+    client_to_server_outbound(config.clone());
+}
+
+fn client_to_server_inbound<U, O, E>(config: U)
 where
     U: OutboundUpgrade<TcpTransStream, Output = O, Error = E> + Send + Clone + 'static,
     U: InboundUpgrade<TcpTransStream, Output = O, Error = E>,
@@ -46,76 +166,59 @@ where
     E: std::error::Error + Send + Sync + 'static,
     O: StreamMuxer + Send + Sync + 'static ,
     <O as StreamMuxer>::Substream: Send + Sync,
-    <O as StreamMuxer>::Substream: Send + Sync,
     <O as StreamMuxer>::OutboundSubstream: Send + Sync,
 {
-    env_logger::init();
-    client_to_server_inbound(config.clone());
-    client_to_server_outbound(config.clone());
-}
-
-fn client_to_server_inbound<U, O, E>(config: U)
-    where
-        U: OutboundUpgrade<TcpTransStream, Output = O, Error = E> + Send + Clone + 'static,
-        U: InboundUpgrade<TcpTransStream, Output = O, Error = E>,
-        U: Debug, // needed for `unwrap()`
-        <U as UpgradeInfo>::NamesIter: Send,
-        <U as UpgradeInfo>::UpgradeId: Send,
-        <U as InboundUpgrade<TcpTransStream>>::Future: Send,
-        <U as OutboundUpgrade<TcpTransStream>>::Future: Send,
-        E: std::error::Error + Send + Sync + 'static,
-        O: StreamMuxer + Send + Sync + 'static ,
-        <O as StreamMuxer>::Substream: Send + Sync,
-        <O as StreamMuxer>::Substream: Send + Sync,
-        <O as StreamMuxer>::OutboundSubstream: Send + Sync,
-{
-    // Simulate a client sending a message to a server through a multiplex upgrade.
+    // Simulate a client sending a message to a server.
     let (tx, rx) = mpsc::channel();
 
     let listener_config = config.clone();
     let bg_thread = thread::spawn(move || {
-        let transport =
-            TcpConfig::new().with_upgrade(listener_config);
+        let transport = TcpConfig::new().with_upgrade(listener_config);
 
         let (listener, addr) = transport
             .listen_on("/ip4/127.0.0.1/tcp/0".parse().unwrap())
             .unwrap();
+
+        // Send our address to the connecting side so they know where to find us
         tx.send(addr).unwrap();
 
-        let future = listener
-            .into_future()
-            .map_err(|(err, _)| err)
-            .and_then(|(client, _)| client.unwrap().0)
-            .and_then(|client| muxing::inbound_from_ref_and_wrap(Arc::new(client)))
-            .map(|client| Builder::new().new_read(client.unwrap()))
-            .and_then(|client| {
-                client
-                    .into_future()
-                    .map_err(|(err, _)| err)
-                    .map(|(msg, _)| msg)
-            })
-            .and_then(|msg| {
-                let msg = msg.unwrap();
-                assert_eq!(msg, "hello world");
-                Ok(())
-            });
+//        let fut1 = listener
+//            .into_future()
+//            .map_err(|(e, _)| e)
+//            .and_then(|(client, _)| client.unwrap().0)
+//            .and_then(|client| muxing::inbound_from_ref_and_wrap(Arc::new(client)))
+//            .map(|client| Builder::new().new_read(client.unwrap()));
+        let fut1 = helpers::framed_listener_fut(listener, true);
+//        let fut1 = helpers::framed_listener_fut(listener, &muxing::inbound_from_ref_and_wrap);
+        let future = fut1
+            .and_then(|stream| stream.take(2).collect())
+            .and_then(|msgs| Ok(assert_eq!(msgs, vec!["hello", "world"])));
 
-        let mut rt = Runtime::new().unwrap();
-        let _ = rt.block_on(future).unwrap();
+
+//        let future = listener
+//            .into_future()
+//            .map_err(|(err, _)| err)
+//            .and_then(|(client, _)| client.unwrap().0)
+//            .and_then(|client| muxing::inbound_from_ref_and_wrap(Arc::new(client)))
+//            .map(|client| Builder::new().new_read(client.unwrap()))
+//            .and_then(|stream| stream.take(2).collect())
+//            .and_then(|msgs| Ok(assert_eq!(msgs, vec!["hello", "world"])));
+
+        Runtime::new().unwrap().block_on(future).unwrap();
     });
 
     let transport = TcpConfig::new().with_upgrade(config);
-
-    let future = transport
-        .dial(rx.recv().unwrap())
-        .unwrap()
-        .and_then(|client| muxing::outbound_from_ref_and_wrap(Arc::new(client)))
-        .map(|server| Builder::new().new_write(server.unwrap()))
-        .and_then(|server| server.send("hello world".into()))
-        .map(|_| ());
-
-    let mut rt = Runtime::new().unwrap();
-    let _ = rt.block_on(future).unwrap();
+    let addr = rx.recv().expect("address is valid");
+    Runtime::new().unwrap().block_on(
+        helpers::dial_fut(transport, addr, false)
+            .and_then(|subs| subs.send("hello".into()))
+            .and_then(|subs| subs.send("world".into()))
+    ).unwrap();
+//    Runtime::new().unwrap().block_on(
+//        helpers::outbound_fut(transport, addr)
+//            .and_then(|subs| subs.send("hello".into()))
+//            .and_then(|subs| subs.send("world".into()))
+//    ).unwrap();
     bg_thread.join().unwrap();
 
 }
@@ -132,7 +235,6 @@ where
     E: std::error::Error + Send + Sync + 'static,
     O: StreamMuxer + Send + Sync + 'static ,
     <O as StreamMuxer>::Substream: Send + Sync,
-    <O as StreamMuxer>::Substream: Send + Sync,
     <O as StreamMuxer>::OutboundSubstream: Send + Sync,
 {
     let (tx, rx) = mpsc::channel();
@@ -144,7 +246,6 @@ where
             .unwrap();
         tx.send(addr).unwrap();
 
-        let mut rt_listener = Runtime::new().unwrap();
         let fut = listener
             // convert stream to future yielding a tuple ("next stream item", "rest of stream")
             .into_future()
@@ -160,32 +261,31 @@ where
                 muxing::outbound_from_ref_and_wrap(Arc::new(muxer))
             })
             .map(|substream| Builder::new().new_read(substream.unwrap() ))
-            .and_then(|substream| {
-                substream.into_future()
-                    .map_err(|(e, _)| e)
-                    .map(|(msg, _)| msg)
-            })
-            .and_then(|msg| {
-                trace!("message received: {:?}", msg);
-                Ok(())
-            });
+            .and_then(|stream| stream.take(2).collect())
+            .and_then(|msgs| Ok(assert_eq!(msgs, vec!["hello", "world"])));
+        let mut rt_listener = Runtime::new().unwrap();
         rt_listener.block_on(fut).unwrap();
+
+//        let fut1 = helpers::framed_listener_fut(listener, false);
+//        let fut = fut1
+//            .and_then(|stream| stream.take(2).collect())
+//            .and_then(|msgs| Ok(assert_eq!(msgs, vec!["hello", "world"])));
+//        Runtime::new().unwrap().block_on(fut).unwrap();
     });
 
     let addr = rx.recv().unwrap();
     info!("Listening on {:?}", addr);
 
     let transport = TcpConfig::new().with_upgrade(config);
-    let fut = transport.dial(addr).unwrap()
-       .and_then(|muxer: O| {
-           muxing::inbound_from_ref_and_wrap(Arc::new(muxer))
-       })
-       .map(|server: Option<SubstreamRef<Arc<O>>>| Builder::new().new_write(server.unwrap()))
-       .and_then(|server| {
-           server.send("hello".into())
-       });
-
-    let mut rt_dialer = Runtime::new().unwrap();
-    let _ = rt_dialer.block_on(fut).unwrap();
+    Runtime::new().unwrap().block_on(
+        helpers::dial_fut(transport, addr, true)
+            .and_then(|subs| subs.send("hello".into()))
+            .and_then(|subs| subs.send("world".into()))
+    ).unwrap();
+//    Runtime::new().unwrap().block_on(
+//        helpers::inbound_fut(transport, addr)
+//            .and_then(|subs| subs.send("hello".into()))
+//            .and_then(|subs| subs.send("world".into()))
+//    ).unwrap();
     thr.join().unwrap();
 }
