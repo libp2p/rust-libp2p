ceb88d09bfdd856f1d8bb15e7303cd146ad33a3d
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-01-11 11:31:32 +0100

    Fix wrong data size

--- protocols/secio/benches/bench.rs
+++ protocols/secio/benches/bench.rs
@@ -104,15 +104,20 @@ fn raw_tcp_connect_and_send_data(bench: &mut Bencher, data: &[u8]) {
 }
 
 fn criterion_benchmarks(bench: &mut Criterion) {
-    let data = (0 .. 1024).map(|_| rand::random::<u8>()).collect::<Vec<_>>();
     bench.bench_function("secio_connect_and_send_hello", move |b| secio_and_send_data(b, b"hello world"));
     bench.bench_function("raw_tcp_connect_and_send_hello", move |b| raw_tcp_connect_and_send_data(b, b"hello world"));
+
+    let data = (0 .. 1024).map(|_| rand::random::<u8>()).collect::<Vec<_>>();
     bench.bench_function("secio_connect_and_send_one_kb", { let data = data.clone(); move |b| secio_and_send_data(b, &data) });
-    bench.bench_function("raw_tcp_connect_and_send_one_kb", { let data = data.clone(); move |b| raw_tcp_connect_and_send_data(b, &data) });
+    bench.bench_function("raw_tcp_connect_and_send_one_kb", move |b| raw_tcp_connect_and_send_data(b, &data));
+
+    let data = (0 .. 1024 * 1024).map(|_| rand::random::<u8>()).collect::<Vec<_>>();
     bench.bench_function("secio_connect_and_send_one_mb", { let data = data.clone(); move |b| secio_and_send_data(b, &data) });
-    bench.bench_function("raw_tcp_connect_and_send_one_mb", { let data = data.clone(); move |b| raw_tcp_connect_and_send_data(b, &data) });
+    bench.bench_function("raw_tcp_connect_and_send_one_mb", move |b| raw_tcp_connect_and_send_data(b, &data));
+
+    let data = (0 .. 2 * 1024 * 1024).map(|_| rand::random::<u8>()).collect::<Vec<_>>();
     bench.bench_function("secio_connect_and_send_two_mb", { let data = data.clone(); move |b| secio_and_send_data(b, &data) });
-    bench.bench_function("raw_tcp_connect_and_send_two_mb", { let data = data.clone(); move |b| raw_tcp_connect_and_send_data(b, &data) });
+    bench.bench_function("raw_tcp_connect_and_send_two_mb", move |b| raw_tcp_connect_and_send_data(b, &data));
 }
 
 criterion_group!(benches, criterion_benchmarks);
