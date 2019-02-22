6e1e0532f6bed5163f46700933e084a95ef0ad95
James Ray <16969914+jamesray1@users.noreply.github.com>
2019-02-22 16:12:59 +1100

    Add config

--- protocols/gossipsub/src/protocol.rs
+++ protocols/gossipsub/src/protocol.rs
@@ -1,3 +1,5 @@
+
+use constants::*;
 use message::{GMessage, GossipsubSubscription,
     GossipsubSubscriptionAction, GossipsubRpc, ControlMessage};
 use {TopicHash};
@@ -14,13 +16,180 @@ use unsigned_varint::codec;
 
 /// Implementation of `ConnectionUpgrade` for the Gossipsub protocol.
 #[derive(Debug, Clone)]
-pub struct GossipsubConfig {}
+pub struct GossipsubConfig {
+    /// Overlay network parameters.
+    /// Number of heartbeats to keep in the `mcache`.
+    pub history_length: usize,
+    /// Number of past heartbeats to gossip about.
+    pub history_gossip: usize,
+
+    /// Target number of peers for the mesh network (D in the spec).
+    pub mesh_n: usize,
+    /// Minimum number of peers in mesh network before adding more (D_lo
+    /// in the spec).
+    pub mesh_n_low: usize,
+    /// Maximum number of peers in mesh network before removing some
+    /// (D_high in the spec).
+    pub mesh_n_high: usize,
+
+    /// Number of peers to emit gossip to during a heartbeat (D_lazy in
+    /// the spec).
+    pub gossip_lazy: usize,
+
+    /// Initial delay in each heartbeat.
+    pub heartbeat_initial_delay: Duration,
+    /// Time between each heartbeat.
+    pub heartbeat_interval: Duration,
+    /// Time to live for fanout peers.
+    pub fanout_ttl: Duration,
+}
 
 impl GossipsubConfig {
     /// Builds a new `GossipsubConfig`.
     #[inline]
     pub fn new() -> GossipsubConfig {
         GossipsubConfig {}
+    }
+}
+
+impl Default for GossipsubConfig {
+    fn default() -> GossipsubConfig {
+        GossipsubConfig {
+            history_length: GOSSIP_HIST_LEN,
+            history_gossip: HISTORY_GOSSIP,
+            mesh_n: TARGET_MESH_DEGREE,
+            mesh_n_low: LOW_WM_MESH_DEGREE,
+            mesh_n_high: HIGH_WM_MESH_DEGREE,
+            gossip_lazy: TARGET_MESH_DEGREE, // default to mesh_n
+            heartbeat_initial_delay: Duration::from_millis(
+                HEARTBEAT_INITIAL_DELAY),
+            heartbeat_interval: Duration::from_secs(HEARTBEAT_INTERVAL),
+            fanout_ttl: Duration::from_secs(FANOUT_TTL),
+        }
+    }
+}
+
+pub struct GossipsubConfigBuilder {
+    history_length: usize,
+    /// Number of past heartbeats to gossip about.
+    history_gossip: usize,
+
+    /// Target number of peers for the mesh network (D in the spec).
+    mesh_n: usize,
+    /// Minimum number of peers in mesh network before adding more (D_lo in the spec).
+    mesh_n_low: usize,
+    /// Maximum number of peers in mesh network before removing some (D_high in the spec).
+    mesh_n_high: usize,
+
+    /// Number of peers to emit gossip to during a heartbeat (D_lazy in the spec).
+    gossip_lazy: usize,
+
+    /// Initial delay in each heartbeat.
+    heartbeat_initial_delay: Duration,
+    /// Time between each heartbeat.
+    heartbeat_interval: Duration,
+    /// Time to live for fanout peers.
+    fanout_ttl: Duration,
+}
+
+impl Default for GossipsubConfigBuilder {
+    fn default() -> GossipsubConfigBuilder {
+        GossipsubConfigBuilder {
+            history_length: GOSSIP_HIST_LEN,
+            history_gossip: HISTORY_GOSSIP,
+            mesh_n: TARGET_MESH_DEGREE,
+            mesh_n_low: LOW_WM_MESH_DEGREE,
+            mesh_n_high: HIGH_WM_MESH_DEGREE,
+            gossip_lazy: TARGET_MESH_DEGREE, // default to mesh_n
+            heartbeat_initial_delay: Duration::from_millis(
+                HEARTBEAT_INITIAL_DELAY),
+            heartbeat_interval: Duration::from_secs(HEARTBEAT_INTERVAL),
+            fanout_ttl: Duration::from_secs(FANOUT_TTL),
+        }
+    }
+}
+
+impl GossipsubConfigBuilder {
+    // set default values
+    pub fn new() -> GossipsubConfigBuilder {
+        GossipsubConfigBuilder::default()
+    }
+
+    pub fn history_length(&mut self, history_length: usize) -> &mut Self {
+        assert!(
+            history_length >= self.history_gossip,
+            "The history_length, {history_length}, must be greater than or equal to the history_gossip length, {self.history_gossip}"
+        );
+        self.history_length = history_length;
+        self
+    }
+
+    pub fn history_gossip(&mut self, history_gossip: usize) -> &mut Self {
+        assert!(
+            self.history_length >= history_gossip,
+            "The history_length, {history_length}, must be greater than or equal to the history_gossip length, {self.history_gossip}"
+        );
+        self.history_gossip = history_gossip;
+        self
+    }
+
+    pub fn mesh_n(&mut self, mesh_n: usize) -> &mut Self {
+        assert!(
+            self.mesh_n_low <= mesh_n && mesh_n <= self.mesh_n_high,
+            "The following equality doesn't hold mesh_n_low <= mesh_n <= mesh_n_high. The mesh_n_low is {self.mesh_n_low}, mesh_n is {mesh_n} and mesh_n_high is {self.mesh_n_high}."
+        );
+        self.mesh_n = mesh_n;
+        self
+    }
+
+    pub fn mesh_n_low(&mut self, mesh_n_low: usize) -> &mut Self {
+        assert!(
+            mesh_n_low <= self.mesh_n && self.mesh_n <= self.mesh_n_high,
+            "The following equality doesn't hold mesh_n_low <= mesh_n <= mesh_n_high. The mesh_n_low is {mesh_n_low}, mesh_n is {self.mesh_n} and mesh_n_high is {self.mesh_n_high}."
+        );
+        self.mesh_ n_low = mesh_n_low;
+        self
+    }
+
+    pub fn mesh_n_high(&mut self, mesh_n_high: usize) -> &mut Self {
+        assert!(
+            self.mesh_n_low <= self.mesh_n && self.mesh_n <= mesh_n_high,
+            "The following equality doesn't hold mesh_n_low <= mesh_n <= mesh_n_high"
+        );
+        self.mesh_n_high = mesh_n_high;
+        self
+    }
+
+    pub fn gossip_lazy(&mut self, gossip_lazy: usize) -> &mut Self {
+        self.gossip_lazy = gossip_lazy;
+        self
+    }
+
+    pub fn heartbeat_initial_delay(&mut self, heartbeat_initial_delay: Duration) -> &mut Self {
+        self.heartbeat_initial_delay = heartbeat_initial_delay;
+        self
+    }
+    pub fn heartbeat_interval(&mut self, heartbeat_interval: Duration) -> &mut Self {
+        self.heartbeat_interval = heartbeat_interval;
+        self
+    }
+    pub fn fanout_ttl(&mut self, fanout_ttl: Duration) -> &mut Self {
+        self.fanout_ttl = fanout_ttl;
+        self
+    }
+
+    pub fn build(&self) -> GossipsubConfig {
+        GossipsubConfig {
+            history_length: self.history_length,
+            history_gossip: self.history_gossip,
+            mesh_n: self.mesh_n,
+            mesh_n_low: self.mesh_n_low,
+            mesh_n_high: self.mesh_n_high,
+            gossip_lazy: self.gossip_lazy,
+            heartbeat_initial_delay: self.heartbeat_initial_delay,
+            heartbeat_interval: self.heartbeat_interval,
+            fanout_ttl: self.fanout_ttl,
+        }
     }
 }
 
