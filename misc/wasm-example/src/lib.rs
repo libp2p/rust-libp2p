4f77981ef92c73d012cb58ca7078213b5539b3a2
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-03-28 01:02:41 +0100

    More work

--- misc/wasm-example/src/lib.rs
+++ misc/wasm-example/src/lib.rs
@@ -23,12 +23,14 @@ use libp2p::{
     core::PublicKey, core::Transport, core::upgrade::InboundUpgradeExt, core::upgrade::OutboundUpgradeExt,
     secio,
 };
+use log::debug;
 use rand_core::{RngCore, SeedableRng};
 use std::time::Duration;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn start(multiaddr_constructor: JsValue, transport: JsValue) -> JsValue {
+    // Let's setup a panic hook so that panics are printed on stderr.
     std::panic::set_hook(Box::new(|panic_info| {
         web_sys::console::log_1(&JsValue::from_str("Panic!"));
         if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
@@ -44,13 +46,14 @@ pub fn start(multiaddr_constructor: JsValue, transport: JsValue) -> JsValue {
         }
     }));
 
+    // Setup logging.
+    console_log::init_with_level(log::Level::Debug).unwrap();
+
     // Create a random key for ourselves.
     let local_key = secio::SecioKeyPair::secp256k1_generated().unwrap();
-    web_sys::console::log_1(&JsValue::from_str(&format!("Pubkey = {:?}", local_key.to_public_key())));
     let local_peer_id = local_key.to_peer_id();
-    web_sys::console::log_1(&JsValue::from_str(&format!("Peerid = {}", local_peer_id.to_base58())));
 
-    let transport = libp2p::js_transport::JsTransport::new(transport, multiaddr_constructor)
+    let transport = libp2p::js_transport::JsTransport::new(transport, multiaddr_constructor).unwrap()
         .with_upgrade(libp2p::secio::SecioConfig::new(local_key))
         .and_then(move |out, endpoint| {
             let peer_id = out.remote_key.into_peer_id();
@@ -67,28 +70,17 @@ pub fn start(multiaddr_constructor: JsValue, transport: JsValue) -> JsValue {
 
     // Create a swarm to manage peers and events.
     let mut swarm = {
-        // Create a Kademlia behaviour.
-        // Note that normally the Kademlia process starts by performing lots of request in order
-        // to insert our local node in the DHT. However here we use `without_init` because this
-        // example is very ephemeral and we don't want to pollute the DHT. In a real world
-        // application, you want to use `new` instead.
         let mut behaviour = libp2p::kad::Kademlia::without_init(local_peer_id.clone());
-        behaviour.add_address(&"QmXmgU6RCiVGAyAuaP3QYSYMQfxHqz48ia5ZNvtPN2VCud".parse().unwrap(), "/ip4/206.189.241.107/tcp/30433/ws".parse().unwrap());
-        /*behaviour.add_address(&"QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd".parse().unwrap(), "/dns4/ams-1.bootstrap.libp2p.io/tcp/443/wss".parse().unwrap());
-        behaviour.add_address(&"QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3".parse().unwrap(), "/dns4/lon-1.bootstrap.libp2p.io/tcp/443/wss".parse().unwrap());
-        behaviour.add_address(&"QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM".parse().unwrap(), "/dns4/sfo-3.bootstrap.libp2p.io/tcp/443/wss".parse().unwrap());
-        behaviour.add_address(&"QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu".parse().unwrap(), "/dns4/sgp-1.bootstrap.libp2p.io/tcp/443/wss".parse().unwrap());
-        behaviour.add_address(&"QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm".parse().unwrap(), "/dns4/nyc-1.bootstrap.libp2p.io/tcp/443/wss".parse().unwrap());
-        behaviour.add_address(&"QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64".parse().unwrap(), "/dns4/nyc-2.bootstrap.libp2p.io/tcp/443/wss".parse().unwrap());
-        behaviour.add_address(&"QmZMxNdpMkewiVZLMRxaNxUeZpDUb34pWjZ1kZvsd16Zic".parse().unwrap(), "/dns4/node0.preload.ipfs.io/tcp/443/wss".parse().unwrap());
-        behaviour.add_address(&"Qmbut9Ywz9YEDrz8ySBSgWyJk41Uvm2QJPhwDJzJyGFsD6".parse().unwrap(), "/dns4/node1.preload.ipfs.io/tcp/443/wss".parse().unwrap());*/
+        behaviour.add_address(
+            &"QmSiUKk9rA6NxXar2D9AjvEgS9BbujLGbbdm3ZtFXSqLV5".parse().unwrap(),
+            "/ip4/127.0.0.1/tcp/30333/ws".parse().unwrap()
+        );
         libp2p::core::Swarm::new(transport, behaviour, local_peer_id)
     };
 
     // Order Kademlia to search for a peer.
     let mut csprng = rand_chacha::ChaChaRng::from_seed([0; 32]);
     let to_search = PublicKey::Secp256k1((0..32).map(|_| csprng.next_u32() as u8).collect()).into_peer_id();
-    //println!("Searching for {:?}", to_search);
     swarm.find_node(to_search);
 
     // Kick it off!
@@ -97,6 +89,7 @@ pub fn start(multiaddr_constructor: JsValue, transport: JsValue) -> JsValue {
             match swarm.poll().expect("Error while polling swarm") {
                 Async::Ready(Some(ev @ libp2p::kad::KademliaOut::FindNodeResult { .. })) => {
                     let out = format!("Result: {:#?}", ev);
+                    debug!("finished result");
                     return Ok(Async::Ready(JsValue::from_str(&out)));
                 },
                 Async::Ready(Some(_)) => (),
