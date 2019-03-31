7a237bbc7423acff2c78e286492ae34f5aa140ca
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-03-31 16:44:04 -0300

    Minor work

--- protocols/brahms/src/sampler.rs
+++ protocols/brahms/src/sampler.rs
@@ -20,25 +20,26 @@
 
 //! Contains the `Sampler` struct. See the doc-comment of this struct for more info.
 
-use rand;
+use rand::distributions::{self, Distribution as _};
 use smallvec::SmallVec;
 use sha2::digest::{generic_array::GenericArray, BlockInput};
 use sha2::{Digest, Sha256};
 
-/// A sampler accepts as input a stream of elements, and can be sampled to obtain a stream of
-/// elements. The output stream contains an approximately uniform distribution of the input,
-/// ignoring redundancy.
+/// A sampler is a data structure where you can insert elements, and that can be sampled to obtain
+/// a stream of elements. The output stream contains an approximately uniform distribution of the
+/// content, ignoring redundancy.
 ///
 /// For example, if you insert the value `A` one million times, the value `B` one thousand times,
-/// and the value `C` once, then sampling has approximately 33% chances to produce `A`, 33% chances
-/// to produce `B`, and 33% chances to produce `C`.
+/// and the value `C` once, then sampling has approximately 33% chance to produce `A`, 33% chance
+/// to produce `B`, and 33% chance to produce `C`.
 pub struct Sampler<TValue, TDigest: Digest + BlockInput = Sha256> {
     /// All the individual samplers. Each sampler holds one value.
     samplers: SmallVec<[IndivSampler<TValue, TDigest>; 32]>,
 }
 
 struct IndivSampler<TValue, TDigest: Digest + BlockInput> {
-    /// Initial state of the hasher to set before we hash a value. Never modified.
+    /// Initial state of the hasher to set before we hash a value. Modified only when we reset
+    /// the sampler.
     /// This ensure that the value has a different hash value in each sampler.
     xor: GenericArray<u8, TDigest::BlockSize>,
     /// The value currently stored in the sampler.
@@ -53,14 +54,14 @@ where
 {
     /// Initializes a new `Sampler` with the given number of samplers.
     ///
-    /// The higher the number of samplers, the more precise the randomness is, but the more CPU
+    /// The higher the number of samplers, the more precise the distribution is, but the more CPU
     /// will be consumed when inserting. Between two inserts, the sampler can only ever produce
     /// `num_samplers` different values.
     pub fn with_len(num_samplers: u32) -> Self {
-        // Note that `rand` doesn't support our array type, so we first put 0s everywhere, then we
-        // fill the arrays with random values.
         let mut samplers: SmallVec<[_; 32]> = (0..num_samplers as usize)
             .map(|_| IndivSampler {
+                // Note that `rand` doesn't support our array type, so we first put 0s everywhere,
+                // then we fill the arrays with random values below.
                 xor: Default::default(),
                 cur_value: None,
                 cur_value_hash: None,
@@ -76,7 +77,7 @@ where
         Sampler { samplers }
     }
 
-    /// Inserts a value into the sampler.
+    /// Inserts a value into the sampler. This is an expensive operation.
     pub fn insert(&mut self, value: TValue)
     where
         TValue: AsRef<[u8]> + Clone,
@@ -106,13 +107,10 @@ where
     /// This not only removes the value, but also reinitializes the samplers that had this value
     /// so that if you insert the same value again there is a high chance that it will not be
     /// inserted.
-    pub fn invalidate<TOther>(&mut self, value: &TOther)
-    where
-        TValue: PartialEq<TOther>,
-    {
+    pub fn invalidate(&mut self, value: &impl PartialEq<TValue>) {
         for sampler in self.samplers.iter_mut() {
             match sampler.cur_value {
-                Some(ref v) if *v == *value => (),
+                Some(ref v) if *value == *v => (),
                 _ => continue,
             }
 
@@ -124,11 +122,11 @@ where
         }
     }
 
-    /// Gets a random element from the sampler. Returns `None` if the sampler is empty
+    /// Gets a random element from the sampler. Returns `None` if the `Sampler` is empty
     /// (ie. `insert()` has never been called).
-    #[inline]
     pub fn sample(&self) -> Option<&TValue> {
-        let mut iter = rand::random::<usize>() % self.samplers.len();
+        let mut iter = distributions::Uniform::new(0, self.samplers.len())
+            .sample(&mut rand::thread_rng());
         let end_num = iter.wrapping_sub(1) % self.samplers.len();
 
         loop {
