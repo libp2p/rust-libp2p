db03ec2648501b9570500051098eb2c2e8d33ff9
Fedor Sakharov <fedor.sakharov@gmail.com>
2019-06-01 12:56:13 +0300

    Fix the test topology and checking the results

--- protocols/kad/src/behaviour/test.rs
+++ protocols/kad/src/behaviour/test.rs
@@ -20,7 +20,13 @@
 
 #![cfg(test)]
 
-use crate::{GetValueResult, Kademlia, KademliaOut, kbucket::{self, Distance}};
+use crate::{
+    GetValueResult,
+    Kademlia,
+    KademliaOut,
+    kbucket::{self, Distance},
+    record::{RecordStore},
+};
 use futures::{future, prelude::*};
 use libp2p_core::{
     PeerId,
@@ -36,9 +42,9 @@ use libp2p_core::{
 use libp2p_secio::SecioConfig;
 use libp2p_yamux as yamux;
 use rand::random;
-use std::{collections::HashSet, io, u64};
+use std::{collections::HashSet, iter::FromIterator, io, u64};
 use tokio::runtime::Runtime;
-use multihash::{Hash, Multihash};
+use multihash::Hash;
 
 type TestSwarm = Swarm<
     Boxed<(PeerId, StreamMuxerBox), io::Error>,
@@ -267,127 +273,90 @@ fn get_value_not_found() {
 
 #[test]
 fn put_value() {
+    // Build a test that checks if PUT_VALUE gets correctly propagated in
+    // a nontrivial topology:
+    //                [31]
+    //               /    \
+    //             [29]  [30]
+    //            /|\      /|\
+    //         [0]..[14] [15]..[28]
+    //
+    // Nodes [29] and [30] have less than kbuckets::MAX_NODES_PER_BUCKET
+    // peers to avoid the situation when the bucket may be overflowed and
+    // some of the connections are dropped from the routing table
     let (port_base, mut swarms) = build_nodes(32);
 
     let swarm_ids: Vec<_> = swarms.iter()
         .map(|swarm| Swarm::local_peer_id(&swarm).clone()).collect();
 
-    // Test routing configuration:
-    // swarms[31] is connected to swarms[30]
-    // swarms[30] is connected to swarms[..29]
-
     // Remove two last swarms temoporarily to make borrow checker happy
     let mut swarm_1 = swarms.pop().unwrap();
     let mut swarm_2 = swarms.pop().unwrap();
+    let mut swarm_3 = swarms.pop().unwrap();
 
-    // Connect swarms[30] to each swarm in swarms[..29]
-    for (i, (_, peer)) in &mut swarms.iter_mut().zip(swarm_ids.clone()).enumerate() {
+    // Connect swarm[30] to each swarm in swarms[..15]
+    for (i, (_, peer)) in &mut swarms.iter_mut().take(15).zip(swarm_ids.iter()).enumerate() {
         swarm_2.add_address(&peer, Protocol::Memory(port_base + i as u64).into());
+    }
+
+    // Connect swarm[29] to each swarm in swarms[..15]
+    for (i, (_, peer)) in &mut swarms.iter_mut().skip(15).take(14).zip(swarm_ids.iter().skip(15)).enumerate() {
+        swarm_3.add_address(&peer, Protocol::Memory(port_base + (i + 15) as u64).into());
     }
 
     // Connect swarms[31] to swarms[30]
     swarm_1.add_address(&swarm_ids[30], Protocol::Memory(port_base + 30 as u64).into());
+    swarm_1.add_address(&swarm_ids[29], Protocol::Memory(port_base + 29 as u64).into());
 
+    swarms.push(swarm_3);
     swarms.push(swarm_2);
     swarms.push(swarm_1);
 
     let target_key = multihash::encode(Hash::SHA2256, &vec![1,2,3]).unwrap();
 
+    let mut sorted_peer_ids: Vec<_> = swarm_ids
+        .iter()
+        .map(|id| (id.clone(), kbucket::Key::from(id.clone()).distance(&kbucket::Key::from(target_key.clone()))))
+        .collect();
+
+    sorted_peer_ids.sort_by(|(_, d1), (_, d2)| d1.cmp(d2));
+
+    let closest: HashSet<PeerId> = HashSet::from_iter(sorted_peer_ids.into_iter().map(|(id, _)| id));
+
     swarms[31].put_value(target_key.clone(), vec![4,5,6]).unwrap();
 
-    struct TestContext {
-        target_key: Multihash,
-        swarm_ids: Vec<PeerId>,
-        swarms: Vec<TestSwarm>,
-        have_key: HashSet<PeerId>,
-        have_no_key: HashSet<PeerId>
-    }
-
-    impl Future for TestContext {
-        type Item = ();
-        type Error = ();
-
-        fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-            loop {
-                let res = self.poll_swarms().unwrap();
-                match res {
-                    Async::Ready((i, e)) => {
-                        match e {
-                            KademliaOut::PutValueResult{ .. } => {
-                                for swarm in self.swarms.iter_mut().take(31) {
-                                    swarm.get_value(&self.target_key);
-                                }
-                            }
-                            KademliaOut::GetValueResult(res) => {
-                                match res {
-                                    GetValueResult::Found{ .. } => {
-                                        self.have_key.insert(self.swarm_ids[i].clone());
-                                    }
-                                    GetValueResult::NotFound{ .. } => {
-                                        self.have_no_key.insert(self.swarm_ids[i].clone());
-                                    }
-                                }
-
-                                if self.have_key.len() + self.have_no_key.len() == 31 {
-                                    assert_eq!(self.have_key.len(), kbucket::MAX_NODES_PER_BUCKET + 1);
-                                    assert_eq!(self.have_no_key.len(), 30 - kbucket::MAX_NODES_PER_BUCKET);
-                                    let key = kbucket::Key::from(self.target_key.clone());
-                                    let mut has_distances: Vec<_> = self.have_key.iter()
-                                        .map(|k| kbucket::Key::from(k.clone()))
-                                        .map(|k| key.distance(&k))
-                                        .collect();
-
-                                    let mut has_no_distances: Vec<_> = self.have_no_key.iter()
-                                        .map(|k| kbucket::Key::from(k.clone()))
-                                        .map(|k| key.distance(&k))
-                                        .collect();
-
-                                    has_distances.sort();
-                                    has_no_distances.sort();
-                                    assert!(has_no_distances.first() >= has_distances.last());
-
-                                    return Ok(Async::Ready(()));
-                                }
-                            }
-                            _ => ()
-                        }
-                    }
-                    Async::NotReady => break,
-                }
-            }
-
-            Ok(Async::NotReady)
-        }
-    }
-
-    impl TestContext {
-        fn poll_swarms(&mut self) -> Poll<(usize, KademliaOut), ()> {
-            for (i, swarm) in self.swarms.iter_mut().enumerate() {
+    Runtime::new().unwrap().block_on(
+        future::poll_fn(move || -> Result<_, io::Error> {
+            let mut check_results = false;
+            for swarm in &mut swarms {
                 loop {
                     match swarm.poll().unwrap() {
-                        Async::Ready(Some(event)) => {
-                            return Ok(Async::Ready((i, event)))
-                        },
+                        Async::Ready(Some(KademliaOut::PutValueResult{ .. })) => {
+                            check_results = true;
+                        }
                         Async::Ready(_) => (),
                         Async::NotReady => break,
                     }
                 }
-            };
+            }
+
+            if check_results {
+                let mut have: HashSet<_> = Default::default();
+
+                for (i, swarm) in swarms.iter().take(31).enumerate() {
+                    if swarm.records.get(&target_key).is_some() {
+                        have.insert(swarm_ids[i].clone());
+                    }
+                }
+
+                let intersection: HashSet<_> = have.intersection(&closest).collect();
+
+                assert_eq!(have.len(), kbucket::MAX_NODES_PER_BUCKET);
+                assert_eq!(intersection.len(), kbucket::MAX_NODES_PER_BUCKET);
+                return Ok(Async::Ready(()));
+            }
 
             Ok(Async::NotReady)
-        }
-    }
-
-    let a = TestContext {
-        target_key,
-        swarm_ids,
-        swarms,
-        have_key: Default::default(),
-        have_no_key: Default::default(),
-    };
-
-    Runtime::new().unwrap().block_on(
-        a
-    )
-    .unwrap();
+        }))
+        .unwrap()
 }
