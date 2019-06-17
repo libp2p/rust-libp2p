2f02786d23f56d47e7417bc7cde2773aa7cd0405
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-06-17 15:20:19 +0200

    PollParameters is now a trait

--- protocols/kad/src/behaviour.rs
+++ protocols/kad/src/behaviour.rs
@@ -824,7 +824,7 @@ where
 
     fn poll(
         &mut self,
-        parameters: &mut PollParameters<'_>,
+        parameters: &mut impl PollParameters,
     ) -> Async<
         NetworkBehaviourAction<
             <Self::ProtocolsHandler as ProtocolsHandler>::InEvent,
@@ -964,7 +964,7 @@ where
                                     key: target.clone(),
                                     provider_peer: KadPeer {
                                         node_id: parameters.local_peer_id().clone(),
-                                        multiaddrs: parameters.external_addresses().cloned().collect(),
+                                        multiaddrs: parameters.external_addresses().collect(),
                                         connection_ty: KadConnectionType::Connected,
                                     }
                                 },
