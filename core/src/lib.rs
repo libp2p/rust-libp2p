88dd60d80451565bfbbd7b1d2a46de5c064dd69b
Toralf Wittner <tw@dtex.org>
2019-07-04 11:09:20 +0200

    Update documentation.

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -18,46 +18,22 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-//! Transport, protocol upgrade and swarm systems of *libp2p*.
+//! Transports, upgrades, multiplexing and node handling of *libp2p*.
 //!
-//! This crate contains all the core traits and mechanisms of the transport and swarm systems
-//! of *libp2p*.
+//! The main concepts of libp2p-core are:
 //!
-//! # Overview
-//!
-//! This documentation focuses on the concepts of *libp2p-core*, and is interesting mostly if you
-//! want to extend *libp2p* with new protocols. If you only want to use libp2p, you might find the
-//! documentation of the main *libp2p* crate more interesting.
-//!
-//! The main concepts of libp2p are:
-//!
-//! - A `PeerId` is a unique global identifier for a node on the network. Each node must have a
-//!   different `PeerId`. Normally, a `PeerId` is the hash of the public key used to negotiate
-//!   encryption on the communication channel, thereby guaranteeing that they cannot be spoofed.
-//! - The `Transport` trait defines how to reach a remote node or listen for incoming remote
-//!   connections. See the `transport` module.
-//! - The `Swarm` struct contains all active and pending connections to remotes and manages the
-//!   state of all the substreams that have been opened, and all the upgrades that were built upon
-//!   these substreams.
-//! - Use the `NetworkBehaviour` trait to customize the behaviour of a `Swarm`. It is the
-//!   `NetworkBehaviour` that controls what happens on the network. Multiple types that implement
-//!   `NetworkBehaviour` can be composed into a single behaviour.
-//! - The `StreamMuxer` trait is implemented on structs that hold a connection to a remote and can
-//!   subdivide this connection into multiple substreams. See the `muxing` module.
-//! - The `UpgradeInfo`, `InboundUpgrade` and `OutboundUpgrade` traits define how to upgrade each
-//!   individual substream to use a protocol. See the `upgrade` module.
-//! - The `ProtocolsHandler` trait defines how each active connection to a remote should behave:
-//!   how to handle incoming substreams, which protocols are supported, when to open a new
-//!   outbound substream, etc. See the `protocols_handler` trait.
-//!
-//! # High-level APIs vs low-level APIs
-//!
-//! This crate provides two sets of APIs:
-//!
-//! - The low-level APIs are contained within the `nodes` module. See the documentation for more
-//!   information.
-//! - The high-level APIs include the concepts of `Swarm`, `ProtocolsHandler` and `NetworkBehaviour`.
-
+//! - A [`PeerId`] is a unique global identifier for a node on the network.
+//!   Each node must have a different `PeerId`. Normally, a `PeerId` is the
+//!   hash of the public key used to negotiate encryption on the
+//!   communication channel, thereby guaranteeing that they cannot be spoofed.
+//! - The [`Transport`] trait defines how to reach a remote node or listen for
+//!   incoming remote connections. See the `transport` module.
+//! - The [`StreamMuxer`] trait is implemented on structs that hold a connection
+//!   to a remote and can subdivide this connection into multiple substreams.
+//!   See the `muxing` module.
+//! - The [`UpgradeInfo`], [`InboundUpgrade`] and [`OutboundUpgrade`] traits
+//!   define how to upgrade each individual substream to use a protocol.
+//!   See the `upgrade` module.
 
 /// Multi-address re-export.
 pub use multiaddr;
--- swarm/src/lib.rs
+++ swarm/src/lib.rs
@@ -20,24 +20,37 @@
 
 //! High level manager of the network.
 //!
-//! A [`Swarm`] contains the state of the network as a whole. The entire behaviour of a
-//! libp2p network can be controlled through the `Swarm`.
+//! A [`Swarm`] contains the state of the network as a whole. The entire
+//! behaviour of a libp2p network can be controlled through the `Swarm`.
+//! The `Swarm` struct contains all active and pending connections to
+//! remotes and manages the state of all the substreams that have been
+//! opened, and all the upgrades that were built upon these substreams.
 //!
 //! # Initializing a Swarm
 //!
 //! Creating a `Swarm` requires three things:
 //!
 //!  1. A network identity of the local node in form of a [`PeerId`].
-//!  2. An implementation of the [`Transport`] trait. This is the type that will be used in
-//!     order to reach nodes on the network based on their address. See the [`transport`] module
-//!     for more information.
-//!  3. An implementation of the [`NetworkBehaviour`] trait. This is a state machine that
-//!     defines how the swarm should behave once it is connected to a node.
+//!  2. An implementation of the [`Transport`] trait. This is the type that
+//!     will be used in order to reach nodes on the network based on their
+//!     address. See the `transport` module for more information.
+//!  3. An implementation of the [`NetworkBehaviour`] trait. This is a state
+//!     machine that defines how the swarm should behave once it is connected
+//!     to a node.
 //!
 //! # Network Behaviour
 //!
-//! The `NetworkBehaviour` trait is implemented on types that indicate to the swarm how it should
-//! behave. This includes which protocols are supported and which nodes to try to connect to.
+//! The [`NetworkBehaviour`] trait is implemented on types that indicate to
+//! the swarm how it should behave. This includes which protocols are supported
+//! and which nodes to try to connect to. It is the `NetworkBehaviour` that
+//! controls what happens on the network. Multiple types that implement
+//! `NetworkBehaviour` can be composed into a single behaviour.
+//!
+//! # Protocols Handler
+//!
+//! The [`ProtocolsHandler`] trait defines how each active connection to a
+//! remote should behave: how to handle incoming substreams, which protocols
+//! are supported, when to open a new outbound substream, etc.
 //!
 
 mod behaviour;
