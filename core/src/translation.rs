3c48724c08908a9c1d98d7b9f85cdf803997fe3e
Max Inden <mail@max-inden.de>
2019-07-17 14:05:52 +0200

    core/src/translation: Support dns4 and dns6
    Add dns4 and dns6 as valid protocol replacements for the origin address
    to construct external addresses of a given node.

--- core/src/translation.rs
+++ core/src/translation.rs
@@ -22,24 +22,26 @@ use multiaddr::{Multiaddr, Protocol};
 
 /// Perform IP address translation.
 ///
-/// Given an `original` [`Multiaddr`] and some `observed` [`Multiaddr`], replace
-/// the first protocol of the `original` with the first protocol of the
-/// `observed` [`Multiaddr`] and return this translated [`Multiaddr`].
+/// Given an `original` [`Multiaddr`] and some `observed` [`Multiaddr`], replace the first protocol
+/// of the `original` with the first protocol of the `observed` [`Multiaddr`] and return this
+/// translated [`Multiaddr`].
 ///
 /// This function can for example be useful when handling tcp connections. Tcp does not listen and
 /// dial on the same port by default. Thus when receiving an observed address on a connection that
-/// we initiated, it will contain our dialing port, not our listening port. Thus we need to take the
-/// ip address or dns address from the observed address and the port from the original address.
+/// we initiated, it will contain our dialing port, not our listening port. We need to take the ip
+/// address or dns address from the observed address and the port from the original address.
 ///
-/// This is a mixed-mode translation, i.e. an IPv4 address may be replaced by
-/// an IPv6 address and vice versa.
+/// This is a mixed-mode translation, i.e. an IPv4 / DNS4 address may be replaced by an IPv6 / DNS6
+/// address and vice versa.
 ///
 /// If the first [`Protocol`]s are not IP addresses, `None` is returned instead.
 pub fn address_translation(original: &Multiaddr, observed: &Multiaddr) -> Option<Multiaddr> {
     original.replace(0, move |proto| match proto {
-        Protocol::Ip4(_) | Protocol::Ip6(_) => match observed.iter().next() {
+        Protocol::Ip4(_) | Protocol::Ip6(_) | Protocol::Dns4(_) | Protocol::Dns6(_) => match observed.iter().next() {
             x @ Some(Protocol::Ip4(_)) => x,
             x @ Some(Protocol::Ip6(_)) => x,
+            x @ Some(Protocol::Dns4(_)) => x,
+            x @ Some(Protocol::Dns6(_)) => x,
             _ => None,
         },
         _ => None,
@@ -82,6 +84,18 @@ mod tests {
                 original: "/ip6/2001:db8:0:0:0:0:0:0/tcp/1".parse().unwrap(),
                 observed: "/ip4/192.0.2.2/tcp/2".parse().unwrap(),
                 expected: "/ip4/192.0.2.2/tcp/1".parse().unwrap(),
+            },
+            // Dns.
+            Test {
+                original: "/dns4/foo/tcp/1".parse().unwrap(),
+                observed: "/dns4/bar/tcp/2".parse().unwrap(),
+                expected: "/dns4/bar/tcp/1".parse().unwrap(),
+            },
+            // Ipv4 Dns mix.
+            Test {
+                original: "/ip4/192.0.2.1/tcp/1".parse().unwrap(),
+                observed: "/dns4/bar/tcp/2".parse().unwrap(),
+                expected: "/dns4/bar/tcp/1".parse().unwrap(),
             },
         ];
 
