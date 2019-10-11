a61ad928b812dc93b7961a1cfab55ad874a25819
Akihito Nakano <sora.akatsuki@gmail.com>
2019-10-11 19:14:18 +0900

    Implement /plaintext/2.0.0 (#1236)
    * WIP
    
    * plaintext/2.0.0
    
    * Refactor protobuf related issues to compatible with the spec
    
    * Rename: new PlainTextConfig -> PlainText2Config
    
    * Keep plaintext/1.0.0 as PlainText1Config
    
    * Config contains pubkey
    
    * Rename: proposition -> exchange
    
    * Add PeerId to Exchange
    
    * Check the validity of the remote's `Exchange`
    
    * Tweak
    
    * Delete unused import
    
    * Add debug log
    
    * Delete unused field: public_key_encoded
    
    * Delete unused field: local
    
    * Delete unused field: exchange_bytes
    
    * The inner instance should not be public
    
    * identity::Publickey::Rsa is not available on wasm
    
    * Delete PeerId from Config as it should be generated from the pubkey
    
    * Catch up for #1240
    
    * Tweak
    
    * Update protocols/plaintext/src/error.rs
    
    Co-Authored-By: Pierre Krieger <pierre.krieger1708@gmail.com>
    
    * Update protocols/plaintext/src/handshake.rs
    
    Co-Authored-By: Pierre Krieger <pierre.krieger1708@gmail.com>
    
    * Update protocols/plaintext/src/error.rs
    
    Co-Authored-By: Pierre Krieger <pierre.krieger1708@gmail.com>
    
    * Update protocols/plaintext/src/error.rs
    
    Co-Authored-By: Roman Borschel <romanb@users.noreply.github.com>
    
    * Update protocols/plaintext/src/error.rs
    
    Co-Authored-By: Roman Borschel <romanb@users.noreply.github.com>
    
    * Rename: pubkey -> local_public_key
    
    * Delete unused error
    
    * Rename: PeerIdValidationFailed -> InvalidPeerId
    
    * Fix: HandShake -> Handshake
    
    * Use bytes insteadof Publickey to avoid code duplication
    
    * Replace with ProtobufError
    
    * Merge HandshakeContext<()> into HandshakeContext<Local>
    
    * Improve the peer ID validation to simplify the handshake
    
    * Propagate Remote to allow extracting the PeerId from the Remote
    
    * Collapse the same kind of errors into the variant

--- protocols/plaintext/src/lib.rs
+++ protocols/plaintext/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2018 Parity Technologies (UK) Ltd.
+// Copyright 2019 Parity Technologies (UK) Ltd.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -18,15 +18,31 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-use futures::future::{self, FutureResult};
-use libp2p_core::{InboundUpgrade, OutboundUpgrade, UpgradeInfo, upgrade::Negotiated};
+use bytes::BytesMut;
+use futures::{Future, StartSend, Poll, future};
+use futures::sink::Sink;
+use futures::stream::MapErr as StreamMapErr;
+use futures::stream::Stream;
+use libp2p_core::{identity, InboundUpgrade, OutboundUpgrade, UpgradeInfo, upgrade::Negotiated, PeerId, PublicKey};
+use log::debug;
+use rw_stream_sink::RwStreamSink;
+use std::io;
 use std::iter;
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_io::codec::length_delimited::Framed;
+use crate::error::PlainTextError;
 use void::Void;
+use futures::future::FutureResult;
+use crate::handshake::Remote;
+
+mod error;
+mod handshake;
+mod pb;
 
 #[derive(Debug, Copy, Clone)]
-pub struct PlainTextConfig;
+pub struct PlainText1Config;
 
-impl UpgradeInfo for PlainTextConfig {
+impl UpgradeInfo for PlainText1Config {
     type Info = &'static [u8];
     type InfoIter = iter::Once<Self::Info>;
 
@@ -35,7 +51,7 @@ impl UpgradeInfo for PlainTextConfig {
     }
 }
 
-impl<C> InboundUpgrade<C> for PlainTextConfig {
+impl<C> InboundUpgrade<C> for PlainText1Config {
     type Output = Negotiated<C>;
     type Error = Void;
     type Future = FutureResult<Negotiated<C>, Self::Error>;
@@ -45,7 +61,7 @@ impl<C> InboundUpgrade<C> for PlainTextConfig {
     }
 }
 
-impl<C> OutboundUpgrade<C> for PlainTextConfig {
+impl<C> OutboundUpgrade<C> for PlainText1Config {
     type Output = Negotiated<C>;
     type Error = Void;
     type Future = FutureResult<Negotiated<C>, Self::Error>;
@@ -55,3 +71,160 @@ impl<C> OutboundUpgrade<C> for PlainTextConfig {
     }
 }
 
+#[derive(Clone)]
+pub struct PlainText2Config {
+    pub local_public_key: identity::PublicKey,
+}
+
+impl UpgradeInfo for PlainText2Config {
+    type Info = &'static [u8];
+    type InfoIter = iter::Once<Self::Info>;
+
+    fn protocol_info(&self) -> Self::InfoIter {
+        iter::once(b"/plaintext/2.0.0")
+    }
+}
+
+impl<C> InboundUpgrade<C> for PlainText2Config
+where
+    C: AsyncRead + AsyncWrite + Send + 'static
+{
+    type Output = (PeerId, PlainTextOutput<Negotiated<C>>);
+    type Error = PlainTextError;
+    type Future = Box<dyn Future<Item = Self::Output, Error = Self::Error> + Send>;
+
+    fn upgrade_inbound(self, socket: Negotiated<C>, _: Self::Info) -> Self::Future {
+        Box::new(self.handshake(socket))
+    }
+}
+
+impl<C> OutboundUpgrade<C> for PlainText2Config
+where
+    C: AsyncRead + AsyncWrite + Send + 'static
+{
+    type Output = (PeerId, PlainTextOutput<Negotiated<C>>);
+    type Error = PlainTextError;
+    type Future = Box<dyn Future<Item = Self::Output, Error = Self::Error> + Send>;
+
+    fn upgrade_outbound(self, socket: Negotiated<C>, _: Self::Info) -> Self::Future {
+        Box::new(self.handshake(socket))
+    }
+}
+
+impl PlainText2Config {
+    fn handshake<T>(self, socket: T) -> impl Future<Item = (PeerId, PlainTextOutput<T>), Error = PlainTextError>
+    where
+        T: AsyncRead + AsyncWrite + Send + 'static
+    {
+        debug!("Starting plaintext upgrade");
+        PlainTextMiddleware::handshake(socket, self)
+            .map(|(stream_sink, remote)| {
+                let mapped = stream_sink.map_err(map_err as fn(_) -> _);
+                (
+                    remote.peer_id,
+                    PlainTextOutput {
+                        stream: RwStreamSink::new(mapped),
+                        remote_key: remote.public_key,
+                    }
+                )
+            })
+    }
+}
+
+#[inline]
+fn map_err(err: io::Error) -> io::Error {
+    debug!("error during plaintext handshake {:?}", err);
+    io::Error::new(io::ErrorKind::InvalidData, err)
+}
+
+pub struct PlainTextMiddleware<S> {
+    inner: Framed<S, BytesMut>,
+}
+
+impl<S> PlainTextMiddleware<S>
+where
+    S: AsyncRead + AsyncWrite + Send,
+{
+    fn handshake(socket: S, config: PlainText2Config)
+        -> impl Future<Item = (PlainTextMiddleware<S>, Remote), Error = PlainTextError>
+    {
+        handshake::handshake(socket, config).map(|(inner, remote)| {
+            (PlainTextMiddleware { inner }, remote)
+        })
+    }
+}
+
+impl<S> Sink for PlainTextMiddleware<S>
+where
+    S: AsyncRead + AsyncWrite,
+{
+    type SinkItem = BytesMut;
+    type SinkError = io::Error;
+
+    #[inline]
+    fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
+        self.inner.start_send(item)
+    }
+
+    #[inline]
+    fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
+        self.inner.poll_complete()
+    }
+
+    #[inline]
+    fn close(&mut self) -> Poll<(), Self::SinkError> {
+        self.inner.close()
+    }
+}
+
+impl<S> Stream for PlainTextMiddleware<S>
+where
+    S: AsyncRead + AsyncWrite,
+{
+    type Item = BytesMut;
+    type Error = io::Error;
+
+    #[inline]
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        self.inner.poll()
+    }
+}
+
+/// Output of the plaintext protocol.
+pub struct PlainTextOutput<S>
+where
+    S: AsyncRead + AsyncWrite,
+{
+    /// The plaintext stream.
+    pub stream: RwStreamSink<StreamMapErr<PlainTextMiddleware<S>, fn(io::Error) -> io::Error>>,
+    /// The public key of the remote.
+    pub remote_key: PublicKey,
+}
+
+impl<S: AsyncRead + AsyncWrite> std::io::Read for PlainTextOutput<S> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        self.stream.read(buf)
+    }
+}
+
+impl<S: AsyncRead + AsyncWrite> AsyncRead for PlainTextOutput<S> {
+    unsafe fn prepare_uninitialized_buffer(&self, buf: &mut [u8]) -> bool {
+        self.stream.prepare_uninitialized_buffer(buf)
+    }
+}
+
+impl<S: AsyncRead + AsyncWrite> std::io::Write for PlainTextOutput<S> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.stream.write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.stream.flush()
+    }
+}
+
+impl<S: AsyncRead + AsyncWrite> AsyncWrite for PlainTextOutput<S> {
+    fn shutdown(&mut self) -> Poll<(), io::Error> {
+        self.stream.shutdown()
+    }
+}
