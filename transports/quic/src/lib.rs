202ccdafee1c9cb4e5a97312d85a41a56b889bca
Demi M. Obenour <demi@parity.io>
2019-12-03 19:16:48 -0500

    Compiling (but not working) quinn-proto based libp2p-quic
    There are plenty of `unimplemented!()` calls, and TLS certificate
    processing has not even been started, but this should still be a good
    start.

--- transports/quic/src/lib.rs
+++ transports/quic/src/lib.rs
@@ -42,10 +42,8 @@
 //! QUIC connections do not need to be upgraded.  You will get a compile-time error if you try.
 //! Instead, you must pass all needed configuration into the constructor.
 
-use futures::{
-    future,
-    prelude::*,
-};
+use async_std::net::UdpSocket;
+use futures::{channel::mpsc, future, prelude::*};
 use ipnet::IpNet;
 use libp2p_core::{
     multiaddr::{ip_to_multiaddr, Multiaddr, Protocol},
@@ -53,13 +51,15 @@ use libp2p_core::{
     StreamMuxer, Transport,
 };
 use log::debug;
-pub use quinn::{Endpoint, EndpointBuilder, EndpointError, ServerConfig};
+use quinn_proto::{Connection, ConnectionEvent, ConnectionHandle, Dir, StreamId};
 use std::{
+    collections::HashMap,
     io,
     net::{IpAddr, SocketAddr},
     pin::Pin,
-    sync::Mutex,
+    sync::{Arc, Mutex, MutexGuard},
     task::{Context, Poll},
+    time::Instant,
 };
 
 /// Represents the configuration for a QUIC/UDP/IP transport capability for libp2p.
@@ -68,25 +68,10 @@ use std::{
 /// obtained by libp2p through the tokio reactor.
 #[derive(Debug, Clone)]
 pub struct QuicConfig {
-    /// The underlying QUIC transport config.  Quinn provides functions for creating a suitable
-    /// one.
-    pub endpoint_builder: EndpointBuilder,
-    /// The underlying QUIC transport endpoint.
-    endpoint: Option<Endpoint>,
+    /// The client configuration.  Quinn provides functions for making one.
+    pub client_configuration: quinn_proto::ClientConfig,
     /// The server configuration.  Quinn provides functions for making one.
-    pub server_configuration: ServerConfig,
-}
-
-/// An error in the QUIC transport
-#[derive(Debug, err_derive::Error)]
-pub enum QuicError {
-    /// An I/O error
-    #[error(display = "Endpoint error: {}", _0)]
-    EndpointError(#[source] quinn::EndpointError),
-    #[error(display = "QUIC Protocol Error: {}", _0)]
-    ConnectionError(#[source] quinn::ConnectionError),
-    #[error(display = "QUIC outbound connection error: {}", _0)]
-    ConnectError(#[source] quinn::ConnectError),
+    pub server_configuration: quinn_proto::ServerConfig,
 }
 
 impl QuicConfig {
@@ -102,105 +87,323 @@ impl Default for QuicConfig {
     }
 }
 
-pub struct QuicIncoming {
-	/// This field uses structural pinning…
-    incoming: quinn::Incoming,
-	/// but this field does not.
-    addr: Multiaddr,
+#[derive(Debug)]
+pub struct Muxer {
+    endpoint: Arc<Endpoint>,
+    connection: Connection,
+    handle: ConnectionHandle,
+    channel: mpsc::UnboundedReceiver<ConnectionEvent>,
+    /// Tasks blocked on writing
+    writers: HashMap<StreamId, Vec<std::task::Waker>>,
+    /// Tasks blocked on reading
+    readers: HashMap<StreamId, Vec<std::task::Waker>>,
+    /// Tasks waiting for new connections
+    acceptors: Vec<std::task::Waker>,
+    /// Tasks waiting to make a connection
+    connectors: Vec<std::task::Waker>,
 }
 
-type CompatConnecting = future::MapErr<quinn::Connecting, fn(quinn::ConnectionError) -> QuicError>;
+#[derive(Debug)]
+struct Endpoint {
+    inner: Mutex<quinn_proto::Endpoint>,
+    socket: UdpSocket,
+    hashtable: Mutex<HashMap<ConnectionHandle, mpsc::Sender<ConnectionEvent>>>,
+    channel: mpsc::Sender<QuicMuxer>,
+}
 
-impl futures_core::stream::Stream for QuicIncoming {
-    type Item = Result<ListenerEvent<CompatConnecting>, QuicError>;
-    fn poll_next(mut self: Pin<&mut Self>, ctx: &mut Context) -> Poll<Option<Self::Item>> {
-        match Pin::new(&mut self.incoming).poll_next(ctx) {
-            Poll::Pending => Poll::Pending,
-            Poll::Ready(Some(upgrade)) => {
-                let peer = upgrade.remote_address();
-                Poll::Ready(Some(Ok(ListenerEvent::Upgrade {
-                    remote_addr: ip_to_multiaddr(
-                        peer.ip(),
-                        &[Protocol::Udp(peer.port()), Protocol::Quic],
-                    ),
-                    upgrade: upgrade.map_err(QuicError::ConnectionError as _),
-                    local_addr: self.addr.clone(),
-                })))
+impl Muxer {
+    /// Send all outgoing packets
+    async fn transmit(&mut self) -> Result<(), async_std::io::Error> {
+        while let Some(quinn_proto::Transmit {
+            destination,
+            ecn,
+            contents,
+        }) = self.connection.poll_transmit(Instant::now())
+        {
+            drop(ecn);
+            self.endpoint
+                .socket
+                .send_to(&*contents, destination)
+                .await?;
+        }
+        Ok(())
+    }
+
+    /// Process all endpoint-facing events for this connection.  This is synchronous and will not
+    /// fail.
+    fn send_to_endpoint(&mut self) {
+        let mut endpoint = self.endpoint.inner.lock().unwrap();
+        while let Some(endpoint_event) = self.connection.poll_endpoint_events() {
+            if let Some(connection_event) = endpoint.handle_event(self.handle, endpoint_event) {
+                self.connection.handle_event(connection_event)
             }
-            Poll::Ready(None) => Poll::Ready(None),
+        }
+    }
+
+    /// Process application events
+    fn process_app_events(&mut self) {
+        use quinn_proto::Event;
+        self.send_to_endpoint();
+        while let Some(event) = self.connection.poll() {
+            match event {
+                Event::Connected => {
+                    panic!("is not emitted more than once; we already received it; qed")
+                }
+                Event::DatagramSendUnblocked => {
+                    panic!("we never try to send datagrams, so this will never happen; qed")
+                }
+                Event::StreamOpened { dir: Dir::Uni } => {
+                    let id = self
+                        .connection
+                        .accept(Dir::Uni)
+                        .expect("we just received an incoming connection; qed");
+                    self.connection
+                        .stop_sending(id, Default::default())
+                        .expect("we just accepted this stream, so we know it; qed")
+                }
+                Event::StreamAvailable { dir: Dir::Uni } | Event::DatagramReceived => continue,
+                Event::StreamReadable { stream } => drop(
+                    self.readers
+                        .get_mut(&stream)
+                        .and_then(|s| s.pop())
+                        .map(|w| w.wake()),
+                ),
+                Event::StreamWritable { stream } => drop(
+                    self.writers
+                        .get_mut(&stream)
+                        .and_then(|s| s.pop())
+                        .map(|w| w.wake()),
+                ),
+                Event::StreamAvailable { dir: Dir::Bi } => {
+                    drop(self.acceptors.pop().map(|w| w.wake()))
+                }
+                Event::ConnectionLost { .. } => break, // nothing more
+                Event::StreamFinished { .. } => unimplemented!(),
+                Event::StreamOpened { dir: Dir::Bi } => {
+                    drop(self.connectors.pop().map(|w| w.wake()))
+                }
+            }
+        }
+    }
+
+    async fn process(&mut self) {
+        while let Some(s) = self.channel.next().await {
+            self.connection.handle_event(s)
         }
     }
 }
 
-struct QuicMuxer {
-    bi_streams: quinn::IncomingBiStreams,
-    connection: quinn::Connection,
-    driver: quinn::ConnectionDriver,
+#[derive(Debug)]
+pub enum FutureMuxer {
+    Connecting(Option<std::task::Waker>, Muxer),
+    Connected,
 }
 
-pub struct SyncQuicMuxer(Mutex<QuicMuxer>);
+impl Future for FutureMuxer {
+    type Output = Result<QuicMuxer, io::Error>;
+    fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        let inner: &mut _ = Pin::into_inner(self);
+        match inner {
+            FutureMuxer::Connected => panic!("polled after yielding Ready"),
+            FutureMuxer::Connecting(ref mut waker, _) => unimplemented!(),
+        }
+    }
+    // 		self.0.
+    // 		let FutureMuxer(ref mut muxer) = self.into_inner();
+    // 		let muxer = muxer.as_mut().expect("polled after yielding Ready");
+    // 		match muxer.connection.poll() {
+    // 			Some(Event::Connected) => true,
+    // 			Some(_) => panic!("`Event::Connected` is the first event emitted; we have not seen it yet, so we will not see any other events; qed"),
+    // 			None =>
+    //
+    //
+    // 		if self.has_connected {
+    // 			Poll::Ready(self.clone());
+    // 		match self.as_mut().connection_waker {
+    // 			None => Poll::Ready(
+}
 
-// FIXME: if quinn ever starts using `!Unpin` futures, this will require `unsafe` code.
-// Will probably fall back to spawning the connection driver in this case 🙁
-impl StreamMuxer for SyncQuicMuxer {
-    type OutboundSubstream = quinn::OpenBi;
-    type Substream = Mutex<(quinn::SendStream, quinn::RecvStream)>;
+#[derive(Debug)]
+pub struct QuicMuxer(Mutex<Muxer>);
+
+impl QuicMuxer {
+    fn inner<'a>(&'a self) -> MutexGuard<'a, Muxer> {
+        self.0
+            .lock()
+            .expect("we already panicked, so are in an inconsistent state; qed")
+    }
+}
+
+impl StreamMuxer for QuicMuxer {
+    type OutboundSubstream = ();
+    type Substream = quinn_proto::StreamId;
     type Error = std::io::Error;
+    fn open_outbound(&self) {}
+    fn destroy_outbound(&self, (): ()) {}
+    fn destroy_substream(&self, substream: Self::Substream) {}
+    fn is_remote_acknowledged(&self) -> bool {
+        true
+    }
+
     fn poll_inbound(&self, cx: &mut Context) -> Poll<Result<Self::Substream, Self::Error>> {
-		let mut this = self.0.lock().unwrap();
-        match Pin::new(&mut this.driver).poll(cx) {
-            Poll::Ready(Ok(())) => unreachable!(
-                "ConnectionDriver will not resolve as long as `self.bi_streams` is alive"
-            ),
-            Poll::Ready(Err(e)) => return Poll::Ready(Err(e.into())),
-            Poll::Pending => (),
+        let mut inner = self.inner();
+        match inner.connection.accept(quinn_proto::Dir::Bi) {
+            None => {
+                inner.acceptors.push(cx.waker().clone());
+                Poll::Pending
+            }
+            Some(stream) => {
+                inner.send_to_endpoint();
+                Poll::Ready(Ok(stream))
+            }
         }
-        match Pin::new(&mut this.bi_streams).poll_next(cx) {
-			Poll::Ready(Some(stream)) => Poll::Ready(stream.map(Mutex::new).map_err(From::from)),
-			Poll::Ready(None) => Poll::Ready(Err(quinn::ConnectionError::LocallyClosed.into())),
-			Poll::Pending => Poll::Pending,
-		}
     }
-    fn open_outbound(&self) -> Self::OutboundSubstream {
-        self.0.lock().unwrap().connection.open_bi()
+
+    fn write_substream(
+        &self,
+        cx: &mut Context,
+        substream: &mut Self::Substream,
+        buf: &[u8],
+    ) -> Poll<Result<usize, Self::Error>> {
+        use quinn_proto::WriteError;
+        let mut inner = self.inner();
+        match inner.connection.write(*substream, buf) {
+            Ok(bytes) => {
+                inner.send_to_endpoint();
+                Poll::Ready(Ok(bytes))
+            }
+            Err(WriteError::Blocked) => {
+                inner
+                    .writers
+                    .entry(*substream)
+                    .or_insert(vec![])
+                    .push(cx.waker().clone());
+                Poll::Pending
+            }
+            Err(WriteError::UnknownStream) => {
+                panic!("libp2p never uses a closed stream, so this cannot happen; qed")
+            }
+            Err(WriteError::Stopped { error_code: _ }) => {
+                Poll::Ready(Err(std::io::ErrorKind::ConnectionAborted.into()))
+            }
+        }
     }
-	fn destroy_substream(&self, _substream: Self::Substream) {
-	}
-	fn destroy_outbound(&self, _substream: Self::OutboundSubstream) {
-	}
-	fn is_remote_acknowledged(&self) -> bool {
-		true
-	}
-	fn write_substream(&self, cx: &mut Context, substream: &mut Self::Substream, buf: &[u8]) -> Poll<Result<usize, Self::Error>> {
-		Pin::new(&mut substream.lock().unwrap().0).poll_write(cx, buf)
-	}
-	fn poll_outbound(&self, cx: &mut Context, substream: &mut Self::OutboundSubstream) -> Poll<Result<Self::Substream, Self::Error>> {
-		Pin::new(substream).poll(cx).map_err(From::from).map_ok(Mutex::new)
-	}
-	fn read_substream(&self, cx: &mut Context, substream: &mut Self::Substream, buf: &mut [u8]) -> Poll<Result<usize, Self::Error>> {
-		Pin::new(&mut substream.lock().unwrap().1).poll_read(cx, buf)
-	}
-    fn shutdown_substream(&self, cx: &mut Context, substream: &mut Self::Substream) -> Poll<Result<(), Self::Error>> {
-		Pin::new(&mut substream.lock().unwrap().0).poll_close(cx)
-	}
-    fn flush_substream(&self, cx: &mut Context, substream: &mut Self::Substream) -> Poll<Result<(), Self::Error>> {
-		// The actual implementation does nothing.  This at least does something.
-		self.write_substream(cx, substream, b"").map_ok(drop)
-	}
+
+    fn poll_outbound(
+        &self,
+        cx: &mut Context,
+        substream: &mut Self::OutboundSubstream,
+    ) -> Poll<Result<Self::Substream, Self::Error>> {
+        let mut inner = self.inner();
+        match inner.connection.open(Dir::Bi) {
+            None => {
+                inner.connectors.push(cx.waker().clone());
+                Poll::Pending
+            }
+            Some(id) => {
+                inner.send_to_endpoint();
+                Poll::Ready(Ok(id))
+            }
+        }
+    }
+
+    fn read_substream(
+        &self,
+        cx: &mut Context,
+        substream: &mut Self::Substream,
+        buf: &mut [u8],
+    ) -> Poll<Result<usize, Self::Error>> {
+        use quinn_proto::ReadError;
+        let mut inner = self.inner();
+        match inner.connection.read(*substream, buf) {
+            Ok(Some(bytes)) => {
+                inner.send_to_endpoint();
+                Poll::Ready(Ok(bytes))
+            }
+            Ok(None) => unimplemented!(),
+            Err(ReadError::Blocked) => {
+                inner
+                    .readers
+                    .entry(*substream)
+                    .or_insert(vec![])
+                    .push(cx.waker().clone());
+                Poll::Pending
+            }
+            Err(ReadError::UnknownStream) => {
+                panic!("libp2p never uses a closed stream, so this cannot happen; qed")
+            }
+            Err(ReadError::Reset { error_code: _ }) => {
+                Poll::Ready(Err(io::ErrorKind::ConnectionReset.into()))
+            }
+        }
+    }
+
+    fn shutdown_substream(
+        &self,
+        cx: &mut Context,
+        substream: &mut Self::Substream,
+    ) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(
+            self.inner()
+                .connection
+                .finish(*substream)
+                .map_err(|e| match e {
+                    quinn_proto::FinishError::UnknownStream => {
+                        panic!("libp2p never uses a closed stream, so this cannot happen; qed")
+                    }
+                    quinn_proto::FinishError::Stopped { .. } => {
+                        io::ErrorKind::ConnectionReset.into()
+                    }
+                }),
+        )
+    }
+
+    fn flush_substream(
+        &self,
+        cx: &mut Context,
+        substream: &mut Self::Substream,
+    ) -> Poll<Result<(), Self::Error>> {
+        self.write_substream(cx, substream, b"").map_ok(drop)
+    }
+
     fn flush_all(&self, _cx: &mut Context) -> Poll<Result<(), Self::Error>> {
-		Poll::Ready(Ok(()))
-	}
-    fn close(&self, _cx: &mut Context) -> Poll<Result<(), Self::Error>> {
-		Poll::Ready(Ok(self.0.lock().unwrap().connection.close(0u32.into(), b"")))
-	}
+        Poll::Ready(Ok(()))
+    }
+
+    fn close(&self, cx: &mut Context) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(self.inner().connection.close(
+            Instant::now(),
+            Default::default(),
+            Default::default(),
+        )))
+    }
+}
+
+impl Stream for Endpoint {
+    type Item = Result<QuicMuxer, io::Error>;
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        let mut inner = self.inner.lock().unwrap();
+        // inner.pending_connections -= 1;
+        inner.accept();
+        unimplemented!()
+    }
+}
+
+#[derive(Debug)]
+struct QuicEndpoint(Arc<Endpoint>);
+
+impl QuicEndpoint {
+    fn inner(&self) -> MutexGuard<'_, quinn_proto::Endpoint> {
+        self.0.inner.lock().unwrap()
+    }
 }
 
 impl Transport for QuicConfig {
-    type Output = quinn::NewConnection;
-    type Error = QuicError;
-    type Listener = QuicIncoming;
-    type ListenerUpgrade = CompatConnecting;
-    type Dial = CompatConnecting;
+    type Output = QuicMuxer;
+    type Error = io::Error;
+    type Listener = mpsc::Receiver<Result<ListenerEvent<Self::ListenerUpgrade>, Self::Error>>;
+    type ListenerUpgrade = future::Ready<Result<QuicMuxer, Self::Error>>;
+    type Dial = FutureMuxer;
 
     fn listen_on(self, addr: Multiaddr) -> Result<Self::Listener, TransportError<Self::Error>> {
         let socket_addr = if let Ok(sa) = multiaddr_to_socketaddr(&addr) {
@@ -209,36 +412,34 @@ impl Transport for QuicConfig {
             return Err(TransportError::MultiaddrNotSupported(addr));
         };
 
-        let (driver, _endpoint, incoming) = self
-            .endpoint_builder
-            .bind(&socket_addr)
-            .map_err(|e| TransportError::Other(QuicError::EndpointError(e)))?;
-        tokio::task::spawn(driver.map_err(drop));
-        Ok(QuicIncoming { incoming, addr })
+        // NOT blocking, as per man:bind(2)
+        let socket = std::net::UdpSocket::bind(&socket_addr)?.into();
+        Endpoint {
+            socket,
+            inner: Mutex::new(
+                quinn_proto::Endpoint::new(Default::default(), None)
+                    .expect("the default config is valid; qed"),
+            ),
+            hashtable: Mutex::new(HashMap::new()),
+            channel: unimplemented!(),
+        };
+        unimplemented!()
     }
 
     fn dial(self, addr: Multiaddr) -> Result<Self::Dial, TransportError<Self::Error>> {
         let socket_addr = if let Ok(socket_addr) = multiaddr_to_socketaddr(&addr) {
             if socket_addr.port() == 0 || socket_addr.ip().is_unspecified() {
                 debug!("Instantly refusing dialing {}, as it is invalid", addr);
-                return Err(TransportError::Other(QuicError::EndpointError(
-                    EndpointError::Socket(io::ErrorKind::ConnectionRefused.into()),
-                )));
+                return Err(TransportError::Other(
+                    io::ErrorKind::ConnectionRefused.into(),
+                ));
             }
             socket_addr
         } else {
             return Err(TransportError::MultiaddrNotSupported(addr));
         };
 
-        let (_driver, endpoint, _incoming) =
-            self.endpoint_builder
-                .bind(&([0u8; 16], 0u16).into())
-                .map_err(|e| TransportError::Other(QuicError::EndpointError(e)))?;
-
-        Ok(endpoint
-            .connect(&socket_addr, &socket_addr.to_string())
-            .map_err(QuicError::ConnectError)?
-            .map_err(QuicError::ConnectionError as _))
+        unimplemented!()
     }
 }
 
