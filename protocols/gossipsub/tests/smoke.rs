ff9565b9f9af3278385c56dad56a453f89f01d02
Max Inden <mail@max-inden.de>
2020-01-06 10:57:29 +0100

    protocols/gossipsub/tests/smoke: Remove unused variable

--- protocols/gossipsub/tests/smoke.rs
+++ protocols/gossipsub/tests/smoke.rs
@@ -207,7 +207,7 @@ fn multi_hop_propagation() {
         // Wait for all nodes to receive the published message.
         let mut received_msgs = 0;
         graph.wait_for(&mut rt, move |ev| {
-            if let GossipsubEvent::Message(_peer_id, _message_id, msg) = ev {
+            if let GossipsubEvent::Message(..) = ev {
                 received_msgs += 1;
                 if received_msgs == number_nodes - 1 {
                     return true;
