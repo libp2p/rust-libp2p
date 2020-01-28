0120149fd80f43e8cf46b8adf4fabb08f5af96c5
Rüdiger Klaehn <rklaehn@gmail.com>
2020-01-28 14:27:56 +0100

    Add gossipsub and ping

---
+++ examples/ipfs.rs
@@ -0,0 +1,309 @@
+// Copyright 2018 Parity Technologies (UK) Ltd.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+//! A basic chat application demonstrating libp2p and the mDNS and floodsub protocols.
+//!
+//! Using two terminal windows, start two instances. If you local network allows mDNS,
+//! they will automatically connect. Type a message in either terminal and hit return: the
+//! message is sent and printed in the other terminal. Close with Ctrl-c.
+//!
+//! You can of course open more terminal windows and add more participants.
+//! Dialing any of the other peers will propagate the new participant to all
+//! chat members and everyone will receive all messages.
+//!
+//! # If they don't automatically connect
+//!
+//! If the nodes don't automatically connect, take note of the listening address of the first
+//! instance and start the second with this address as the first argument. In the first terminal
+//! window, run:
+//!
+//! ```sh
+//! cargo run --example chat
+//! ```
+//!
+//! It will print the PeerId and the listening address, e.g. `Listening on
+//! "/ip4/0.0.0.0/tcp/24915"`
+//!
+//! In the second terminal window, start a new instance of the example with:
+//!
+//! ```sh
+//! cargo run --example chat -- /ip4/127.0.0.1/tcp/24915
+//! ```
+//!
+//! The two nodes then connect.
+
+use async_std::{io, task};
+use futures::{future, prelude::*};
+use libp2p::core::transport::upgrade::Version;
+use libp2p::mplex::MplexConfig;
+use libp2p::pnet::PnetConfig;
+use libp2p::secio::SecioConfig;
+use libp2p::tcp::TcpConfig;
+use libp2p::identify::{Identify, IdentifyEvent};
+use libp2p::yamux::Config as YamuxConfig;
+use libp2p::Transport;
+use libp2p::{
+    floodsub::{self, Floodsub, FloodsubEvent},
+    gossipsub::{self, Gossipsub, GossipsubEvent},
+    identity,
+    mdns::{Mdns, MdnsEvent},
+    pnet::PreSharedKey,
+    ping::{self, Ping, PingConfig, PingEvent},
+    swarm::NetworkBehaviourEventProcess,    
+    Multiaddr, NetworkBehaviour, PeerId, Swarm,
+};
+use libp2p_core::StreamMuxer;
+use std::str::FromStr;
+use std::time::Duration;
+use std::{
+    error::Error,
+    task::{Context, Poll},
+};
+
+/// Builds the transport that serves as a common ground for all connections.
+///
+/// Set up an encrypted TCP transport over the Mplex protocol.
+pub fn build_transport(
+    key_pair: identity::Keypair,
+    psk: PreSharedKey,
+) -> io::Result<
+    impl Transport<
+            Output = (
+                PeerId,
+                impl StreamMuxer<
+                        OutboundSubstream = impl Send,
+                        Substream = impl Send,
+                        Error = impl Into<io::Error>,
+                    > + Send
+                    + Sync,
+            ),
+            Error = impl Error + Send,
+            Listener = impl Send,
+            Dial = impl Send,
+            ListenerUpgrade = impl Send,
+        > + Clone,
+> {
+    let secio_config = SecioConfig::new(key_pair);
+    let yamux_config = YamuxConfig::default();
+    let mplex_config = MplexConfig::new();    
+
+    Ok(TcpConfig::new()
+        .nodelay(true)
+        .and_then(move |socket, _| PnetConfig::new(psk).handshake(socket))
+        .upgrade(Version::V1)
+        .authenticate(secio_config)
+        .multiplex(libp2p::core::upgrade::SelectUpgrade::new(
+            yamux_config,
+            mplex_config,
+        ))
+        .timeout(Duration::from_secs(20)))
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
+    // Create a random PeerId
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    let psk = PreSharedKey::from_str("/key/swarm/psk/1.0.0/\n/base16/\n6189c5cf0b87fb800c1a9feeda73c6ab5e998db48fb9e6a978575c770ceef683").unwrap();
+    println!("Local peer id: {:?}", local_peer_id);
+    println!("Swarm key: {}", psk);
+    println!("Swarm key fingerprint: {}", psk.fingerprint());
+
+    // Set up a an encrypted DNS-enabled TCP Transport over the Mplex and Yamux protocols
+    let transport = build_transport(local_key.clone(), psk)?;
+
+    // Create a Floodsub topic
+    let floodsub_topic = floodsub::Topic::new("hello");
+
+    // Create a Gossipsub topic
+    let gossipsub_topic = gossipsub::Topic::new("hello".into());
+
+    // We create a custom network behaviour that combines floodsub and mDNS.
+    // In the future, we want to improve libp2p to make this easier to do.
+    // Use the derive to generate delegating NetworkBehaviour impl and require the
+    // NetworkBehaviourEventProcess implementations below.
+    #[derive(NetworkBehaviour)]
+    struct MyBehaviour<TSubstream: AsyncRead + AsyncWrite> {
+        floodsub: Floodsub<TSubstream>,
+        mdns: Mdns<TSubstream>,
+        identify: Identify<TSubstream>,
+        gossipsub: Gossipsub<TSubstream>,
+        ping: Ping<TSubstream>,
+
+        // Struct fields which do not implement NetworkBehaviour need to be ignored
+        #[behaviour(ignore)]
+        #[allow(dead_code)]
+        ignored_member: bool,
+    }
+
+    impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<IdentifyEvent>
+        for MyBehaviour<TSubstream>
+    {
+        // Called when `identify` produces an event.
+        fn inject_event(&mut self, event: IdentifyEvent) {
+            println!("identify: {:?}", event);
+        }
+    }
+
+    impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<FloodsubEvent>
+        for MyBehaviour<TSubstream>
+    {
+        // Called when `floodsub` produces an event.
+        fn inject_event(&mut self, message: FloodsubEvent) {
+            if let FloodsubEvent::Message(message) = message {
+                println!(
+                    "Received: '{:?}' from {:?}",
+                    String::from_utf8_lossy(&message.data),
+                    message.source
+                );
+            }
+        }
+    }
+
+    impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<MdnsEvent>
+        for MyBehaviour<TSubstream>
+    {
+        // Called when `mdns` produces an event.
+        fn inject_event(&mut self, event: MdnsEvent) {
+            match event {
+                MdnsEvent::Discovered(list) => {
+                    for (peer, addr) in list {
+                        println!("Discovered: {}/ipfs/{}", addr, peer);
+                        self.floodsub.add_node_to_partial_view(peer);
+                    }
+                }
+                MdnsEvent::Expired(list) => {
+                    for (peer, addr) in list {
+                        println!("Expired: {}/ipfs/{}", addr, peer);
+                        if !self.mdns.has_node(&peer) {
+                            self.floodsub.remove_node_from_partial_view(&peer);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<GossipsubEvent>
+        for MyBehaviour<TSubstream>
+    {
+        // Called when `mdns` produces an event.
+        fn inject_event(&mut self, event: GossipsubEvent) {
+            match event {
+                GossipsubEvent::Message(peer_id, id, message) => println!(
+                    "Got message: {} with id: {} from peer: {:?}",
+                    String::from_utf8_lossy(&message.data),
+                    id,
+                    peer_id
+                ),
+                _ => {}
+            }
+        }
+    }
+
+    impl<TSubstream: AsyncRead + AsyncWrite> NetworkBehaviourEventProcess<PingEvent>
+        for MyBehaviour<TSubstream>
+    {
+        // Called when `ping` produces an event.
+        fn inject_event(&mut self, event: PingEvent) {
+            use ping::handler::{PingSuccess, PingFailure};
+            match event {
+                PingEvent { peer, result: Result::Ok(PingSuccess::Ping { rtt }) } => {
+                    println!("ping: rtt to {} is {} ms", peer.to_base58(), rtt.as_millis());
+                },
+                PingEvent { peer, result: Result::Ok(PingSuccess::Pong) } => {
+                    println!("ping: pong from {}", peer.to_base58());
+                },
+                PingEvent { peer, result: Result::Err(PingFailure::Timeout) } => {
+                    println!("ping: timeout to {}", peer.to_base58());
+                },
+                PingEvent { peer, result: Result::Err(PingFailure::Other { error }) } => {
+                    println!("ping: failure with {}: {}", peer.to_base58(), error);
+                }
+            }
+        }
+    }
+
+    // Create a Swarm to manage peers and events
+    let mut swarm = {
+        let mdns = Mdns::new()?;
+        let gossipsub_config = gossipsub::GossipsubConfig::default();
+        let mut behaviour = MyBehaviour {
+            floodsub: Floodsub::new(local_peer_id.clone()),
+            gossipsub: gossipsub::Gossipsub::new(local_peer_id.clone(), gossipsub_config),
+            identify: Identify::new(
+                "/ipfs/0.1.0".into(),
+                "rust-ipfs".into(),
+                local_key.public(),
+            ),
+            ping: Ping::new(PingConfig::new()),
+            mdns,
+            ignored_member: false,
+        };
+
+        behaviour.floodsub.subscribe(floodsub_topic.clone());
+        Swarm::new(transport, behaviour, local_peer_id.clone())
+    };
+
+    // Reach out to another node if specified
+    if let Some(to_dial) = std::env::args().nth(1) {
+        let addr: Multiaddr = to_dial.parse()?;
+        Swarm::dial_addr(&mut swarm, addr)?;
+        println!("Dialed {:?}", to_dial)
+    }
+
+    // Read full lines from stdin
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
+
+    // Listen on all interfaces and whatever port the OS assigns
+    Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    // Kick it off
+    let mut listening = false;
+    task::block_on(future::poll_fn(move |cx: &mut Context| {
+        loop {
+            match stdin.try_poll_next_unpin(cx)? {
+                Poll::Ready(Some(line)) => {
+                    swarm.floodsub.publish(floodsub_topic.clone(), line.as_bytes());
+                    swarm.gossipsub.publish(&gossipsub_topic, line.as_bytes());
+                },
+                Poll::Ready(None) => panic!("Stdin closed"),
+                Poll::Pending => break,
+            }
+        }
+        loop {
+            match swarm.poll_next_unpin(cx) {
+                Poll::Ready(Some(event)) => println!("{:?}", event),
+                Poll::Ready(None) => return Poll::Ready(Ok(())),
+                Poll::Pending => {
+                    if !listening {
+                        for addr in Swarm::listeners(&swarm) {
+                            println!("Identity {}/ipfs/{}", addr, local_peer_id);
+                            listening = true;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        Poll::Pending
+    }))
+}
