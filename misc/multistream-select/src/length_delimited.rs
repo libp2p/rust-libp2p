24fe550e591d2c38b43386bb7a41e53c40867401
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-03-05 18:20:41 +0100

    Fix intradoc links

--- misc/multistream-select/src/length_delimited.rs
+++ misc/multistream-select/src/length_delimited.rs
@@ -299,10 +299,10 @@ impl<R> LengthDelimitedReader<R> {
     /// # Panic
     ///
     /// Will panic if called while there is data in the read or write buffer.
-    /// The read buffer is guaranteed to be empty whenever [`Stream::poll`] yields
-    /// a new `Message`. The write buffer is guaranteed to be empty whenever
-    /// [`LengthDelimited::poll_write_buffer`] yields [`Async::Ready`] or after
-    /// the [`Sink`] has been completely flushed via [`Sink::poll_complete`].
+    /// The read buffer is guaranteed to be empty whenever [`Stream::poll_next`]
+    /// yield a new `Message`. The write buffer is guaranteed to be empty whenever
+    /// [`LengthDelimited::poll_write_buffer`] yields [`Poll::Ready`] or after
+    /// the [`Sink`] has been completely flushed via [`Sink::poll_flush`].
     pub fn into_inner(self) -> (R, BytesMut) {
         self.inner.into_inner()
     }
