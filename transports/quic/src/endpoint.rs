d0ab22977ca4ef74b4ab340ea57af505bef7f504
Demi M. Obenour <demi@parity.io>
2020-03-26 11:27:11 -0400

    libp2p-tls → libp2p-x509

--- transports/quic/src/endpoint.rs
+++ transports/quic/src/endpoint.rs
@@ -61,7 +61,7 @@ impl Config {
         transport.datagram_receive_buffer_size(None);
         transport.keep_alive_interval(Some(Duration::from_millis(10)));
         let transport = Arc::new(transport);
-        let (client_tls_config, server_tls_config) = tls::make_tls_config(keypair);
+        let (client_tls_config, server_tls_config) = x509::make_tls_config(keypair);
         let mut server_config = quinn_proto::ServerConfig::default();
         server_config.transport = transport.clone();
         server_config.crypto = Arc::new(server_tls_config);
@@ -272,7 +272,7 @@ impl Connection {
             .expect("we always require the peer to present a certificate; qed");
         // we have already verified that there is (exactly) one peer certificate,
         // and that it has a valid libp2p extension.
-        Poll::Ready(Ok(tls::extract_peerid_or_panic(certificate[0].as_ref())))
+        Poll::Ready(Ok(x509::extract_peerid_or_panic(certificate[0].as_ref())))
     }
 
     /// Wake up the last task registered by
