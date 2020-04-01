ab59fcd5882002a4f4c08a6fc299ede1f558d390
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-04-01 13:48:44 +0200

    Finish change

--- swarm/src/behaviour.rs
+++ swarm/src/behaviour.rs
@@ -146,7 +146,7 @@ pub trait NetworkBehaviour: Send + 'static {
     }
 
     /// A listener closed.
-    fn inject_listener_closed(&mut self, _id: ListenerId, _reason: Result<(), std::io::Error>) {
+    fn inject_listener_closed(&mut self, _id: ListenerId, _reason: Result<(), &std::io::Error>) {
     }
 
     /// Polls for things that swarm should do.
