e736ff47a51586a13d2a3f1aae496f34944a7917
Toralf Wittner <tw@dtex.org>
2020-06-15 16:10:04 +0200

    Avoid recursive dependencies due to tests.
    Integration tests from `core` and `core-derived` are moved into a
    top-level `tests` directory.

--- core/src/connection/listeners.rs
+++ core/src/connection/listeners.rs
@@ -37,46 +37,6 @@ use std::{collections::VecDeque, fmt, pin::Pin};
 /// The `ListenersStream` never ends and never produces errors. If a listener errors or closes,
 /// an event is generated on the stream and the listener is then dropped, but the `ListenersStream`
 /// itself continues.
-///
-/// # Example
-///
-/// ```no_run
-/// # fn main() {
-/// use futures::prelude::*;
-/// use libp2p_core::connection::{ListenersEvent, ListenersStream};
-///
-/// let mut listeners = ListenersStream::new(libp2p_tcp::TcpConfig::new());
-///
-/// // Ask the `listeners` to start listening on the given multiaddress.
-/// listeners.listen_on("/ip4/0.0.0.0/tcp/0".parse().unwrap()).unwrap();
-///
-/// // The `listeners` will now generate events when polled.
-/// futures::executor::block_on(async move {
-///     while let Some(event) = listeners.next().await {
-///         match event {
-///             ListenersEvent::NewAddress { listener_id, listen_addr } => {
-///                 println!("Listener {:?} is listening at address {}", listener_id, listen_addr);
-///             },
-///             ListenersEvent::AddressExpired { listener_id, listen_addr } => {
-///                 println!("Listener {:?} is no longer listening at address {}", listener_id, listen_addr);
-///             },
-///             ListenersEvent::Closed { listener_id, .. } => {
-///                 println!("Listener {:?} has been closed", listener_id);
-///             },
-///             ListenersEvent::Error { listener_id, error } => {
-///                 println!("Listener {:?} has experienced an error: {}", listener_id, error);
-///             },
-///             ListenersEvent::Incoming { listener_id, upgrade, local_addr, .. } => {
-///                 println!("Listener {:?} has a new connection on {}", listener_id, local_addr);
-///                 // We don't do anything with the newly-opened connection, but in a real-life
-///                 // program you probably want to use it!
-///                 drop(upgrade);
-///             },
-///         }
-///     }
-/// })
-/// # }
-/// ```
 pub struct ListenersStream<TTrans>
 where
     TTrans: Transport,
