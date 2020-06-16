e5542e8cd474cd9fb91220a7e0b50c81760148c6
Max Inden <mail@max-inden.de>
2020-06-16 19:00:59 +0200

    Revert "protocols/kad/query/disjoint: Log number finished on finish_paths"
    This reverts commit b69f24faf8419768d470c73bdbed24c5a11e43c7.

--- protocols/kad/src/query/peers/closest/disjoint.rs
+++ protocols/kad/src/query/peers/closest/disjoint.rs
@@ -21,7 +21,6 @@
 use super::*;
 use crate::kbucket::{Key, KeyBytes};
 use libp2p_core::PeerId;
-use log::debug;
 use std::{
     collections::HashMap,
     iter::{Cycle, Map, Peekable},
@@ -283,18 +282,10 @@ impl ClosestDisjointPeersIter {
     where
         I: IntoIterator<Item = &'a PeerId>
     {
-        let mut count = 0;
-
         for peer in peers {
-            count += 1;
             if let Some(PeerState{ initiated_by, .. }) = self.contacted_peers.get_mut(peer) {
                 self.iters[*initiated_by].finish();
             }
-        }
-
-        debug!("Finished {} out of {} paths.", count, self.iters.len());
-        if self.is_finished() {
-            debug!("All paths finished.");
         }
     }
 
