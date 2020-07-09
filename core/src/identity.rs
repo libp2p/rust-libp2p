4ffe7280f3c46ffcb0b45cbcd997bf0e2de842e6
Age Manning <Age@AgeManning.com>
2020-07-09 11:37:07 +1000

    Revert changes to core/

--- core/src/identity.rs
+++ core/src/identity.rs
@@ -29,7 +29,7 @@ pub mod secp256k1;
 pub mod error;
 
 use self::error::*;
-use crate::{keys_proto, PeerId};
+use crate::{PeerId, keys_proto};
 
 /// Identity keypair of a node.
 ///
@@ -57,7 +57,7 @@ pub enum Keypair {
     Rsa(rsa::Keypair),
     /// A Secp256k1 keypair.
     #[cfg(feature = "secp256k1")]
-    Secp256k1(secp256k1::Keypair),
+    Secp256k1(secp256k1::Keypair)
 }
 
 impl Keypair {
@@ -100,7 +100,7 @@ impl Keypair {
             #[cfg(not(target_arch = "wasm32"))]
             Rsa(ref pair) => pair.sign(msg),
             #[cfg(feature = "secp256k1")]
-            Secp256k1(ref pair) => pair.secret().sign(msg),
+            Secp256k1(ref pair) => pair.secret().sign(msg)
         }
     }
 
@@ -127,7 +127,7 @@ pub enum PublicKey {
     Rsa(rsa::PublicKey),
     #[cfg(feature = "secp256k1")]
     /// A public Secp256k1 key.
-    Secp256k1(secp256k1::PublicKey),
+    Secp256k1(secp256k1::PublicKey)
 }
 
 impl PublicKey {
@@ -142,7 +142,7 @@ impl PublicKey {
             #[cfg(not(target_arch = "wasm32"))]
             Rsa(pk) => pk.verify(msg, sig),
             #[cfg(feature = "secp256k1")]
-            Secp256k1(pk) => pk.verify(msg, sig),
+            Secp256k1(pk) => pk.verify(msg, sig)
         }
     }
 
@@ -152,26 +152,27 @@ impl PublicKey {
         use prost::Message;
 
         let public_key = match self {
-            PublicKey::Ed25519(key) => keys_proto::PublicKey {
-                r#type: keys_proto::KeyType::Ed25519 as i32,
-                data: key.encode().to_vec(),
-            },
+            PublicKey::Ed25519(key) =>
+                keys_proto::PublicKey {
+                    r#type: keys_proto::KeyType::Ed25519 as i32,
+                    data: key.encode().to_vec()
+                },
             #[cfg(not(target_arch = "wasm32"))]
-            PublicKey::Rsa(key) => keys_proto::PublicKey {
-                r#type: keys_proto::KeyType::Rsa as i32,
-                data: key.encode_x509(),
-            },
+            PublicKey::Rsa(key) =>
+                keys_proto::PublicKey {
+                    r#type: keys_proto::KeyType::Rsa as i32,
+                    data: key.encode_x509()
+                },
             #[cfg(feature = "secp256k1")]
-            PublicKey::Secp256k1(key) => keys_proto::PublicKey {
-                r#type: keys_proto::KeyType::Secp256k1 as i32,
-                data: key.encode().to_vec(),
-            },
+            PublicKey::Secp256k1(key) =>
+                keys_proto::PublicKey {
+                    r#type: keys_proto::KeyType::Secp256k1 as i32,
+                    data: key.encode().to_vec()
+                }
         };
 
         let mut buf = Vec::with_capacity(public_key.encoded_len());
-        public_key
-            .encode(&mut buf)
-            .expect("Vec<u8> provides capacity as needed");
+        public_key.encode(&mut buf).expect("Vec<u8> provides capacity as needed");
         buf
     }
 
@@ -190,7 +191,7 @@ impl PublicKey {
         match key_type {
             keys_proto::KeyType::Ed25519 => {
                 ed25519::PublicKey::decode(&pubkey.data).map(PublicKey::Ed25519)
-            }
+            },
             #[cfg(not(target_arch = "wasm32"))]
             keys_proto::KeyType::Rsa => {
                 rsa::PublicKey::decode_x509(&pubkey.data).map(PublicKey::Rsa)
@@ -199,7 +200,7 @@ impl PublicKey {
             keys_proto::KeyType::Rsa => {
                 log::debug!("support for RSA was disabled at compile-time");
                 Err(DecodingError::new("Unsupported"))
-            }
+            },
             #[cfg(feature = "secp256k1")]
             keys_proto::KeyType::Secp256k1 => {
                 secp256k1::PublicKey::decode(&pubkey.data).map(PublicKey::Secp256k1)
@@ -217,3 +218,4 @@ impl PublicKey {
         self.into()
     }
 }
+
