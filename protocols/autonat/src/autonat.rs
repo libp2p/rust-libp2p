b2f527aaa9bfbd42e57eb56526e32003ac3b343a
David Craven <david@craven.ch>
2020-07-20 19:03:33 +0200

    Add behaviour boilerplate.

--- protocols/autonat/src/autonat.rs
+++ protocols/autonat/src/autonat.rs
@@ -18,6 +18,149 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-pub struct AutoNat;
+pub use crate::protocol::DialResponse;
+use crate::protocol::{AutoNatCodec, AutoNatProtocol};
+use libp2p_core::{connection::ConnectionId, ConnectedPoint, Multiaddr, PeerId};
+use libp2p_request_response::{
+    handler::RequestProtocol, handler::RequestResponseHandlerEvent, ProtocolSupport,
+    RequestResponse, RequestResponseConfig, RequestResponseEvent, RequestResponseMessage,
+};
+use libp2p_swarm::{NetworkBehaviour, NetworkBehaviourAction, PollParameters};
+use std::{
+    iter,
+    task::{Context, Poll},
+};
 
-pub enum AutoNatEvent {}
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum AutoNatEvent {
+    DialResponse(DialResponse),
+}
+
+pub struct AutoNat {
+    inner: RequestResponse<AutoNatCodec>,
+}
+
+impl AutoNat {
+    pub fn new() -> Self {
+        let protocols = iter::once((AutoNatProtocol, ProtocolSupport::Full));
+        let cfg = RequestResponseConfig::default();
+        let inner = RequestResponse::new(AutoNatCodec, protocols, cfg);
+        Self { inner }
+    }
+
+    pub fn add_address(&mut self, peer_id: &PeerId, addr: Multiaddr) {
+        self.inner.add_address(peer_id, addr)
+    }
+}
+
+impl NetworkBehaviour for AutoNat {
+    type ProtocolsHandler = <RequestResponse<AutoNatCodec> as NetworkBehaviour>::ProtocolsHandler;
+    type OutEvent = AutoNatEvent;
+
+    fn new_handler(&mut self) -> Self::ProtocolsHandler {
+        self.inner.new_handler()
+    }
+
+    fn addresses_of_peer(&mut self, peer: &PeerId) -> Vec<Multiaddr> {
+        self.inner.addresses_of_peer(peer)
+    }
+
+    fn inject_connected(&mut self, peer: &PeerId) {
+        self.inner.inject_connected(peer)
+    }
+
+    fn inject_connection_established(
+        &mut self,
+        peer: &PeerId,
+        conn: &ConnectionId,
+        endpoint: &ConnectedPoint,
+    ) {
+        self.inner
+            .inject_connection_established(peer, conn, endpoint)
+    }
+
+    fn inject_connection_closed(
+        &mut self,
+        peer: &PeerId,
+        conn: &ConnectionId,
+        endpoint: &ConnectedPoint,
+    ) {
+        self.inner.inject_connection_closed(peer, conn, endpoint)
+    }
+
+    fn inject_disconnected(&mut self, peer: &PeerId) {
+        self.inner.inject_disconnected(peer)
+    }
+
+    fn inject_dial_failure(&mut self, peer: &PeerId) {
+        self.inner.inject_dial_failure(peer)
+    }
+
+    fn inject_event(
+        &mut self,
+        peer: PeerId,
+        conn: ConnectionId,
+        event: RequestResponseHandlerEvent<AutoNatCodec>,
+    ) {
+        self.inner.inject_event(peer, conn, event)
+    }
+
+    fn poll(
+        &mut self,
+        cx: &mut Context,
+        pp: &mut impl PollParameters,
+    ) -> Poll<NetworkBehaviourAction<RequestProtocol<AutoNatCodec>, AutoNatEvent>> {
+        loop {
+            match self.inner.poll(cx, pp) {
+                Poll::Ready(NetworkBehaviourAction::GenerateEvent(
+                    RequestResponseEvent::Message { peer, message },
+                )) => match message {
+                    RequestResponseMessage::Request { request, channel } => {
+                        println!("{} {:?} {:?}", peer, request, channel);
+                    }
+                    RequestResponseMessage::Response {
+                        request_id,
+                        response,
+                    } => {
+                        println!("{} {:?} {:?}", peer, request_id, response);
+                    }
+                },
+                Poll::Ready(NetworkBehaviourAction::GenerateEvent(
+                    RequestResponseEvent::OutboundFailure {
+                        peer,
+                        request_id,
+                        error,
+                    },
+                )) => {
+                    println!("outbound failure {} {:?} {:?}", peer, request_id, error);
+                }
+                Poll::Ready(NetworkBehaviourAction::GenerateEvent(
+                    RequestResponseEvent::InboundFailure { peer, error },
+                )) => {
+                    println!("inbound failure {} {:?}", peer, error);
+                }
+                Poll::Ready(NetworkBehaviourAction::DialAddress { address }) => {
+                    return Poll::Ready(NetworkBehaviourAction::DialAddress { address })
+                }
+                Poll::Ready(NetworkBehaviourAction::DialPeer { peer_id, condition }) => {
+                    return Poll::Ready(NetworkBehaviourAction::DialPeer { peer_id, condition })
+                }
+                Poll::Ready(NetworkBehaviourAction::NotifyHandler {
+                    peer_id,
+                    handler,
+                    event,
+                }) => {
+                    return Poll::Ready(NetworkBehaviourAction::NotifyHandler {
+                        peer_id,
+                        handler,
+                        event,
+                    })
+                }
+                Poll::Ready(NetworkBehaviourAction::ReportObservedAddr { address }) => {
+                    return Poll::Ready(NetworkBehaviourAction::ReportObservedAddr { address })
+                }
+                Poll::Pending => return Poll::Pending,
+            }
+        }
+    }
+}
