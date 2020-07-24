dc0dda5f92ce59a065822462e2c7726ccbbac528
Rüdiger Klaehn <rklaehn@protonmail.com>
2020-07-24 23:22:21 +0200

    Change the gossipsub rpc protocol to use bytes for message ids
    And do the corresponding changes to make this work.

--- protocols/gossipsub/src/behaviour.rs
+++ protocols/gossipsub/src/behaviour.rs
@@ -314,7 +314,7 @@ impl Gossipsub {
             None => {
                 warn!(
                     "Message not in cache. Ignoring forwarding. Message Id: {}",
-                    message_id.0
+                    message_id
                 );
                 return false;
             }
