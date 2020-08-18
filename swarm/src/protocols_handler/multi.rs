b132ab5e678012c8d79ae12ba47c8ec100361748
Toralf Wittner <tw@dtex.org>
2020-08-18 15:50:00 +0200

    Add `MultiHandler::inject_listen_ugrade_error`.

--- swarm/src/protocols_handler/multi.rs
+++ swarm/src/protocols_handler/multi.rs
@@ -36,7 +36,8 @@ use crate::upgrade::{
     UpgradeInfoSend
 };
 use futures::{future::BoxFuture, prelude::*};
-use libp2p_core::{ConnectedPoint, Multiaddr, PeerId, upgrade::ProtocolName};
+use libp2p_core::{ConnectedPoint, Multiaddr, PeerId};
+use libp2p_core::upgrade::{ProtocolName, UpgradeError, NegotiationError, ProtocolError};
 use rand::Rng;
 use std::{
     collections::{HashMap, HashSet},
@@ -155,9 +156,49 @@ where
 
     fn inject_listen_upgrade_error(
         &mut self,
-        _: ProtocolsHandlerUpgrErr<<Self::InboundProtocol as InboundUpgradeSend>::Error>
+        error: ProtocolsHandlerUpgrErr<<Self::InboundProtocol as InboundUpgradeSend>::Error>
     ) {
-        // TODO: ???
+        match error {
+            ProtocolsHandlerUpgrErr::Timer =>
+                for h in self.handlers.values_mut() {
+                    h.inject_listen_upgrade_error(ProtocolsHandlerUpgrErr::Timer)
+                }
+            ProtocolsHandlerUpgrErr::Timeout =>
+                for h in self.handlers.values_mut() {
+                    h.inject_listen_upgrade_error(ProtocolsHandlerUpgrErr::Timeout)
+                }
+            ProtocolsHandlerUpgrErr::Upgrade(UpgradeError::Select(NegotiationError::Failed)) =>
+                for h in self.handlers.values_mut() {
+                    h.inject_listen_upgrade_error(ProtocolsHandlerUpgrErr::Upgrade(UpgradeError::Select(NegotiationError::Failed)))
+                }
+            ProtocolsHandlerUpgrErr::Upgrade(UpgradeError::Select(NegotiationError::ProtocolError(e))) =>
+                match e {
+                    ProtocolError::IoError(e) =>
+                        for h in self.handlers.values_mut() {
+                            let e = NegotiationError::ProtocolError(ProtocolError::IoError(e.kind().into()));
+                            h.inject_listen_upgrade_error(ProtocolsHandlerUpgrErr::Upgrade(UpgradeError::Select(e)))
+                        }
+                    ProtocolError::InvalidMessage =>
+                        for h in self.handlers.values_mut() {
+                            let e = NegotiationError::ProtocolError(ProtocolError::InvalidMessage);
+                            h.inject_listen_upgrade_error(ProtocolsHandlerUpgrErr::Upgrade(UpgradeError::Select(e)))
+                        }
+                    ProtocolError::InvalidProtocol =>
+                        for h in self.handlers.values_mut() {
+                            let e = NegotiationError::ProtocolError(ProtocolError::InvalidProtocol);
+                            h.inject_listen_upgrade_error(ProtocolsHandlerUpgrErr::Upgrade(UpgradeError::Select(e)))
+                        }
+                    ProtocolError::TooManyProtocols =>
+                        for h in self.handlers.values_mut() {
+                            let e = NegotiationError::ProtocolError(ProtocolError::TooManyProtocols);
+                            h.inject_listen_upgrade_error(ProtocolsHandlerUpgrErr::Upgrade(UpgradeError::Select(e)))
+                        }
+                }
+            ProtocolsHandlerUpgrErr::Upgrade(UpgradeError::Apply((k, e))) =>
+                if let Some(h) = self.handlers.get_mut(&k) {
+                    h.inject_listen_upgrade_error(ProtocolsHandlerUpgrErr::Upgrade(UpgradeError::Apply(e)))
+                }
+        }
     }
 
     fn connection_keep_alive(&self) -> KeepAlive {
