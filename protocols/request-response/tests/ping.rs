4e0d318ad14b78fdf771634e032340051e73d751
Tejas Sanap <sanap.tejas@gmail.com>
2021-01-19 14:18:51 +0530

    Adding new test to ping.rs
    Say you have two requests R1 and R2 all targeting peer P1. Say R1 is done and a result is returned to the upper layer. Say R2 is still in self.pending_outbound_requests.
    
    If you call is_pending_outbound(P1, R1) you will get true even though R1 is already finished. You need to check the request_id in the RequestProtocol in pending_outbound_request.
    
    Signed-off-by: Tejas Sanap <sanap.tejas@gmail.com>

--- protocols/request-response/tests/ping.rs
+++ protocols/request-response/tests/ping.rs
@@ -106,14 +106,21 @@ fn ping_protocol() {
                     assert_eq!(&peer, &peer1_id);
                     assert_eq!(req_id, request_id);
                     if count >= num_pings {
-                        return
+                       break 
                     } else {
                         req_id = swarm2.send_request(&peer1_id, ping.clone());
                     }
+                    println!("hi, {}", req_id);
                 },
                 e => panic!("Peer2: Unexpected event: {:?}", e)
             }
         }
+
+        // check if is_pending_outbound returns false for a finished request when we have already issued a new one
+        let req_id2 = swarm2.send_request(&peer1_id, ping.clone());
+        assert!(swarm2.is_pending_outbound(&peer1_id, &req_id) == false);
+        assert!(swarm2.is_pending_outbound(&peer1_id, &req_id2));
+
     };
 
     async_std::task::spawn(Box::pin(peer1));
