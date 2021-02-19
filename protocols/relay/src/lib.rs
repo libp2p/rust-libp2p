d2f17b3c1e9471a8525d378232ba58d8efc2e199
Max Inden <mail@max-inden.de>
2021-02-19 15:15:41 +0100

    protocols/relay: Add crate level example

--- protocols/relay/src/lib.rs
+++ protocols/relay/src/lib.rs
@@ -21,6 +21,50 @@
 //! Implementation of the [libp2p circuit relay
 //! specification](https://github.com/libp2p/specs/tree/master/relay).
 //!
+//! ## Example
+//!
+//! ```rust
+//! # use libp2p_core::transport::memory::MemoryTransport;
+//! # use libp2p_relay::{RelayConfig, new_transport_and_behaviour};
+//! # use libp2p_swarm::Swarm;
+//! # use libp2p_core::{identity, Multiaddr, multiaddr::Protocol, PeerId, upgrade, Transport};
+//! # use libp2p_yamux::YamuxConfig;
+//! # use libp2p_plaintext::PlainText2Config;
+//! # use std::convert::TryInto;
+//! # use std::str::FromStr;
+//! #
+//! # let local_key = identity::Keypair::generate_ed25519();
+//! # let local_public_key = local_key.public();
+//! # let local_peer_id = local_public_key.clone().into_peer_id();
+//! # let plaintext = PlainText2Config {
+//! #     local_public_key: local_public_key.clone(),
+//! # };
+//! #
+//! let (relay_transport, relay_behaviour) = new_transport_and_behaviour(
+//!     RelayConfig::default(),
+//!     MemoryTransport::default(),
+//! );
+//!
+//! let transport = relay_transport
+//!     .upgrade(upgrade::Version::V1)
+//!     .authenticate(plaintext)
+//!     .multiplex(YamuxConfig::default())
+//!     .boxed();
+//!
+//! let mut swarm = Swarm::new(transport, relay_behaviour, local_peer_id);
+//!
+//! let relay_addr = Multiaddr::from_str("/memory/1234").unwrap()
+//!     .with(Protocol::P2p(PeerId::random().into()))
+//!     .with(Protocol::P2pCircuit);
+//! let dst_addr = relay_addr.clone().with(Protocol::Memory(5678));
+//!
+//! // Listen for incoming connections via relay node (1234).
+//! Swarm::listen_on(&mut swarm, relay_addr).unwrap();
+//!
+//! // Dial node (5678) via relay node (1234).
+//! Swarm::dial_addr(&mut swarm, dst_addr).unwrap();
+//! ```
+//!
 //! ## Terminology
 //!
 //! ### Entities
@@ -63,8 +107,7 @@ pub fn new_transport_and_behaviour<T: Transport + Clone>(
     config: RelayConfig,
     transport: T,
 ) -> (RelayTransportWrapper<T>, Relay) {
-    let (transport, (to_transport, from_transport)) =
-        RelayTransportWrapper::new(transport);
+    let (transport, (to_transport, from_transport)) = RelayTransportWrapper::new(transport);
     let behaviour = Relay::new(config, to_transport, from_transport);
     (transport, behaviour)
 }
