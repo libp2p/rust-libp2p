581cee0c8fb77e26ac885b047abf9e7e801a17dc
Thomas Eizinger <thomas@eizinger.io>
2021-06-08 17:48:58 +1000

    Implement rendezvous protocol using `upgrade::from_fn`

--- protocols/rendezvous/src/protocol.rs
+++ protocols/rendezvous/src/protocol.rs
@@ -1,52 +1,28 @@
 use crate::codec::RendezvousCodec;
 use asynchronous_codec::Framed;
-use futures::AsyncRead;
-use futures::AsyncWrite;
-use libp2p_core::{InboundUpgrade, OutboundUpgrade, UpgradeInfo};
-use log::debug;
-use std::{future, iter};
+use libp2p_core::upgrade::FromFnUpgrade;
+use libp2p_core::{upgrade, Endpoint};
+use libp2p_swarm::NegotiatedSubstream;
+use std::future;
+use upgrade::from_fn;
 use void::Void;
 
 pub fn new() -> Rendezvous {
-    Rendezvous::new()
+    from_fn(
+        b"/rendezvous/1.0.0",
+        Box::new(|socket, _| future::ready(Ok(Framed::new(socket, RendezvousCodec::default())))),
+    )
 }
 
-#[derive(Default, Debug, Copy, Clone)]
-pub struct Rendezvous;
-
-impl Rendezvous {
-    fn new() -> Rendezvous {
-        Rendezvous
-    }
-}
-
-impl UpgradeInfo for Rendezvous {
-    type Info = &'static [u8];
-    type InfoIter = iter::Once<Self::Info>;
-
-    fn protocol_info(&self) -> Self::InfoIter {
-        iter::once(b"/rendezvous/1.0.0")
-    }
-}
-
-impl<TSocket: AsyncRead + AsyncWrite> InboundUpgrade<TSocket> for Rendezvous {
-    type Output = Framed<TSocket, RendezvousCodec>;
-    type Error = Void;
-    type Future = future::Ready<Result<Self::Output, Self::Error>>;
-
-    fn upgrade_inbound(self, socket: TSocket, _: Self::Info) -> Self::Future {
-        debug!("upgrading inbound");
-        future::ready(Ok(Framed::new(socket, RendezvousCodec::default())))
-    }
-}
-
-impl<TSocket: AsyncRead + AsyncWrite> OutboundUpgrade<TSocket> for Rendezvous {
-    type Output = Framed<TSocket, RendezvousCodec>;
-    type Error = Void;
-    type Future = future::Ready<Result<Self::Output, Self::Error>>;
-
-    fn upgrade_outbound(self, socket: TSocket, _: Self::Info) -> Self::Future {
-        debug!("upgrading outbound");
-        future::ready(Ok(Framed::new(socket, RendezvousCodec::default())))
-    }
-}
+pub type Rendezvous = FromFnUpgrade<
+    &'static [u8],
+    Box<
+        dyn Fn(
+                NegotiatedSubstream,
+                Endpoint,
+            )
+                -> future::Ready<Result<Framed<NegotiatedSubstream, RendezvousCodec>, Void>>
+            + Send
+            + 'static,
+    >,
+>;
