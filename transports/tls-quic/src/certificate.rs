cda726443996031e8125475d937aea1cdd10c477
Roman Proskuryakov <r.proskuryakoff@gmail.com>
2021-07-12 18:47:12 +0300

    transports/tls-quic: Fix review notes

--- transports/tls-quic/src/certificate.rs
+++ transports/tls-quic/src/certificate.rs
@@ -25,6 +25,18 @@
 use libp2p_core::identity;
 use x509_parser::prelude::*;
 
+/// The libp2p Public Key Extension is a X.509 extension
+/// with the Object Identier 1.3.6.1.4.1.53594.1.1,
+/// allocated by IANA to the libp2p project at Protocol Labs.
+const P2P_EXT_OID: [u64; 9] = [1, 3, 6, 1, 4, 1, 53594, 1, 1];
+
+/// The peer signs the concatenation of the string `libp2p-tls-handshake:`
+/// and the public key that it used to generate the certificate carrying
+/// the libp2p Public Key Extension, using its private host key.
+/// This signature provides cryptographic proof that the peer was
+/// in possession of the private host key at the time the certificate was signed.
+const P2P_SIGNING_PREFIX: [u8; 21] = *b"libp2p-tls-handshake:";
+
 /// Generates a self-signed TLS certificate that includes a libp2p-specific
 /// certificate extension containing the public key of the given keypair.
 pub fn make_certificate(
@@ -50,7 +62,7 @@ pub fn make_certificate(
         // the libp2p Public Key Extension, using its private host key.
         let signature = {
             let mut msg = vec![];
-            msg.extend(*b"libp2p-tls-handshake:");
+            msg.extend(P2P_SIGNING_PREFIX);
             msg.extend(certif_keypair.public_key_der());
 
             keypair.sign(&msg).map_err(|_| rcgen::RcgenError::RingUnspecified)?
@@ -68,13 +80,8 @@ pub fn make_certificate(
             yasna::encode_der(&(serialized_pubkey, signature))
         };
 
-        // The libp2p Public Key Extension is a X.509 extension
-        // with the Object Identier 1.3.6.1.4.1.53594.1.1,
-        // allocated by IANA to the libp2p project at Protocol Labs.
-        let libp2p_ext_oid = &[1, 3, 6, 1, 4, 1, 53594, 1, 1];  // Based on libp2p TLS 1.3 specs
-
         // This extension MAY be marked critical.
-        rcgen::CustomExtension::from_oid_content(libp2p_ext_oid, extension_content)
+        rcgen::CustomExtension::from_oid_content(&P2P_EXT_OID, extension_content)
     };
 
     let certificate = {
@@ -89,15 +96,22 @@ pub fn make_certificate(
     Ok(certificate)
 }
 
-// The contents of the specific libp2p extension, containing the public host key
-// and a signature performed using the private host key
+/// The contents of the specific libp2p extension, containing the public host key
+/// and a signature performed using the private host key.
 pub struct P2pExtension {
     public_key: identity::PublicKey,
+    /// This signature provides cryptographic proof that the peer was
+    /// in possession of the private host key at the time the certificate was signed.
     signature: Vec<u8>,
 }
 
+/// An x.509 certificate with a libp2p-specific extension
+/// is used to secure libp2p connections.
 pub struct P2pCertificate<'a> {
     certificate: X509Certificate<'a>,
+    /// This is a specific libp2p Public Key Extension with two values:
+    /// * the public host key
+    /// * a signature performed using the private host key
     extension: P2pExtension,
 }
 
@@ -115,10 +129,7 @@ pub fn parse_certificate(der_input: &[u8]) -> Result<P2pCertificate, X509Error> 
             }
         })?;
 
-    // The libp2p Public Key Extension is a X.509 extension
-    // with the Object Identier 1.3.6.1.4.1.53594.1.1,
-    // allocated by IANA to the libp2p project at Protocol Labs.
-    let p2p_ext_oid = der_parser::oid::Oid::from(&[1, 3, 6, 1, 4, 1, 53594, 1, 1])
+    let p2p_ext_oid = der_parser::oid::Oid::from(&P2P_EXT_OID)
         .expect("This is a valid OID of p2p extension; qed");
 
     let mut libp2p_extension = None;
@@ -130,8 +141,27 @@ pub fn parse_certificate(der_input: &[u8]) -> Result<P2pCertificate, X509Error> 
         }
 
         if oid == &p2p_ext_oid {
+            // The public host key and the signature are ANS.1-encoded
+            // into the SignedKey data structure, which is carried
+            // in the libp2p Public Key Extension.
+            // SignedKey ::= SEQUENCE {
+            //    publicKey BIT STRING,
+            //    signature BIT STRING
+            // }
             let (public_key, signature): (Vec<u8>, Vec<u8>) = yasna::decode_der(ext.value)
                 .map_err(|_| X509Error::InvalidUserCertificate)?;
+            // The publicKey field of SignedKey contains the public host key
+            // of the endpoint, encoded using the following protobuf:
+            // enum KeyType {
+            //    RSA = 0;
+            //    Ed25519 = 1;
+            //    Secp256k1 = 2;
+            //    ECDSA = 3;
+            // }
+            // message PublicKey {
+            //    required KeyType Type = 1;
+            //    required bytes Data = 2;
+            // }
             let public_key = identity::PublicKey::from_protobuf_encoding(&public_key)
                 .map_err(|_| X509Error::InvalidUserCertificate)?;
             let ext = P2pExtension { public_key, signature};
@@ -154,15 +184,31 @@ pub fn parse_certificate(der_input: &[u8]) -> Result<P2pCertificate, X509Error> 
             extension,
         })
     } else {
+        // The certificate MUST contain the libp2p Public Key Extension.
         // If this extension is missing, endpoints MUST abort the connection attempt.
         Err(X509Error::InvalidCertificate)
     }
 }
 
 impl P2pCertificate<'_> {
+    /// This method validates the certificate according to libp2p TLS 1.3 specs.
+    /// The certificate MUST:
+    /// 1. be valid at the time it is received by the peer;
+    /// 2. be self signed;
+    /// 3. use the NamedCurve encoding;
+    /// 4. use hash functions with an output length not less than 256 bits;
+    /// 5. contain a valid signature in the specific libp2p extension.
     pub fn is_valid(&self) -> bool {
+
+        // The certificate MUST have NotBefore and NotAfter fields set
+        // such that the certificate is valid at the time it is received by the peer.
+        if !self.certificate.validity().is_valid() {
+            return false
+        }
+
         // Endpoints MUST abort the connection attempt if the certificate’s
         // self-signature is not valid.
+        // FIXME: not all algorithms are supported.
         let self_signed = self.certificate.verify_signature(None).is_ok();
         if !self_signed {
             return false
@@ -170,6 +216,10 @@ impl P2pCertificate<'_> {
 
         // Certificates MUST use the NamedCurve encoding for elliptic curve parameters.
         // Endpoints MUST abort the connection attempt if it is not used.
+        // Similarly, hash functions with an output length less than 256 bits
+        // MUST NOT be used, due to the possibility of collision attacks.
+        // In particular, MD5 and SHA1 MUST NOT be used.
+        // FIXME: update https://github.com/rusticata/oid-registry/ and use oids from it
         let alg_oid = self.certificate.signature_algorithm.algorithm.iter();
         if let Some(alg_oid) = alg_oid {
             let signature_algorithm = rcgen::SignatureAlgorithm::from_oid(&alg_oid.collect::<Vec<_>>());
@@ -195,14 +245,10 @@ impl P2pCertificate<'_> {
         } else {
             return false;
         };
-        let params_der = if let Some(params) = &pki.algorithm.parameters {
-            if let Ok(der) = params.to_vec() {
-                Some(der)
-            } else {
-                return false
-            }
-        } else {
-            None
+        let params_der = match pki.algorithm.parameters.as_ref().map(der_parser::ber::BerObject::to_vec) {
+            Some(Ok(der)) => Some(der),
+            Some(Err(_)) => return false,
+            _ => None
         };
         let subject_pki = yasna::construct_der(|writer| {
             writer.write_sequence(|writer| {
@@ -220,7 +266,7 @@ impl P2pCertificate<'_> {
         // and the public key that it used to generate the certificate carrying
         // the libp2p Public Key Extension, using its private host key.
         let mut msg = vec![];
-        msg.extend(*b"libp2p-tls-handshake:");
+        msg.extend(P2P_SIGNING_PREFIX);
         msg.extend(subject_pki);
 
         // This signature provides cryptographic proof that the peer was in possession
