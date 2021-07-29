5d14af5332c22482ad4cef165c2f4c739466587a
Thomas Eizinger <thomas@eizinger.io>
2021-07-29 18:09:38 +1000

    Leave Default implementation of `DummyBehaviour` at `No`
    Making this configurable also allows us to delete some code from the
    relay tests.

--- protocols/ping/tests/ping.rs
+++ protocols/ping/tests/ping.rs
@@ -31,7 +31,7 @@ use libp2p_core::{
 use libp2p_mplex as mplex;
 use libp2p_noise as noise;
 use libp2p_ping::*;
-use libp2p_swarm::{DummyBehaviour, Swarm, SwarmEvent};
+use libp2p_swarm::{DummyBehaviour, KeepAlive, Swarm, SwarmEvent};
 use libp2p_tcp::TcpConfig;
 use libp2p_yamux as yamux;
 use futures::{prelude::*, channel::mpsc};
@@ -192,7 +192,7 @@ fn max_failures() {
 #[test]
 fn unsupported_doesnt_fail() {
     let (peer1_id, trans) = mk_transport(MuxerChoice::Mplex);
-    let mut swarm1 = Swarm::new(trans, DummyBehaviour {}, peer1_id.clone());
+    let mut swarm1 = Swarm::new(trans, DummyBehaviour::with_keep_alive(KeepAlive::Yes), peer1_id.clone());
 
     let (peer2_id, trans) = mk_transport(MuxerChoice::Mplex);
     let mut swarm2 = Swarm::new(trans, Ping::new(PingConfig::new().with_keep_alive(true)), peer2_id.clone());
