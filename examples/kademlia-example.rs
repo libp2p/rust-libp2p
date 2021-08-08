b8cbe621540e72e1fb5592fd52d47e78f7077d20
Tejas Sanap <sanap.tejas@gmail.com>
2021-08-09 01:02:43 +0530

    finally able to discover other peers

--- examples/kademlia-example.rs
+++ examples/kademlia-example.rs
@@ -1,5 +1,8 @@
 #![allow(dead_code, unused_variables)]
 
+// Use the following command to run:
+// cargo run --example kademlia-example --features="tcp-tokio mdns"
+
 // Based on the following example code:
 // https://github.com/zupzup/rust-peer-to-peer-example/blob/main/src/main.rs
 
@@ -10,18 +13,21 @@ use libp2p::{
     mdns::{Mdns, MdnsConfig, MdnsEvent},
     mplex,
     noise::{Keypair, NoiseConfig, X25519Spec},
-    swarm::NetworkBehaviourEventProcess,
+    swarm::{NetworkBehaviourEventProcess, SwarmEvent},
+    // Ignore the unresolved import error here.
     tcp::TokioTcpConfig,
-    NetworkBehaviour, PeerId, Swarm, Transport,
+    NetworkBehaviour,
+    PeerId,
+    Swarm,
+    Transport,
 };
 
-use futures::prelude::*;
-use std::task::{Context, Poll};
-use tokio::runtime::Runtime;
+use futures::StreamExt;
 
 #[derive(NetworkBehaviour)]
 struct MyBehaviour {
     kademlia: Kademlia<MemoryStore>,
+    // Using MDNS for peer discovery.
     mdns: Mdns,
 }
 
@@ -36,12 +42,12 @@ impl NetworkBehaviourEventProcess<KademliaEvent> for MyBehaviour {
         }
     }
 }
+
 impl NetworkBehaviourEventProcess<MdnsEvent> for MyBehaviour {
     fn inject_event(&mut self, event: MdnsEvent) {
         match event {
             MdnsEvent::Discovered(nodes) => {
                 for (peer_id, multiaddr) in nodes {
-                    println!("{}", &peer_id);
                     self.kademlia.add_address(&peer_id, multiaddr);
                 }
             }
@@ -51,9 +57,9 @@ impl NetworkBehaviourEventProcess<MdnsEvent> for MyBehaviour {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> ! {
     let key = identity::Keypair::generate_ed25519();
-    let peer_id = PeerId::from_public_key(key.public());
+    let peer_id = PeerId::from_public_key(&key.public());
 
     let auth_keys = Keypair::<X25519Spec>::new() // create new auth keys
         .into_authentic(&key) // sign the keys
@@ -76,15 +82,38 @@ async fn main() {
         .listen_on("/ip4/0.0.0.0/tcp/0".parse().unwrap())
         .unwrap();
 
-    loop {
-        let mut buffer = String::new();
-        println!("reading");
-        std::io::stdin().read_line(&mut buffer).unwrap();
+    // loop {
+    //     let mut buffer = String::new();
+    //     println!("reading");
+    //     std::io::stdin().read_line(&mut buffer).unwrap();
 
-        if buffer.starts_with("lp") {
-            println!("printing peers");
-            for peer in swarm.behaviour_mut().mdns.discovered_nodes() {
-                println!("{}", peer);
+    //     if buffer.starts_with("lp") {
+    //         let event =
+    //         println!("printing peers");
+    //         for peer in swarm.behaviour_mut().mdns.discovered_nodes() {
+    //             println!("{}", peer);
+    //         }
+    //     }
+    // }
+
+    loop {
+        futures::select! {
+            event = swarm.next() => {
+                match event.unwrap() {
+                    SwarmEvent::NewListenAddr{ address, ..} => println!("Listening on: {}", address),
+                    SwarmEvent::ListenerClosed { listener_id, addresses, reason } => println!("Listening closed on: {:?}", addresses),
+                    SwarmEvent::ConnectionEstablished{ peer_id, endpoint, ..} => println!("Connected to {} on {}",peer_id,endpoint.get_remote_address()),
+                    SwarmEvent::ConnectionClosed { peer_id, endpoint, .. } => println!("Dis-connected to {} on {}",peer_id,endpoint.get_remote_address()),
+                    SwarmEvent::Behaviour(_) => todo!(),
+                    SwarmEvent::IncomingConnection { local_addr, send_back_addr } => todo!(),
+                    SwarmEvent::IncomingConnectionError { local_addr, send_back_addr, error } => todo!(),
+                    SwarmEvent::BannedPeer { peer_id, endpoint } => todo!(),
+                    SwarmEvent::UnreachableAddr { peer_id, address, error, attempts_remaining } => todo!(),
+                    SwarmEvent::UnknownPeerUnreachableAddr { address, error } => todo!(),
+                    SwarmEvent::ExpiredListenAddr { listener_id, address } => todo!(),
+                    SwarmEvent::ListenerError { listener_id, error } => todo!(),
+                    SwarmEvent::Dialing(_) => todo!(),
+                }
             }
         }
     }
