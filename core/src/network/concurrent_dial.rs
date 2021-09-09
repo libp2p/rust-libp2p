6c1ce85f56327355f7b3b79194a7fac4d948b899
Max Inden <mail@max-inden.de>
2021-09-09 13:06:27 +0200

    core/src/: Remove printlns

--- core/src/network/concurrent_dial.rs
+++ core/src/network/concurrent_dial.rs
@@ -1,3 +1,23 @@
+// Copyright 2021 Protocol Labs.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
 pub use crate::connection::{ConnectionCounters, ConnectionLimits};
 
 use crate::{
@@ -38,7 +58,6 @@ impl<TMuxer, TError> ConcurrentDial<TMuxer, TError> {
         TTrans::Dial: Send + 'static,
         TError: std::fmt::Debug,
     {
-        println!("Length of addresses: {}", addresses.len());
         let dials = FuturesUnordered::default();
         let mut errors = vec![];
 
@@ -57,7 +76,6 @@ impl<TMuxer, TError> Future for ConcurrentDial<TMuxer, TError> {
     type Output = Result<(PeerId, Multiaddr, TMuxer), Vec<(Multiaddr, TransportError<TError>)>>;
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-        println!("Lenght of dials: {}", self.dials.len());
         loop {
             match ready!(self.dials.poll_next_unpin(cx)) {
                 // TODO: What about self.errors? Sure we should loose these?
