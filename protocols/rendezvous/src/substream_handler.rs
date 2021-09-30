9a3494e065c25f1bd0a8e9eaf234aa0f0a3e3b89
wngr <oliver@wngr.de>
2021-09-30 22:26:31 +0200

    protocols/rendezvous: replace usage of std::time::instant with libp2p_core::Instant

--- protocols/rendezvous/src/substream_handler.rs
+++ protocols/rendezvous/src/substream_handler.rs
@@ -27,7 +27,7 @@
 
 use futures::future::{self, BoxFuture, Fuse, FusedFuture};
 use futures::FutureExt;
-use libp2p_core::{InboundUpgrade, OutboundUpgrade, UpgradeInfo};
+use libp2p_core::{InboundUpgrade, Instant, OutboundUpgrade, UpgradeInfo};
 use libp2p_swarm::protocols_handler::{InboundUpgradeSend, OutboundUpgradeSend};
 use libp2p_swarm::{
     KeepAlive, NegotiatedSubstream, ProtocolsHandler, ProtocolsHandlerEvent,
@@ -38,7 +38,7 @@ use std::fmt;
 use std::future::Future;
 use std::hash::Hash;
 use std::task::{Context, Poll};
-use std::time::{Duration, Instant};
+use std::time::Duration;
 use void::Void;
 
 /// Handles a substream throughout its lifetime.
