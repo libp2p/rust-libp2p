580f6688fd675c698ca918d44808915fa9bda914
ronzigelman <ronzigelman@gmail.com>
2021-11-04 15:32:30 +0200

    protocols/relay/examples: Structure command line args
    - Allow deterministic peer identity.
    - Choose between ipv6 and ipv4.

--- protocols/relay/examples/relay_v2.rs
+++ protocols/relay/examples/relay_v2.rs
@@ -23,19 +23,26 @@ use futures::executor::block_on;
 use futures::stream::StreamExt;
 use libp2p::core::upgrade;
 use libp2p::identify::{Identify, IdentifyConfig, IdentifyEvent};
-use libp2p::noise;
+use libp2p::multiaddr::Protocol;
 use libp2p::ping::{Ping, PingConfig, PingEvent};
 use libp2p::relay::v2::relay::{self, Relay};
 use libp2p::swarm::{Swarm, SwarmEvent};
 use libp2p::tcp::TcpConfig;
 use libp2p::Transport;
 use libp2p::{identity, NetworkBehaviour, PeerId};
+use libp2p::{noise, Multiaddr};
 use std::error::Error;
+use std::net::{Ipv4Addr, Ipv6Addr};
+use structopt::StructOpt;
 
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let local_key = identity::Keypair::generate_ed25519();
+    let opt = Opt::from_args();
+    println!("opt: {:?}", opt);
+
+    // Create a static known PeerId based on given secret
+    let local_key: identity::Keypair = generate_ed25519(opt.secret_key_seed);
     let local_peer_id = PeerId::from(local_key.public());
     println!("Local peer id: {:?}", local_peer_id);
 
@@ -51,39 +58,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         .multiplex(libp2p_yamux::YamuxConfig::default())
         .boxed();
 
-    #[derive(NetworkBehaviour)]
-    #[behaviour(out_event = "Event", event_process = false)]
-    struct Behaviour {
-        relay: Relay,
-        ping: Ping,
-        identify: Identify,
-    }
-
-    #[derive(Debug)]
-    enum Event {
-        Ping(PingEvent),
-        Identify(IdentifyEvent),
-        Relay(relay::Event),
-    }
-
-    impl From<PingEvent> for Event {
-        fn from(e: PingEvent) -> Self {
-            Event::Ping(e)
-        }
-    }
-
-    impl From<IdentifyEvent> for Event {
-        fn from(e: IdentifyEvent) -> Self {
-            Event::Identify(e)
-        }
-    }
-
-    impl From<relay::Event> for Event {
-        fn from(e: relay::Event) -> Self {
-            Event::Relay(e)
-        }
-    }
-
     let behaviour = Behaviour {
         relay: Relay::new(local_peer_id, Default::default()),
         ping: Ping::new(PingConfig::new()),
@@ -95,8 +69,14 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
 
-    // Listen on all interfaces and whatever port the OS assigns
-    swarm.listen_on("/ip4/0.0.0.0/tcp/4001".parse()?)?;
+    // Listen on all interfaces
+    let listen_addr = Multiaddr::empty()
+        .with(match opt.use_ipv6 {
+            Some(true) => Protocol::from(Ipv6Addr::UNSPECIFIED),
+            _ => Protocol::from(Ipv4Addr::UNSPECIFIED),
+        })
+        .with(Protocol::Tcp(opt.port));
+    swarm.listen_on(listen_addr)?;
 
     block_on(async {
         loop {
@@ -111,4 +91,62 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
     })
+}
+
+#[derive(NetworkBehaviour)]
+#[behaviour(out_event = "Event", event_process = false)]
+struct Behaviour {
+    relay: Relay,
+    ping: Ping,
+    identify: Identify,
+}
+
+#[derive(Debug)]
+enum Event {
+    Ping(PingEvent),
+    Identify(IdentifyEvent),
+    Relay(relay::Event),
+}
+
+impl From<PingEvent> for Event {
+    fn from(e: PingEvent) -> Self {
+        Event::Ping(e)
+    }
+}
+
+impl From<IdentifyEvent> for Event {
+    fn from(e: IdentifyEvent) -> Self {
+        Event::Identify(e)
+    }
+}
+
+impl From<relay::Event> for Event {
+    fn from(e: relay::Event) -> Self {
+        Event::Relay(e)
+    }
+}
+
+fn generate_ed25519(secret_key_seed: u8) -> identity::Keypair {
+    let mut bytes = [0u8; 32];
+    bytes[0] = secret_key_seed;
+
+    let secret_key = identity::ed25519::SecretKey::from_bytes(&mut bytes)
+        .expect("this returns `Err` only if the length is wrong; the length is correct; qed");
+    identity::Keypair::Ed25519(secret_key.into())
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "libp2p relay")]
+struct Opt {
+    /// Determine if the relay listen on ipv6 or ipv4 loopback address. the default is ipv4
+    #[structopt(long)]
+    use_ipv6: Option<bool>,
+
+    /// Fixed value to generate deterministic peer id
+    #[structopt(long)]
+    secret_key_seed: u8,
+
+    /// The port used to listen on all interfaces
+    #[structopt(long)]
+    port: u16,
 }
