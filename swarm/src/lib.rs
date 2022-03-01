f96ffdeb2e362dba5c8b2c4989c38d235d9b8abe
Max Inden <mail@max-inden.de>
2022-03-01 10:32:55 +0100

    Merge master and improve connection count

--- swarm/src/lib.rs
+++ swarm/src/lib.rs
@@ -1583,27 +1583,36 @@ mod tests {
         TBehaviour: NetworkBehaviour,
         <<TBehaviour::ConnectionHandler as IntoConnectionHandler>::Handler as ConnectionHandler>::OutEvent: Clone,
     {
-        [swarm1, swarm2]
-            .iter()
-            .all(|s| s.behaviour.inject_connection_established.len() == num_connections)
+        swarm1
+            .behaviour()
+            .num_connections_to_peer(*swarm2.local_peer_id())
+            == num_connections
+            && swarm2
+                .behaviour()
+                .num_connections_to_peer(*swarm1.local_peer_id())
+                == num_connections
+            && swarm1.is_connected(swarm2.local_peer_id())
+            && swarm2.is_connected(swarm1.local_peer_id())
     }
 
     fn swarms_disconnected<TBehaviour: NetworkBehaviour>(
         swarm1: &Swarm<CallTraceBehaviour<TBehaviour>>,
         swarm2: &Swarm<CallTraceBehaviour<TBehaviour>>,
-        num_connections: usize,
     ) -> bool
     where
         TBehaviour: NetworkBehaviour,
         <<TBehaviour::ConnectionHandler as IntoConnectionHandler>::Handler as ConnectionHandler>::OutEvent: Clone
     {
-        [swarm1, swarm2]
-            .iter()
-            .all(|s| s.behaviour.inject_connection_closed.len() == num_connections)
-            && [swarm1, swarm2].iter().all(|s| {
-                let (.., last_remaining) = s.behaviour.inject_connection_closed.last().unwrap();
-                *last_remaining == 0
-            })
+        swarm1
+            .behaviour()
+            .num_connections_to_peer(*swarm2.local_peer_id())
+            == 0
+            && swarm2
+                .behaviour()
+                .num_connections_to_peer(*swarm1.local_peer_id())
+                == 0
+            && !swarm1.is_connected(swarm2.local_peer_id())
+            && !swarm2.is_connected(swarm1.local_peer_id())
     }
 
     /// Establishes multiple connections between two peers,
@@ -1781,7 +1790,7 @@ mod tests {
                     }
                 }
                 State::Disconnecting => {
-                    if swarms_disconnected(&swarm1, &swarm2, num_connections) {
+                    if swarms_disconnected(&swarm1, &swarm2) {
                         if reconnected {
                             return Poll::Ready(());
                         }
@@ -1839,13 +1848,10 @@ mod tests {
             let poll2 = Swarm::poll_next_event(Pin::new(&mut swarm2), cx);
             match state {
                 State::Connecting => {
-                    println!("Connecting.");
                     if swarms_connected(&swarm1, &swarm2, num_connections) {
                         if reconnected {
-                            println!("Swarms reconnected.");
                             return Poll::Ready(());
                         }
-                        println!("Swarms connected.");
                         swarm2.behaviour.inner().next_action.replace(
                             NetworkBehaviourAction::CloseConnection {
                                 peer_id: swarm1_id,
@@ -1857,12 +1863,7 @@ mod tests {
                     }
                 }
                 State::Disconnecting => {
-                    println!("Disconnecting.");
-                    if swarms_disconnected(&swarm1, &swarm2, num_connections) {
-                        println!("Swarms disconnected.");
-                        if reconnected {
-                            return Poll::Ready(());
-                        }
+                    if swarms_disconnected(&swarm1, &swarm2) {
                         reconnected = true;
                         for _ in 0..num_connections {
                             swarm2.dial(addr1.clone()).unwrap();
