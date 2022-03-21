a4fa867ebb1dbfeabb4bd8057981ea82cd6212d0
Jorjick <68968657+Jerrody@users.noreply.github.com>
2022-03-21 22:30:37 +0200

    Reformatting.

--- examples/gossipsub-chat.rs
+++ examples/gossipsub-chat.rs
@@ -87,7 +87,8 @@ impl NetworkBehaviourEventProcess<MdnsEvent> for Behaviour {
                         match self.sender.try_send(peer_id) {
                             Ok(_) => {
                                 let addresses = self.addresses_of_peer(&peer_id);
-                                self.peers.insert(peer_id, addresses.first().unwrap().to_owned());
+                                self.peers
+                                    .insert(peer_id, addresses.first().unwrap().to_owned());
                             }
                             Err(_) => eprintln!("Failed to send."),
                         }
