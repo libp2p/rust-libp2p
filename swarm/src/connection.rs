70d38520fd38012bf6b8dc73d560dcc0311ecbba
Hubert <hubert@chainsafe.io>
2022-05-03 13:11:48 +0200

    *: Activate `clippy::style` lint group (#2620)

--- swarm/src/connection.rs
+++ swarm/src/connection.rs
@@ -176,7 +176,7 @@ pub struct IncomingInfo<'a> {
 
 impl<'a> IncomingInfo<'a> {
     /// Builds the [`ConnectedPoint`] corresponding to the incoming connection.
-    pub fn to_connected_point(&self) -> ConnectedPoint {
+    pub fn create_connected_point(&self) -> ConnectedPoint {
         ConnectedPoint::Listener {
             local_addr: self.local_addr.clone(),
             send_back_addr: self.send_back_addr.clone(),
