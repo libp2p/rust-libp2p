3b2c7a09cc6ec6677283f9d1af1fb733a014f491
Max Inden <mail@max-inden.de>
2022-05-30 21:41:27 +0200

    src/lib: Fix tokio development transport

--- src/lib.rs
+++ src/lib.rs
@@ -259,9 +259,10 @@ pub fn tokio_development_transport(
     keypair: identity::Keypair,
 ) -> std::io::Result<core::transport::Boxed<(PeerId, core::muxing::StreamMuxerBox)>> {
     let transport = {
-        let tcp = tcp::TokioTcpConfig::new().nodelay(true);
-        let dns_tcp = dns::TokioDnsConfig::system(tcp)?;
-        let ws_dns_tcp = websocket::WsConfig::new(dns_tcp.clone());
+        let dns_tcp = dns::TokioDnsConfig::system(tcp::TokioTcpConfig::new().nodelay(true))?;
+        let ws_dns_tcp = websocket::WsConfig::new(dns::TokioDnsConfig::system(
+            tcp::TokioTcpConfig::new().nodelay(true),
+        )?);
         dns_tcp.or_transport(ws_dns_tcp)
     };
 
