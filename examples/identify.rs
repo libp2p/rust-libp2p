173e82344273f03d0418059c8496a14ca0415bdc
dadepo <dadepo@gmail.com>
2022-06-03 21:10:28 +0200

    Update examples/identify.rs
    Co-authored-by: Max Inden <mail@max-inden.de>

--- examples/identify.rs
+++ examples/identify.rs
@@ -26,7 +26,7 @@
 //! cargo run --example identify
 //! ```
 //! It will print the `PeerId` and the listening addresses, e.g. `Listening on
-//! "/ip4/0.0.0.0/tcp/24915"`
+//! "/ip6/2001:db8:: /tcp/24915"`
 //!
 //! In the second terminal window, start a new instance of the example with:
 //!
