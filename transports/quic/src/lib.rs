e937ce63c20ccbfd333ba51e05842cf3d6ecc1e2
Roman Proskuryakov <r.proskuryakoff@gmail.com>
2022-06-08 16:40:10 +0300

    Remove additional recursion_limit

--- transports/quic/src/lib.rs
+++ transports/quic/src/lib.rs
@@ -18,8 +18,6 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-#![recursion_limit = "1024"]
-
 //! Implementation of the libp2p `Transport` and `StreamMuxer` traits for QUIC.
 //!
 //! # Usage
