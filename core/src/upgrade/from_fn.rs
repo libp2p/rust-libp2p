7e76ab178c1b1dbcfea9a83798c0c8700eaccd62
Thomas Eizinger <thomas@eizinger.io>
2022-08-19 11:46:43 +0200

    Don't use `&str` for `ProtocolName` in docs

--- core/src/upgrade/from_fn.rs
+++ core/src/upgrade/from_fn.rs
@@ -37,7 +37,7 @@ use std::iter;
 /// # use futures::AsyncWriteExt;
 /// let _transport = MemoryTransport::default()
 ///     .and_then(move |out, cp| {
-///         upgrade::apply(out, upgrade::from_fn("/foo/1", move |mut sock: Negotiated<Channel<Vec<u8>>>, endpoint| async move {
+///         upgrade::apply(out, upgrade::from_fn(b"/foo/1", move |mut sock: Negotiated<Channel<Vec<u8>>>, endpoint| async move {
 ///             if endpoint.is_dialer() {
 ///                 upgrade::write_length_prefixed(&mut sock, "some handshake data").await?;
 ///                 sock.close().await?;
