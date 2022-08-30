23c65e74796d6e1b3c85ba5657e998fb765eddeb
Roman Proskuryakov <r.proskuryakoff@gmail.com>
2022-08-30 16:00:21 +0300

    Fix compiler warning

--- protocols/gossipsub/src/behaviour.rs
+++ protocols/gossipsub/src/behaviour.rs
@@ -3203,7 +3203,7 @@ where
             debug!("Peer disconnected: {}", peer_id);
             {
                 let topics = match self.peer_topics.get(peer_id) {
-                    Some(topics) => (topics),
+                    Some(topics) => topics,
                     None => {
                         debug_assert!(
                             self.blacklisted_peers.contains(peer_id),
