18cfb3cada9db93a5177cd52305c21702d6cc6d2
folex <0xdxdy@gmail.com>
2022-09-26 22:15:50 +0400

    Update swarm-derive/src/lib.rs
    Co-authored-by: Max Inden <mail@max-inden.de>

--- swarm-derive/src/lib.rs
+++ swarm-derive/src/lib.rs
@@ -125,7 +125,7 @@ fn build_struct(ast: &DeriveInput, data_struct: &DataStruct) -> TokenStream {
                             )
                             .unwrap();
                             let ty = &field.ty;
-                            quote! {#variant(<#ty as ::libp2p::swarm::NetworkBehaviour>::OutEvent)}
+                            quote! {#variant(<#ty as #trait_to_impl>::OutEvent)}
                         })
                         .collect::<Vec<_>>();
                     let visibility = &ast.vis;
