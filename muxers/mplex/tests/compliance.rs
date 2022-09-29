f8ebb757d7f74273573adabe52ffe15ee344532c
Thomas Eizinger <thomas@eizinger.io>
2022-09-29 15:46:12 +1000

    Add compliance test for read after close

--- muxers/mplex/tests/compliance.rs
+++ muxers/mplex/tests/compliance.rs
@@ -17,3 +17,12 @@ async fn dialer_can_receive() {
 
     libp2p_muxer_test_harness::dialer_can_receive(alice, bob).await;
 }
+
+#[async_std::test]
+async fn read_after_close() {
+    let (alice, bob) =
+        libp2p_muxer_test_harness::connected_muxers_on_memory_transport::<MplexConfig, _, _>()
+            .await;
+
+    libp2p_muxer_test_harness::read_after_close(alice, bob).await;
+}
