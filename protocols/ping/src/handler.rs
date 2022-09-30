68c18ad003a09decf5daa33f451a416d41ff00bb
Thomas Eizinger <thomas@eizinger.io>
2022-09-30 16:39:12 +1000

    Fix clash in naming

--- protocols/ping/src/handler.rs
+++ protocols/ping/src/handler.rs
@@ -185,7 +185,7 @@ pub struct Handler {
     /// Each successful ping resets this counter to 0.
     failures: u32,
     /// The outbound ping state.
-    outbound: Option<State>,
+    outbound: Option<OutboundState>,
     /// The inbound pong handler, i.e. if there is an inbound
     /// substream, this is always a future that waits for the
     /// next inbound ping to be answered.
@@ -241,7 +241,7 @@ impl ConnectionHandler for Handler {
 
     fn inject_fully_negotiated_outbound(&mut self, stream: NegotiatedSubstream, (): ()) {
         self.timer.reset(self.config.timeout);
-        self.outbound = Some(State::Ping(protocol::send_ping(stream).boxed()));
+        self.outbound = Some(OutboundState::Ping(protocol::send_ping(stream).boxed()));
     }
 
     fn inject_event(&mut self, _: Void) {}
@@ -330,19 +330,19 @@ impl ConnectionHandler for Handler {
 
             // Continue outbound pings.
             match self.outbound.take() {
-                Some(State::Ping(mut ping)) => match ping.poll_unpin(cx) {
+                Some(OutboundState::Ping(mut ping)) => match ping.poll_unpin(cx) {
                     Poll::Pending => {
                         if self.timer.poll_unpin(cx).is_ready() {
                             self.pending_errors.push_front(Failure::Timeout);
                         } else {
-                            self.outbound = Some(State::Ping(ping));
+                            self.outbound = Some(OutboundState::Ping(ping));
                             break;
                         }
                     }
                     Poll::Ready(Ok((stream, rtt))) => {
                         self.failures = 0;
                         self.timer.reset(self.config.interval);
-                        self.outbound = Some(State::Idle(stream));
+                        self.outbound = Some(OutboundState::Idle(stream));
                         return Poll::Ready(ConnectionHandlerEvent::Custom(Ok(Success::Ping {
                             rtt,
                         })));
@@ -352,22 +352,23 @@ impl ConnectionHandler for Handler {
                             .push_front(Failure::Other { error: Box::new(e) });
                     }
                 },
-                Some(State::Idle(stream)) => match self.timer.poll_unpin(cx) {
+                Some(OutboundState::Idle(stream)) => match self.timer.poll_unpin(cx) {
                     Poll::Pending => {
-                        self.outbound = Some(State::Idle(stream));
+                        self.outbound = Some(OutboundState::Idle(stream));
                         break;
                     }
                     Poll::Ready(()) => {
                         self.timer.reset(self.config.timeout);
-                        self.outbound = Some(State::Ping(protocol::send_ping(stream).boxed()));
+                        self.outbound =
+                            Some(OutboundState::Ping(protocol::send_ping(stream).boxed()));
                     }
                 },
-                Some(State::OpenStream) => {
-                    self.outbound = Some(State::OpenStream);
+                Some(OutboundState::OpenStream) => {
+                    self.outbound = Some(OutboundState::OpenStream);
                     break;
                 }
                 None => {
-                    self.outbound = Some(State::OpenStream);
+                    self.outbound = Some(OutboundState::OpenStream);
                     let protocol = SubstreamProtocol::new(ReadyUpgrade::new(PROTOCOL_NAME), ())
                         .with_timeout(self.config.timeout);
                     return Poll::Ready(ConnectionHandlerEvent::OutboundSubstreamRequest {
@@ -385,7 +386,7 @@ type PingFuture = BoxFuture<'static, Result<(NegotiatedSubstream, Duration), io:
 type PongFuture = BoxFuture<'static, Result<NegotiatedSubstream, io::Error>>;
 
 /// The current state w.r.t. outbound pings.
-enum State {
+enum OutboundState {
     /// A new substream is being negotiated for the ping protocol.
     OpenStream,
     /// The substream is idle, waiting to send the next ping.
