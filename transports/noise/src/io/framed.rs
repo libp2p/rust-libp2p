84ca840e9381ddfadccfe11a423b1709d032809a
Thomas Eizinger <thomas@eizinger.io>
2022-10-04 08:47:17 +1100

    Use `?` for better indentation

--- transports/noise/src/io/framed.rs
+++ transports/noise/src/io/framed.rs
@@ -101,21 +101,17 @@ impl<T> NoiseFramed<T, snow::HandshakeState> {
                 Ok(dh_pk) => Some(dh_pk),
             },
         };
-        match self.session.into_transport_mode() {
-            Err(e) => Err(e.into()),
-            Ok(s) => {
-                let io = NoiseFramed {
-                    session: s,
-                    io: self.io,
-                    read_state: ReadState::Ready,
-                    write_state: WriteState::Ready,
-                    read_buffer: self.read_buffer,
-                    write_buffer: self.write_buffer,
-                    decrypt_buffer: self.decrypt_buffer,
-                };
-                Ok((dh_remote_pubkey, NoiseOutput::new(io)))
-            }
-        }
+        let io = NoiseFramed {
+            session: self.session.into_transport_mode()?,
+            io: self.io,
+            read_state: ReadState::Ready,
+            write_state: WriteState::Ready,
+            read_buffer: self.read_buffer,
+            write_buffer: self.write_buffer,
+            decrypt_buffer: self.decrypt_buffer,
+        };
+
+        Ok((dh_remote_pubkey, NoiseOutput::new(io)))
     }
 }
 
