f834d9e0ab5709ec6208351593a107a7e525709b
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-10-10 21:37:09 +0200

    Adjusted example to match the ping example

--- examples/ping-onion.rs
+++ examples/ping-onion.rs
@@ -45,9 +45,10 @@
 
 use futures::prelude::*;
 use libp2p::onion::AddressConversion;
-use libp2p::swarm::{Swarm, SwarmEvent};
+use libp2p::swarm::{Swarm, SwarmEvent, keep_alive};
 use libp2p::{
     core::upgrade, identity, mplex, noise, onion, ping, yamux, Multiaddr, PeerId, Transport,
+    NetworkBehaviour
 };
 use std::error::Error;
 
@@ -83,17 +84,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let local_peer_id = PeerId::from(local_key.public());
     println!("Local peer id: {:?}", local_peer_id);
 
-    // create a transport
     let transport = onion_transport(local_key).await?;
 
-    // Create a ping network behaviour.
-    //
-    // For illustrative purposes, the ping protocol is configured to
-    // keep the connection alive, so a continuous sequence of pings
-    // can be observed.
-    let behaviour = ping::Behaviour::new(ping::Config::new().with_keep_alive(true));
-
-    let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
+    let mut swarm = Swarm::new(transport, Behaviour::default(), local_peer_id);
 
     // Dial the peer identified by the multi-address given as the second
     // command-line argument, if any.
@@ -114,4 +107,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
             _ => {}
         }
     }
+}
+
+/// Our network behaviour.
+///
+/// For illustrative purposes, this includes the [`KeepAlive`](behaviour::KeepAlive) behaviour so a continuous sequence of
+/// pings can be observed.
+#[derive(NetworkBehaviour, Default)]
+struct Behaviour {
+    keep_alive: keep_alive::Behaviour,
+    ping: ping::Behaviour,
 }
