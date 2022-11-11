0720debe58c93f48a6641fd998585045b236cf7c
dignifiedquire <me@dignifiedquire.com>
2022-11-11 20:31:29 +0100

    fix example

--- examples/distributed-key-value-store.rs
+++ examples/distributed-key-value-store.rs
@@ -172,6 +172,57 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     _ => {}
                 }
             }
+            SwarmEvent::Behaviour(MyBehaviourEvent::Kademlia(KademliaEvent::OutboundQueryProgressed { result, ..})) => {
+                match result {
+                    QueryResult::GetProviders(Ok(GetProvidersOk { key, providers, .. })) => {
+                        for peer in providers {
+                            println!(
+                                "Peer {:?} provides key {:?}",
+                                peer,
+                                std::str::from_utf8(key.as_ref()).unwrap()
+                            );
+                        }
+                    }
+                    QueryResult::GetProviders(Err(err)) => {
+                        eprintln!("Failed to get providers: {:?}", err);
+                    }
+                    QueryResult::GetRecord(Ok(ok)) => {
+                        for PeerRecord {
+                            record: Record { key, value, .. },
+                            ..
+                        } in ok.records
+                        {
+                            println!(
+                                "Got record {:?} {:?}",
+                                std::str::from_utf8(key.as_ref()).unwrap(),
+                                std::str::from_utf8(&value).unwrap(),
+                            );
+                        }
+                    }
+                    QueryResult::GetRecord(Err(err)) => {
+                        eprintln!("Failed to get record: {:?}", err);
+                    }
+                    QueryResult::PutRecord(Ok(PutRecordOk { key })) => {
+                        println!(
+                            "Successfully put record {:?}",
+                            std::str::from_utf8(key.as_ref()).unwrap()
+                        );
+                    }
+                    QueryResult::PutRecord(Err(err)) => {
+                        eprintln!("Failed to put record: {:?}", err);
+                    }
+                    QueryResult::StartProviding(Ok(AddProviderOk { key })) => {
+                        println!(
+                            "Successfully put provider record {:?}",
+                            std::str::from_utf8(key.as_ref()).unwrap()
+                        );
+                    }
+                    QueryResult::StartProviding(Err(err)) => {
+                        eprintln!("Failed to put provider record: {:?}", err);
+                    }
+                    _ => {}
+                }
+            }
             _ => {}
         }
         }
@@ -191,7 +242,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         }
                     }
                 };
-                kademlia.get_record(key, Quorum::One);
+                kademlia.get_record(key);
             }
             Some("GET_PROVIDERS") => {
                 let key = {
