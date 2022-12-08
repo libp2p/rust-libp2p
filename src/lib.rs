a2cc5418755b76bdc49338ae0fde01b11bea2f65
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-12-09 00:08:41 +0100

    Remove unnecessary change

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
+//! libp2p is a modular peer-to-peer networking framework.
 //! To learn more about the general libp2p multi-language framework visit
 //! [libp2p.io](https://libp2p.io/).
 //!
