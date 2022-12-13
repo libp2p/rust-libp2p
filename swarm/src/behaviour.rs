2e3aa5805b2571b45ca38341eb26dbfc4c338185
Thomas Eizinger <thomas@eizinger.io>
2022-12-13 14:38:04 +1100

    Add changelog entry

--- swarm/src/behaviour.rs
+++ swarm/src/behaviour.rs
@@ -408,21 +408,21 @@ pub trait PollParameters {
 
     /// Returns the list of the addresses we're listening on.
     #[deprecated(
-        since = "0.41.0",
+        since = "0.42.0",
         note = "Use `libp2p_swarm::ListenAddresses` instead."
     )]
     fn listened_addresses(&self) -> Self::ListenedAddressesIter;
 
     /// Returns the list of the addresses nodes can use to reach us.
     #[deprecated(
-        since = "0.41.0",
+        since = "0.42.0",
         note = "Use `libp2p_swarm::ExternalAddresses` instead."
     )]
     fn external_addresses(&self) -> Self::ExternalAddressesIter;
 
     /// Returns the peer id of the local node.
     #[deprecated(
-        since = "0.41.0",
+        since = "0.42.0",
         note = "Pass the node's `PeerId` into the behaviour instead."
     )]
     fn local_peer_id(&self) -> &PeerId;
