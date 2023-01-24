d85582cefbfac0ddea6b1caaea35c79e22351f49
Thomas Eizinger <thomas@eizinger.io>
2023-01-24 16:14:47 +1100

    Remove unnecessary docs
    These docs will never render because this code is only available
    without any features and we build docs with all features.

--- identity/src/keypair_dummy.rs
+++ identity/src/keypair_dummy.rs
@@ -20,45 +20,22 @@
 
 use crate::error::{DecodingError, SigningError};
 
-/// Identity keypair of a node.
-///
-/// # Example: Generating RSA keys with OpenSSL
-///
-/// ```text
-/// openssl genrsa -out private.pem 2048
-/// openssl pkcs8 -in private.pem -inform PEM -topk8 -out private.pk8 -outform DER -nocrypt
-/// rm private.pem      # optional
-/// ```
-///
-/// Loading the keys:
-///
-/// ```text
-/// let mut bytes = std::fs::read("private.pk8").unwrap();
-/// let keypair = Keypair::rsa_from_pkcs8(&mut bytes);
-/// ```
-///
 #[derive(Debug, Clone)]
-#[allow(clippy::large_enum_variant)]
 pub enum Keypair {}
 
 impl Keypair {
-    /// Sign a message using the private key of this keypair, producing
-    /// a signature that can be verified using the corresponding public key.
     pub fn sign(&self, _: &[u8]) -> Result<Vec<u8>, SigningError> {
         unreachable!("Can never construct empty enum")
     }
 
-    /// Get the public key of this keypair.
     pub fn public(&self) -> PublicKey {
         unreachable!("Can never construct empty enum")
     }
 
-    /// Encode a private key as protobuf structure.
     pub fn to_protobuf_encoding(&self) -> Result<Vec<u8>, DecodingError> {
         unreachable!("Can never construct empty enum")
     }
 
-    /// Decode a private key from a protobuf structure and parse it as a [`Keypair`].
     pub fn from_protobuf_encoding(_: &[u8]) -> Result<Keypair, DecodingError> {
         Err(DecodingError::missing_feature(
             "ecdsa|rsa|ed25519|secp256k1",
@@ -66,35 +43,25 @@ impl Keypair {
     }
 }
 
-/// The public key of a node's identity keypair.
 #[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum PublicKey {}
 
 impl PublicKey {
-    /// Verify a signature for a message using this public key, i.e. check
-    /// that the signature has been produced by the corresponding
-    /// private key (authenticity), and that the message has not been
-    /// tampered with (integrity).
     #[must_use]
     pub fn verify(&self, _: &[u8], _: &[u8]) -> bool {
         unreachable!("Can never construct empty enum")
     }
 
-    /// Encode the public key into a protobuf structure for storage or
-    /// exchange with other nodes.
     pub fn to_protobuf_encoding(&self) -> Vec<u8> {
         unreachable!("Can never construct empty enum")
     }
 
-    /// Decode a public key from a protobuf structure, e.g. read from storage
-    /// or received from another node.
     pub fn from_protobuf_encoding(_: &[u8]) -> Result<PublicKey, DecodingError> {
         Err(DecodingError::missing_feature(
             "ecdsa|rsa|ed25519|secp256k1",
         ))
     }
 
-    /// Convert the `PublicKey` into the corresponding `PeerId`.
     #[cfg(feature = "peerid")]
     pub fn to_peer_id(&self) -> crate::PeerId {
         unreachable!("Can never construct empty enum")
