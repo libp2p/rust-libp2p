e2b3c1190a2a06fa0db78b558f2fbb24780115e7
Fina <code@felinira.net>
2023-01-27 05:02:20 +0100

    fix(mdns): Don't expire mDNS nodes on connection close (#3367)
    mDNS records should not be expiring when an unrelated connection timeout with said peer is reached.
    
    Fixes #3309.

--- protocols/mdns/CHANGELOG.md
+++ protocols/mdns/CHANGELOG.md
@@ -6,7 +6,12 @@
 
 - Update to `libp2p-swarm` `v0.42.0`.
 
+- Don't expire mDNS records when the last connection was closed.
+  mDNS records will only be expired when the TTL is reached and the DNS record is no longer valid.
+  See [PR 3367].
+
 [PR 3153]: https://github.com/libp2p/rust-libp2p/pull/3153
+[PR 3367]: https://github.com/libp2p/rust-libp2p/pull/3367
 
 # 0.42.0
 
--- protocols/mdns/src/behaviour.rs
+++ protocols/mdns/src/behaviour.rs
@@ -28,7 +28,7 @@ use crate::Config;
 use futures::Stream;
 use if_watch::IfEvent;
 use libp2p_core::{Multiaddr, PeerId};
-use libp2p_swarm::behaviour::{ConnectionClosed, FromSwarm};
+use libp2p_swarm::behaviour::FromSwarm;
 use libp2p_swarm::{
     dummy, ListenAddresses, NetworkBehaviour, NetworkBehaviourAction, PollParameters,
     THandlerOutEvent,
@@ -199,22 +199,14 @@ where
         self.listen_addresses.on_swarm_event(&event);
 
         match event {
-            FromSwarm::ConnectionClosed(ConnectionClosed {
-                peer_id,
-                remaining_established,
-                ..
-            }) => {
-                if remaining_established == 0 {
-                    self.expire_node(&peer_id);
-                }
-            }
             FromSwarm::NewListener(_) => {
                 log::trace!("waking interface state because listening address changed");
                 for iface in self.iface_states.values_mut() {
                     iface.fire_timer();
                 }
             }
-            FromSwarm::ConnectionEstablished(_)
+            FromSwarm::ConnectionClosed(_)
+            | FromSwarm::ConnectionEstablished(_)
             | FromSwarm::DialFailure(_)
             | FromSwarm::AddressChange(_)
             | FromSwarm::ListenFailure(_)
