f81d14125d15080603efc7a3e6e098de7cbfda61
StemCll <lydjotj6f@mozmail.com>
2023-02-19 18:43:43 +0100

    Minor fix in error deprecated note

--- protocols/gossipsub/src/error.rs
+++ protocols/gossipsub/src/error.rs
@@ -20,13 +20,13 @@
 
 #[deprecated(
     since = "0.44.0",
-    note = "Use `libp2p::gossipsub::PublishError` instead, as the `error` will become crate-private in the future."
+    note = "Use `libp2p::gossipsub::PublishError` instead, as the `error` module will become crate-private in the future."
 )]
 pub type PublishError = crate::error_priv::PublishError;
 
 #[deprecated(
     since = "0.44.0",
-    note = "Use `libp2p::gossipsub::SubscriptionError` instead, as the `error` will become crate-private in the future."
+    note = "Use `libp2p::gossipsub::SubscriptionError` instead, as the `error` module will become crate-private in the future."
 )]
 pub type SubscriptionError = crate::error_priv::SubscriptionError;
 
@@ -38,12 +38,12 @@ pub type GossipsubHandlerError = crate::error_priv::HandlerError;
 
 #[deprecated(
     since = "0.44.0",
-    note = "Use `libp2p::gossipsub::HandlerError` instead, as the `error` will become crate-private in the future."
+    note = "Use `libp2p::gossipsub::HandlerError` instead, as the `error` module will become crate-private in the future."
 )]
 pub type HandlerError = crate::error_priv::HandlerError;
 
 #[deprecated(
     since = "0.44.0",
-    note = "Use `libp2p::gossipsub::ValidationError` instead, as the `error` will become crate-private in the future."
+    note = "Use `libp2p::gossipsub::ValidationError` instead, as the `error` module will become crate-private in the future."
 )]
 pub type ValidationError = crate::error_priv::ValidationError;
