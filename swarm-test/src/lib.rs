cd93320dddd9f918465ada0ff46b358ab63bb419
Thomas Eizinger <thomas@eizinger.io>
2023-03-02 14:55:07 +1100

    Fix clippy

--- swarm-test/src/lib.rs
+++ swarm-test/src/lib.rs
@@ -143,17 +143,11 @@ where
     let res2_len = res2.len();
 
     (
-        res1.try_into().unwrap_or_else(|| {
-            panic!(
-                "expected {NUM_EVENTS_SWARM_1} items from first swarm but got {}",
-                res1_len
-            )
+        res1.try_into().unwrap_or_else(|_| {
+            panic!("expected {NUM_EVENTS_SWARM_1} items from first swarm but got {res1_len}",)
         }),
-        res2.try_into().unwrap_or_else(|| {
-            panic!(
-                "expected {NUM_EVENTS_SWARM_2} items from second swarm but got {}",
-                res2_len
-            )
+        res2.try_into().unwrap_or_else(|_| {
+            panic!("expected {NUM_EVENTS_SWARM_2} items from second swarm but got {res2_len}",)
         }),
     )
 }
