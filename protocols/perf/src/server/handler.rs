d0e96d2db6a270a8350664bebdfa94818b6880ff
Max Inden <mail@max-inden.de>
2023-03-07 15:19:32 +0100

    Use Void for server handler error

--- protocols/perf/src/server/handler.rs
+++ protocols/perf/src/server/handler.rs
@@ -65,7 +65,7 @@ impl Default for Handler {
 impl ConnectionHandler for Handler {
     type InEvent = Void;
     type OutEvent = Event;
-    type Error = std::io::Error;
+    type Error = Void;
     type InboundProtocol = ReadyUpgrade<&'static [u8]>;
     type OutboundProtocol = DeniedUpgrade;
     type OutboundOpenInfo = Void;
