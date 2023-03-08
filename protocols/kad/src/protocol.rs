984ebe9a95ecf4bd885927658f16929edd457209
Thomas Eizinger <thomas@eizinger.io>
2023-03-08 11:04:18 +0100

    Fix bad merge

--- protocols/kad/src/protocol.rs
+++ protocols/kad/src/protocol.rs
@@ -443,7 +443,7 @@ fn proto_to_req_msg(message: proto::Message) -> Result<KadRequestMsg, io::Error>
     match message.type_pb {
         proto::MessageType::PING => Ok(KadRequestMsg::Ping),
         proto::MessageType::PUT_VALUE => {
-            let record = record_from_proto(message.record)?;
+            let record = record_from_proto(message.record.unwrap_or_default())?;
             Ok(KadRequestMsg::PutValue { record })
         }
         proto::MessageType::GET_VALUE => Ok(KadRequestMsg::GetValue {
