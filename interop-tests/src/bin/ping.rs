3191b6d6da5ebabb588b41fa017f7cdcb1f9c06f
Thomas Eizinger <thomas@eizinger.io>
2023-03-15 22:35:58 +0100

    Fix fmt

--- interop-tests/src/bin/ping.rs
+++ interop-tests/src/bin/ping.rs
@@ -13,8 +13,8 @@ use libp2p::swarm::{keep_alive, NetworkBehaviour, SwarmEvent};
 use libp2p::tls::TlsStream;
 use libp2p::websocket::WsConfig;
 use libp2p::{
-    identity, mplex, noise, ping, swarm::SwarmBuilder, tcp, tls, yamux,
-    InboundUpgradeExt, Multiaddr, OutboundUpgradeExt, PeerId, Transport as _,
+    identity, mplex, noise, ping, swarm::SwarmBuilder, tcp, tls, yamux, InboundUpgradeExt,
+    Multiaddr, OutboundUpgradeExt, PeerId, Transport as _,
 };
 use libp2p_quic as quic;
 use libp2p_webrtc as webrtc;
