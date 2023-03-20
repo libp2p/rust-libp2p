bff4279a87cb46f8dc6c8b04e2ddcba43230839d
Max Inden <mail@max-inden.de>
2023-03-20 14:43:51 +0100

    Add ping latency benchmark

--- protocols/perf/src/bin/perf-client.rs
+++ protocols/perf/src/bin/perf-client.rs
@@ -27,7 +27,7 @@ use instant::Instant;
 use libp2p_core::{muxing::StreamMuxerBox, transport::OrTransport, upgrade, Multiaddr, Transport};
 use libp2p_dns::DnsConfig;
 use libp2p_identity::PeerId;
-use libp2p_perf::client::RunParams;
+use libp2p_perf::client::{RunParams, RunStats, RunTimers};
 use libp2p_swarm::{Swarm, SwarmBuilder, SwarmEvent};
 use log::info;
 
@@ -44,7 +44,8 @@ async fn main() -> Result<()> {
 
     let opts = Opts::parse();
 
-    let benchmarks: [Box<dyn Benchmark>; 3] = [
+    let benchmarks: [Box<dyn Benchmark>; 4] = [
+        Box::new(Latency {}),
         Box::new(Throughput {}),
         Box::new(RequestsPerSecond {}),
         Box::new(ConnectionsPerSecond {}),
@@ -67,6 +68,74 @@ trait Benchmark {
     fn name(&self) -> &'static str;
 
     async fn run(&self, server_address: Multiaddr) -> Result<()>;
+}
+
+struct Latency {}
+
+#[async_trait]
+impl Benchmark for Latency {
+    fn name(&self) -> &'static str {
+        "round-trip-time latency"
+    }
+
+    async fn run(&self, server_address: Multiaddr) -> Result<()> {
+        let mut swarm = swarm().await;
+
+        let server_peer_id = connect(&mut swarm, server_address.clone()).await?;
+
+        let num = 1_000;
+        let mut latencies = Vec::new();
+
+        for _ in 0..num {
+            swarm.behaviour_mut().perf(
+                server_peer_id,
+                RunParams {
+                    to_send: 1,
+                    to_receive: 1,
+                },
+            )?;
+
+            let latency = loop {
+                match swarm.next().await.unwrap() {
+                    SwarmEvent::ConnectionEstablished {
+                        peer_id, endpoint, ..
+                    } => {
+                        info!("Established connection to {:?} via {:?}", peer_id, endpoint);
+                    }
+                    SwarmEvent::OutgoingConnectionError { peer_id, error } => {
+                        info!("Outgoing connection error to {:?}: {:?}", peer_id, error);
+                    }
+                    SwarmEvent::Behaviour(libp2p_perf::client::Event {
+                        id: _,
+                        result:
+                            Ok(RunStats {
+                                timers:
+                                    RunTimers {
+                                        write_start,
+                                        read_done,
+                                        ..
+                                    },
+                                ..
+                            }),
+                    }) => break read_done.duration_since(write_start).as_secs_f64(),
+                    e => panic!("{e:?}"),
+                };
+            };
+            latencies.push(latency);
+        }
+
+        latencies.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+        info!("Finished: {num} pings",);
+        info!("- {:.4} s median", percentile(&latencies, 0.50),);
+        info!("- {:.4} s 95th percentile\n", percentile(&latencies, 0.95),);
+        Ok(())
+    }
+}
+
+fn percentile<V: PartialOrd + Copy>(values: &[V], percentile: f64) -> V {
+    let n: usize = (values.len() as f64 * percentile).ceil() as usize - 1;
+    values[n]
 }
 
 struct Throughput {}
@@ -109,20 +178,19 @@ impl Benchmark for Throughput {
 
         let sent_mebibytes = stats.params.to_send as f64 / 1024.0 / 1024.0;
         let sent_time = (stats.timers.write_done - stats.timers.write_start).as_secs_f64();
-        let sent_bandwidth_mebibit_second =
-            format!("{:.2} MiBit/s", (sent_mebibytes * 8.0) / sent_time).bold();
+        let sent_bandwidth_mebibit_second = (sent_mebibytes * 8.0) / sent_time;
 
         let received_mebibytes = stats.params.to_receive as f64 / 1024.0 / 1024.0;
         let receive_time = (stats.timers.read_done - stats.timers.write_done).as_secs_f64();
-        let receive_bandwidth_mebibit_second =
-            format!("{:.2} MiBit/s", (received_mebibytes * 8.0) / receive_time).bold();
+        let receive_bandwidth_mebibit_second = (received_mebibytes * 8.0) / receive_time;
 
         info!(
-            "Finished: sent {sent_mebibytes:.2} MiB in {sent_time:.2} s with \
-         {sent_bandwidth_mebibit_second} and received \
-         {received_mebibytes:.2} MiB in {receive_time:.2} s with \
-         {receive_bandwidth_mebibit_second}\n",
+            "Finished: sent {sent_mebibytes:.2} MiB in {sent_time:.2} s \
+             and received {received_mebibytes:.2} MiB in {receive_time:.2} s",
         );
+        info!("- {sent_bandwidth_mebibit_second:.2}");
+        info!("- {receive_bandwidth_mebibit_second:.2}\n");
+
         Ok(())
     }
 }
@@ -182,11 +250,12 @@ impl Benchmark for RequestsPerSecond {
         }
 
         let duration = start.elapsed().as_secs_f64();
-        let requests_per_second = format!("{:.2} req/s", num as f64 / duration).bold();
+        let requests_per_second = num as f64 / duration;
 
         info!(
-        "Finished: sent {num} {to_send} bytes requests with {to_receive} bytes response each within {duration:.2} s thus {requests_per_second}\n",
-    );
+            "Finished: sent {num} {to_send} bytes requests with {to_receive} bytes response each within {duration:.2} s",
+        );
+        info!("- {requests_per_second:.2} req/s\n");
 
         Ok(())
     }
@@ -205,12 +274,18 @@ impl Benchmark for ConnectionsPerSecond {
         let to_send = 1;
         let to_receive = 1;
         let start = Instant::now();
-        let mut finished = 0;
+
+        let mut latency_connection_establishment = Vec::new();
+        let mut latency_connection_establishment_plus_request = Vec::new();
 
         for _ in 0..num {
             let mut swarm = swarm().await;
 
+            let start = Instant::now();
+
             let server_peer_id = connect(&mut swarm, server_address.clone()).await?;
+
+            latency_connection_establishment.push(start.elapsed().as_secs_f64());
 
             swarm.behaviour_mut().perf(
                 server_peer_id,
@@ -221,34 +296,32 @@ impl Benchmark for ConnectionsPerSecond {
             )?;
 
             match swarm.next().await.unwrap() {
-                SwarmEvent::ConnectionEstablished {
-                    peer_id, endpoint, ..
-                } => {
-                    info!("Established connection to {:?} via {:?}", peer_id, endpoint);
-                }
-                SwarmEvent::OutgoingConnectionError { peer_id, error } => {
-                    info!("Outgoing connection error to {:?}: {:?}", peer_id, error);
-                }
                 SwarmEvent::Behaviour(libp2p_perf::client::Event {
                     id: _,
                     result: Ok(_),
-                }) => {
-                    finished += 1;
-
-                    if finished == num {
-                        break;
-                    }
-                }
+                }) => {}
                 e => panic!("{e:?}"),
             };
+
+            latency_connection_establishment_plus_request.push(start.elapsed().as_secs_f64())
         }
 
         let duration = start.elapsed().as_secs_f64();
-        let connections_per_second = format!("{:.2} conns/s", num as f64 / duration).bold();
+        let connections_per_second = num as f64 / duration;
+
+        latency_connection_establishment.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        latency_connection_establishment_plus_request.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+        let connection_establishment_95th = percentile(&latency_connection_establishment, 0.95);
+        let connection_establishment_plus_request_95th =
+            percentile(&latency_connection_establishment_plus_request, 0.95);
 
         info!(
-        "Finished: established {num} connections with one {to_send} bytes request with {to_receive} bytes response each within {duration:.2} s thus {connections_per_second}\n",
-    );
+            "Finished: established {num} connections with one {to_send} bytes request and one {to_receive} bytes response within {duration:.2} s",
+        );
+        info!("- {connections_per_second:.2} conns/s");
+        info!("- {connection_establishment_95th:.4} s 95th percentile connection establishment");
+        info!("- {connection_establishment_plus_request_95th:.4} s 95th percentile connection establishment + one request");
 
         Ok(())
     }
