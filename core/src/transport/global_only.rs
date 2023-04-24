5ab87b471b030ec534fe6956f841e03caafd124e
Thomas Coratger <thomas.coratger@gmail.com>
2023-04-25 00:07:04 +0200

    add is_global for ipv6

--- core/src/transport/global_only.rs
+++ core/src/transport/global_only.rs
@@ -25,7 +25,7 @@ use crate::{
 };
 use log::debug;
 use std::{
-    net::Ipv4Addr,
+    net::{Ipv4Addr, Ipv6Addr},
     pin::Pin,
     task::{Context, Poll},
 };
@@ -37,6 +37,7 @@ pub struct GlobalIpOnly<T> {
 }
 
 pub struct Ipv4Global;
+pub struct Ipv6Global;
 
 impl Ipv4Global {
     /// Returns [`true`] if this address part of the `0.0.0.0/8` range.
@@ -173,6 +174,122 @@ impl Ipv4Global {
     }
 }
 
+impl Ipv6Global {
+    /// Returns [`true`] if this address is `::ffff:0:0/96`.
+    pub const fn is_ipv4_mapped_address(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0, 0, 0, 0, 0, 0xffff, _, _])
+    }
+
+    /// Returns [`true`] if this address is `64:ff9b::/96` or `64:ff9b:1::/48`.
+    pub const fn is_ipv4_ipv6_translat(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x64, 0xff9b, 0, 0, 0, 0, _, _])
+            || matches!(a.segments(), [0x64, 0xff9b, 1, _, _, _, _, _])
+    }
+
+    /// Returns [`true`] if this address is `100::/64`.
+    pub const fn is_discard_only_address_block(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x100, 0, 0, 0, _, _, _, _])
+    }
+
+    /// Returns [`true`] if this address is `2001::/23`.
+    pub const fn is_ietf_protocol_assignments(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x2001, b, _, _, _, _, _, _] if b <= 0x1FF)
+    }
+
+    /// Returns [`true`] if this address is `2001::/32`.
+    pub const fn is_teredo(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x2001, 0, _, _, _, _, _, _])
+    }
+
+    /// Returns [`true`] if this address is `2001:1::1/128`.
+    pub const fn is_port_control_protocol_anycast(a: Ipv6Addr) -> bool {
+        u128::from_be_bytes(a.octets()) == 0x2001_0001_0000_0000_0000_0000_0000_0001
+    }
+
+    /// Returns [`true`] if this address is `2001:1::2/128`.
+    pub const fn is_traversal_relays_nat_anycast(a: Ipv6Addr) -> bool {
+        u128::from_be_bytes(a.octets()) == 0x2001_0001_0000_0000_0000_0000_0000_0002
+    }
+
+    /// Returns [`true`] if this address is `2001:2::/48`.
+    pub const fn is_benchmarking(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x2001, 2, 0, _, _, _, _, _])
+    }
+
+    /// Returns [`true`] if this address is `2001:3::/32`.
+    pub const fn is_amt(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x2001, 3, _, _, _, _, _, _])
+    }
+
+    /// Returns [`true`] if this address is `2001:4:112::/48`.
+    pub const fn is_as112_v6(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x2001, 4, 0x112, _, _, _, _, _])
+    }
+
+    /// Returns [`true`] if this address is `2001:10::/28`.
+    pub const fn is_deprecated_previously_orchid(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x2001, b, _, _, _, _, _, _] if b >= 0x10 && b <= 0x1F)
+    }
+
+    /// Returns [`true`] if this address is `2001:20::/28`.
+    pub const fn is_orchid_v2(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x2001, b, _, _, _, _, _, _] if b >= 0x20 && b <= 0x2F)
+    }
+
+    /// Returns [`true`] if this address is `2001:30::/28`.
+    pub const fn is_drone_remote(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x2001, b, _, _, _, _, _, _] if b >= 0x30 && b <= 0x3F)
+    }
+
+    /// Returns [`true`] if this address is `2001:db8::/32`.
+    pub const fn is_documentation(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x2001, 0xdb8, _, _, _, _, _, _])
+    }
+
+    /// Returns [`true`] if this address is `2002::/16`.
+    pub const fn is_6to4(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x2002, _, _, _, _, _, _, _])
+    }
+
+    /// Returns [`true`] if this address is `2620:4f:8000::/48`.
+    pub const fn is_direct_delegation_as112_service(a: Ipv6Addr) -> bool {
+        matches!(a.segments(), [0x2620, 0x4f, 0x8000, _, _, _, _, _])
+    }
+
+    /// Returns [`true`] if this address is `fc00::/7`.
+    pub const fn is_unique_local(a: Ipv6Addr) -> bool {
+        (a.segments()[0] & 0xfe00) == 0xfc00
+    }
+
+    /// Returns [`true`] if this address is `fe80::/10`.
+    pub const fn is_unicast_link_local(a: Ipv6Addr) -> bool {
+        (a.segments()[0] & 0xffc0) == 0xfe80
+    }
+
+    pub fn is_global(a: Ipv6Addr) -> bool {
+        !(a.is_loopback()
+            || a.is_unspecified()
+            || Self::is_ipv4_mapped_address(a)
+            || Self::is_ipv4_ipv6_translat(a)
+            || Self::is_discard_only_address_block(a)
+            || Self::is_ietf_protocol_assignments(a)
+            || Self::is_teredo(a)
+            || Self::is_port_control_protocol_anycast(a)
+            || Self::is_traversal_relays_nat_anycast(a)
+            || Self::is_benchmarking(a)
+            || Self::is_amt(a)
+            || Self::is_as112_v6(a)
+            || Self::is_deprecated_previously_orchid(a)
+            || Self::is_orchid_v2(a)
+            || Self::is_drone_remote(a)
+            || Self::is_documentation(a)
+            || Self::is_6to4(a)
+            || Self::is_direct_delegation_as112_service(a)
+            || Self::is_unique_local(a)
+            || Self::is_unicast_link_local(a))
+    }
+}
+
 impl<T> GlobalIpOnly<T> {
     pub fn new(transport: T) -> Self {
         GlobalIpOnly { inner: transport }
@@ -204,7 +321,7 @@ impl<T: Transport + Unpin> Transport for GlobalIpOnly<T> {
                 }
             }
             Some(Protocol::Ip6(a)) => {
-                if a.is_global() {
+                if Ipv6Global::is_global(a) {
                     self.inner.dial(addr)
                 } else {
                     debug!("Not dialing non global IP address {:?}.", a);
