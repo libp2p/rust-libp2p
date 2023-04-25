6560de181b04459932a058ce0b10c744a8275049
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2023-04-25 20:03:56 +0200

    Update core/src/transport/global_only.rs
    Co-authored-by: Thomas Eizinger <thomas@eizinger.io>

--- core/src/transport/global_only.rs
+++ core/src/transport/global_only.rs
@@ -1,4 +1,4 @@
-// Copyright 2019 Parity Technologies (UK) Ltd.
+// Copyright 2023 Protocol Labs
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
