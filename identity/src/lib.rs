efa3874addbabd8908ed7ff0007d3a8eacc341c3
Thomas Coratger <thomas.coratger@gmail.com>
2023-04-25 09:16:04 +0200

    add docsrs flag identity

--- identity/src/lib.rs
+++ identity/src/lib.rs
@@ -33,6 +33,8 @@
 //! (e.g. [ed25519 binary format](https://datatracker.ietf.org/doc/html/rfc8032#section-5.1.5)).
 //! All key types have functions to enable conversion to/from their binary representations.
 
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+
 #[cfg(any(
     feature = "ecdsa",
     feature = "secp256k1",
