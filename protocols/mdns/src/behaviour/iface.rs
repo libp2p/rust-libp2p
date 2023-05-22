115bcd847ed211fbfc6b13516eef09b0d4255888
Philippe Jalaber <philippe.jalaber@stormshield.eu>
2023-05-22 15:07:50 +0200

    feat(mdns): add a neighbour discovery logic using an adaptive initial interval

--- protocols/mdns/src/behaviour/iface.rs
+++ protocols/mdns/src/behaviour/iface.rs
@@ -38,6 +38,67 @@ use std::{
     time::{Duration, Instant},
 };
 
+/// Initial interval for starting probe
+const INITIAL_TIMEOUT_INTERVAL: Duration = Duration::from_millis(500);
+
+#[derive(Debug, Clone, Default)]
+pub enum ProbeState {
+    #[default]
+    Start,
+    Probing(Duration),
+    Finished(Duration),
+}
+
+impl ProbeState {
+    /// Returns `true` if the probe state is [`Probing`].
+    ///
+    /// [`Probing`]: ProbeState::Probing
+    #[must_use]
+    pub fn is_probing(&self) -> bool {
+        matches!(self, Self::Probing(..))
+    }
+
+    /// Update the internal state
+    fn update(&mut self, continue_probing: bool, maximum_interval: &Duration) {
+        *self = match *self {
+            ProbeState::Start => ProbeState::Probing(INITIAL_TIMEOUT_INTERVAL),
+            ProbeState::Probing(initial_interval) => {
+                self.on_probing(initial_interval, continue_probing, maximum_interval)
+            }
+            ProbeState::Finished(query_interval) => ProbeState::Finished(query_interval),
+        };
+    }
+
+    fn on_probing(
+        &mut self,
+        initial_interval: Duration,
+        continue_probing: bool,
+        maximum_interval: &Duration,
+    ) -> Self {
+        // Check if a peer has been discovered
+        if continue_probing {
+            // Increase the intial interval if we are still probing for a peer
+            // Stop to probe when the initial interval reach the query interval
+            let initial_interval = initial_interval * 2;
+            if initial_interval >= *maximum_interval {
+                ProbeState::Finished(*maximum_interval)
+            } else {
+                ProbeState::Probing(initial_interval)
+            }
+        } else {
+            ProbeState::Finished(*maximum_interval)
+        }
+    }
+
+    fn interval(&self) -> &Duration {
+        match self {
+            ProbeState::Start => &INITIAL_TIMEOUT_INTERVAL,
+            ProbeState::Probing(query_interval) => query_interval,
+            ProbeState::Finished(query_interval) => query_interval,
+        }
+    }
+}
+
 /// An mDNS instance for a networking interface. To discover all peers when having multiple
 /// interfaces an [`InterfaceState`] is required for each interface.
 #[derive(Debug)]
@@ -67,7 +128,7 @@ pub(crate) struct InterfaceState<U, T> {
     discovered: VecDeque<(PeerId, Multiaddr, Instant)>,
     /// TTL
     ttl: Duration,
-
+    probe_state: ProbeState,
     local_peer_id: PeerId,
 }
 
@@ -134,19 +195,21 @@ where
             send_buffer: Default::default(),
             discovered: Default::default(),
             query_interval,
-            timeout: T::interval_at(Instant::now(), query_interval),
+            timeout: T::interval_at(Instant::now(), INITIAL_TIMEOUT_INTERVAL),
             multicast_addr,
             ttl: config.ttl,
+            probe_state: Default::default(),
             local_peer_id,
         })
     }
 
     pub(crate) fn reset_timer(&mut self) {
-        self.timeout = T::interval(self.query_interval);
+        let interval = *self.probe_state.interval();
+        self.timeout = T::interval(interval);
     }
 
     pub(crate) fn fire_timer(&mut self) {
-        self.timeout = T::interval_at(Instant::now(), self.query_interval);
+        self.timeout = T::interval_at(Instant::now(), INITIAL_TIMEOUT_INTERVAL);
     }
 
     pub(crate) fn poll(
@@ -159,6 +222,16 @@ where
             if Pin::new(&mut self.timeout).poll_next(cx).is_ready() {
                 log::trace!("sending query on iface {}", self.addr);
                 self.send_buffer.push_back(build_query());
+
+                let is_probing = self.probe_state.is_probing();
+                self.probe_state
+                    .update(self.discovered.is_empty(), &self.query_interval);
+
+                // Reset if the previous state was Probing or if the state has been switched to Finished
+                // We do not need to reset the timer after a tick in Finished state, the reset should occur reset when the interface receive a response from a peer.
+                if is_probing {
+                    self.reset_timer();
+                }
             }
 
             // 2nd priority: Keep local buffers small: Send packets to remote.
@@ -193,7 +266,6 @@ where
                 .map_ok(|(len, from)| MdnsPacket::new_from_bytes(&self.recv_buffer[..len], from))
             {
                 Poll::Ready(Ok(Ok(Some(MdnsPacket::Query(query))))) => {
-                    self.reset_timer();
                     log::trace!(
                         "received query from {} on {}",
                         query.remote_addr(),
@@ -217,6 +289,12 @@ where
 
                     self.discovered
                         .extend(response.extract_discovered(Instant::now(), self.local_peer_id));
+
+                    // Stop probing when we have a valid response
+                    if !self.discovered.is_empty() {
+                        self.probe_state.update(true, &self.query_interval);
+                        self.reset_timer();
+                    }
                     continue;
                 }
                 Poll::Ready(Ok(Ok(Some(MdnsPacket::ServiceDiscovery(disc))))) => {
