d72d9a7581c8e018b10f898793d4f17c7280bd4d
ozkanonur <work@onurozkan.dev>
2023-05-22 19:33:30 +0300

    provide wasm compatibility for gossipsub protocol
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- protocols/gossipsub/src/peer_score.rs
+++ protocols/gossipsub/src/peer_score.rs
@@ -24,12 +24,12 @@
 use crate::metrics::{Metrics, Penalty};
 use crate::time_cache::TimeCache;
 use crate::{MessageId, TopicHash};
+use instant::Instant;
 use libp2p_identity::PeerId;
 use log::{debug, trace, warn};
 use std::collections::{hash_map, HashMap, HashSet};
 use std::net::IpAddr;
 use std::time::Duration;
-use wasm_timer::Instant;
 
 mod params;
 use crate::ValidationError;
