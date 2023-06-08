8eda5a76899a3355b8164e56ac6e9d110b7085ee
Arpan Kapoor <a@arpankapoor.com>
2023-06-08 20:25:18 +0530

    Update external address

--- examples/relay-server/src/main.rs
+++ examples/relay-server/src/main.rs
@@ -101,6 +101,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     block_on(async {
         loop {
             match swarm.next().await.expect("Infinite Stream.") {
+                SwarmEvent::Behaviour(BehaviourEvent::Identify(identify::Event::Received {
+                    info: identify::Info { observed_addr, .. },
+                    ..
+                })) => {
+                    swarm.add_external_address(observed_addr);
+                }
                 SwarmEvent::Behaviour(event) => {
                     println!("{event:?}")
                 }
