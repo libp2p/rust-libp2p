3bab194041d6188af0d9c5350e8f92a23d617da2
Thomas Eizinger <thomas@eizinger.io>
2023-06-20 10:52:29 +0200

    Condense commands

--- scripts/list-external-contributors.sh
+++ scripts/list-external-contributors.sh
@@ -7,12 +7,8 @@ set -e
 TAG=$1
 
 release_date=$(git log -1 --format=%aI --date=iso-strict $TAG)
-
 authors=$(gh api "repos/libp2p/rust-libp2p/commits?since=$release_date" --paginate -q '.[].author.login' | sort -u)
-
 unique_authors=$(echo "$authors" | sort -u)
-
-# Fetch all members of the specific GitHub team
 team_members=$(gh api teams/6797340/members --paginate | jq -r '.[].login' | sort -u)
 
 echo "$unique_authors" | grep -vxF -f <(echo "$team_members") | grep -vF "bot" | grep -vF "web-flow"
