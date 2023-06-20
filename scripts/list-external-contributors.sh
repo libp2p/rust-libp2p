d3f06394abb3379a70e8676e2f0eafcfd0888d54
Thomas Eizinger <thomas@eizinger.io>
2023-06-20 10:54:09 +0200

    Better variable naming

--- scripts/list-external-contributors.sh
+++ scripts/list-external-contributors.sh
@@ -4,11 +4,8 @@
 
 set -e
 
-TAG=$1
+date_of_tag=$(git log -1 --format=%aI --date=iso-strict $1)
+unique_authors=$(gh api "repos/libp2p/rust-libp2p/commits?since=$date_of_tag" --paginate -q '.[].author.login' | sort -u)
+rust_libp2p_maintainers_team_members=$(gh api teams/6797340/members --paginate | jq -r '.[].login' | sort -u)
 
-release_date=$(git log -1 --format=%aI --date=iso-strict $TAG)
-authors=$(gh api "repos/libp2p/rust-libp2p/commits?since=$release_date" --paginate -q '.[].author.login' | sort -u)
-unique_authors=$(echo "$authors" | sort -u)
-team_members=$(gh api teams/6797340/members --paginate | jq -r '.[].login' | sort -u)
-
-echo "$unique_authors" | grep -vxF -f <(echo "$team_members") | grep -vF "bot" | grep -vF "web-flow"
+echo "$unique_authors" | grep -vxF -f <(echo "$rust_libp2p_maintainers_team_members") | grep -vF "bot" | grep -vF "web-flow"
