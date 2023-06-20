c9f810919367060d02b3004ab4b398d1190d236e
Thomas Eizinger <thomas@eizinger.io>
2023-06-20 10:51:49 +0200

    Add script for generating list of external contributors

---
+++ scripts/list-external-contributors.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Usage: ./scripts/list-external-contributors.sh <TAG>
+
+set -e
+
+TAG=$1
+
+release_date=$(git log -1 --format=%aI --date=iso-strict $TAG)
+
+authors=$(gh api "repos/libp2p/rust-libp2p/commits?since=$release_date" --paginate -q '.[].author.login' | sort -u)
+
+unique_authors=$(echo "$authors" | sort -u)
+
+# Fetch all members of the specific GitHub team
+team_members=$(gh api teams/6797340/members --paginate | jq -r '.[].login' | sort -u)
+
+echo "$unique_authors" | grep -vxF -f <(echo "$team_members") | grep -vF "bot" | grep -vF "web-flow"
