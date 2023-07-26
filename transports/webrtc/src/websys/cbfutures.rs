d1ee35ef8dba9c72d6154aff5e07cb1501d4ac63
Doug A <douganderson444@gmail.com>
2023-07-26 18:06:11 -0300

    add Fused Callback Future & maybe_init

--- transports/webrtc/src/websys/cbfutures.rs
+++ transports/webrtc/src/websys/cbfutures.rs
@@ -1,8 +1,33 @@
+use futures::FutureExt;
 use std::cell::Cell;
 use std::future::Future;
 use std::pin::Pin;
 use std::rc::Rc;
-use std::task::{Context, Poll, Waker};
+use std::task::{ready, Context, Poll, Waker};
+
+pub struct FusedCbFuture<T>(Option<CbFuture<T>>);
+
+impl<T> FusedCbFuture<T> {
+    pub(crate) fn new() -> Self {
+        Self(None)
+    }
+
+    pub(crate) fn maybe_init<F>(&mut self, init: F) -> &mut Self
+    where
+        F: FnOnce() -> CbFuture<T>,
+    {
+        if self.0.is_none() {
+            self.0 = Some(init());
+        }
+
+        self
+    }
+
+    /// Checks if future is already running
+    pub(crate) fn is_active(&self) -> bool {
+        self.0.is_some()
+    }
+}
 
 #[derive(Clone, Debug)]
 pub struct CbFuture<T>(Rc<CallbackFutureInner<T>>);
@@ -52,5 +77,22 @@ impl<T> Future for CbFuture<T> {
                 Poll::Pending
             }
         }
+    }
+}
+
+impl<T> Future for FusedCbFuture<T> {
+    type Output = T;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let val = ready!(self
+            .0
+            .as_mut()
+            .expect("FusedCbFuture not initialized")
+            .poll_unpin(cx));
+
+        // Future finished, drop it
+        self.0.take();
+
+        Poll::Ready(val)
     }
 }
