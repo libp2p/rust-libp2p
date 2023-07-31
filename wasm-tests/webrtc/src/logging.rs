16d8943bc5a6b75c864b9b8105009359d958aaae
Doug A <douganderson444@gmail.com>
2023-07-31 09:53:16 -0300

    set up client test logging

---
+++ wasm-tests/webrtc/src/logging.rs
@@ -0,0 +1,27 @@
+use fern::colors::{Color, ColoredLevelConfig};
+
+pub fn set_up_logging() {
+    let colors_line = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        // we actually don't need to specify the color for debug and info, they are white by default
+        .info(Color::Green)
+        .debug(Color::Yellow)
+        // depending on the terminals color scheme, this is the same as the background color
+        .trace(Color::BrightBlack);
+    let colors_level = colors_line.info(Color::Green);
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "[{date} {level} {target}] {message}\x1B[0m",
+                date = chrono::Local::now().format("%T%.3f"),
+                target = record.target(),
+                level = colors_level.color(record.level()),
+                message = message,
+            ));
+        })
+        .level(log::LevelFilter::Trace)
+        .chain(fern::Output::call(console_log::log))
+        .apply()
+        .unwrap();
+}
