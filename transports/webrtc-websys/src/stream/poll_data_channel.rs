c8995ac6ddd9d9966d0118afb986d0876fe0eb8b
Doug A <douganderson444@gmail.com>
2023-08-04 14:02:08 -0300

    fix log::debug, general cleanup

--- transports/webrtc-websys/src/stream/poll_data_channel.rs
+++ transports/webrtc-websys/src/stream/poll_data_channel.rs
@@ -1,12 +1,7 @@
-// Copied from webrtc::data::data_channel::poll_data_channel.rs
-// use self::error::Result;
-// use crate::{
-//     error::Error, message::message_channel_ack::*, message::message_channel_open::*, message::*,
-// };
+// Inspired by webrtc::data::data_channel::poll_data_channel.rs
 use crate::cbfutures::{CbFuture, CbStream};
 use crate::error::Error;
 use futures::{AsyncRead, AsyncWrite, FutureExt, StreamExt};
-use log::debug;
 use std::fmt;
 use std::future::Future;
 use std::io;
@@ -19,32 +14,6 @@ use web_sys::{MessageEvent, RtcDataChannel, RtcDataChannelEvent, RtcDataChannelS
 /// Default capacity of the temporary read buffer used by [`webrtc_sctp::stream::PollStream`].
 const DEFAULT_READ_BUF_SIZE: usize = 8192;
 
-/// State of the read `Future` in [`PollStream`].
-enum ReadFut {
-    /// Nothing in progress.
-    Idle,
-    /// Reading data from the underlying stream.
-    Reading(Pin<Box<dyn Future<Output = Result<Vec<u8>, Error>> + Send>>),
-    /// Finished reading, but there's unread data in the temporary buffer.
-    RemainingData(Vec<u8>),
-}
-
-impl ReadFut {
-    /// Gets a mutable reference to the future stored inside `Reading(future)`.
-    ///
-    /// # Panics
-    ///
-    /// Panics if `ReadFut` variant is not `Reading`.
-    fn get_reading_mut(
-        &mut self,
-    ) -> &mut Pin<Box<dyn Future<Output = Result<Vec<u8>, Error>> + Send>> {
-        match self {
-            ReadFut::Reading(ref mut fut) => fut,
-            _ => panic!("expected ReadFut to be Reading"),
-        }
-    }
-}
-
 /// A wrapper around around [`RtcDataChannel`], which implements [`AsyncRead`] and
 /// [`AsyncWrite`].
 ///
@@ -53,9 +22,11 @@ impl ReadFut {
 pub struct PollDataChannel {
     data_channel: RtcDataChannel,
 
+    /// onmessage is an Option, Some(data) means there is data, None means the channel has closed
     onmessage_fut: CbStream<Vec<u8>>,
     onopen_fut: CbFuture<()>,
     onbufferedamountlow_fut: CbFuture<()>,
+    onclose_fut: CbFuture<()>,
 
     read_buf_cap: usize,
 }
@@ -69,26 +40,12 @@ impl PollDataChannel {
 
         let onopen_callback = Closure::<dyn FnMut(_)>::new(move |_ev: RtcDataChannelEvent| {
             // TODO: Send any queued messages
-            debug!("Data Channel opened");
+            log::debug!("Data Channel opened");
             onopen_cback_clone.publish(());
         });
 
         data_channel.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
         onopen_callback.forget();
-
-        // On Close --  never needed, poll_close() doesn't use it.
-        // let onclose_fut = CbFuture::new();
-        // let onclose_cback_clone = onclose_fut.clone();
-
-        // let onclose_callback = Closure::<dyn FnMut(_)>::new(move |_ev: RtcDataChannelEvent| {
-        //     // TODO: Set state to closed?
-        //     // TODO: Set futures::Stream Poll::Ready(None)?
-        //     debug!("Data Channel closed. onclose_callback");
-        //     onclose_cback_clone.publish(());
-        // });
-
-        // data_channel.set_onclose(Some(onclose_callback.as_ref().unchecked_ref()));
-        // onclose_callback.forget();
 
         /*
          * On Error
@@ -97,7 +54,7 @@ impl PollDataChannel {
         let onerror_cback_clone = onerror_fut.clone();
 
         let onerror_callback = Closure::<dyn FnMut(_)>::new(move |ev: RtcDataChannelEvent| {
-            debug!("Data Channel error");
+            log::debug!("Data Channel error");
             onerror_cback_clone.publish(());
         });
 
@@ -111,17 +68,29 @@ impl PollDataChannel {
         let onmessage_cback_clone = onmessage_fut.clone();
 
         let onmessage_callback = Closure::<dyn FnMut(_)>::new(move |ev: MessageEvent| {
-            // TODO: Use Protobuf decoder?
             let data = ev.data();
-            // The convert fron Js ArrayBuffer to Vec<u8>
+            // Convert from Js ArrayBuffer to Vec<u8>
             let data = js_sys::Uint8Array::new(&data).to_vec();
-            debug!("onmessage data: {:?}", data);
-            // TODO: publish(None) to close the stream when the channel closes
+            log::debug!("onmessage data: {:?}", data);
             onmessage_cback_clone.publish(data);
         });
 
         data_channel.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
         onmessage_callback.forget();
+
+        // On Close
+        let onclose_fut = CbFuture::new();
+        let onclose_cback_clone = onclose_fut.clone();
+
+        let onclose_callback = Closure::<dyn FnMut(_)>::new(move |_ev: RtcDataChannelEvent| {
+            // TODO: Set state to closed?
+            // TODO: Set futures::Stream Poll::Ready(None)?
+            log::debug!("Data Channel closed. onclose_callback");
+            onclose_cback_clone.publish(());
+        });
+
+        data_channel.set_onclose(Some(onclose_callback.as_ref().unchecked_ref()));
+        onclose_callback.forget();
 
         /*
          * Convert `RTCDataChannel: bufferedamountlow event` Low Event Callback to Future
@@ -131,7 +100,7 @@ impl PollDataChannel {
 
         let onbufferedamountlow_callback =
             Closure::<dyn FnMut(_)>::new(move |_ev: RtcDataChannelEvent| {
-                debug!("bufferedamountlow event");
+                log::debug!("bufferedamountlow event");
                 onbufferedamountlow_cback_clone.publish(());
             });
 
@@ -143,6 +112,7 @@ impl PollDataChannel {
             data_channel,
             onmessage_fut,
             onopen_fut,
+            onclose_fut,
             onbufferedamountlow_fut,
             read_buf_cap: DEFAULT_READ_BUF_SIZE,
         }
@@ -175,7 +145,7 @@ impl PollDataChannel {
 
     /// Send data buffer
     pub fn send(&self, data: &[u8]) -> Result<(), Error> {
-        debug!("send: {:?}", data);
+        log::debug!("send: {:?}", data);
         self.data_channel.send_with_u8_array(data)?;
         Ok(())
     }
@@ -202,16 +172,16 @@ impl AsyncRead for PollDataChannel {
         cx: &mut Context<'_>,
         buf: &mut [u8],
     ) -> Poll<Result<usize, std::io::Error>> {
-        if let Some(data) = ready!(self.onmessage_fut.poll_next_unpin(cx)) {
-            let data_len = data.len();
-            let buf_len = buf.len();
-            debug!("poll_read [{:?} of {} bytes]", data_len, buf_len);
-            let len = std::cmp::min(data_len, buf_len);
-            buf[..len].copy_from_slice(&data[..len]);
-            Poll::Ready(Ok(len))
-        } else {
-            // if None, the stream is exhausted, no data to read
-            Poll::Ready(Ok(0))
+        match ready!(self.onmessage_fut.poll_next_unpin(cx)) {
+            Some(data) => {
+                let data_len = data.len();
+                let buf_len = buf.len();
+                log::debug!("poll_read [{:?} of {} bytes]", data_len, buf_len);
+                let len = std::cmp::min(data_len, buf_len);
+                buf[..len].copy_from_slice(&data[..len]);
+                Poll::Ready(Ok(len))
+            }
+            None => Poll::Ready(Ok(0)), // if None, the stream is exhausted, no data to read
         }
     }
 }
@@ -222,16 +192,15 @@ impl AsyncWrite for PollDataChannel {
         cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<io::Result<usize>> {
-        debug!("poll_write: [{:?}]", buf.len());
-        // As long as the data channel is open we can write
-        // So, poll on open state to confirm the channel is open
+        log::debug!("poll_write: [{:?}]", buf.len());
+        // If the data channel is not open,
+        // poll on open future until the channel is open
         if self.data_channel.ready_state() != RtcDataChannelState::Open {
-            // poll onopen
             ready!(self.onopen_fut.poll_unpin(cx));
         }
 
         // Now that the channel is open, send the data
-        match self.data_channel.send_with_u8_array(buf) {
+        match self.send(buf) {
             Ok(_) => Poll::Ready(Ok(buf.len())),
             Err(e) => Poll::Ready(Err(io::Error::new(
                 io::ErrorKind::Other,
@@ -251,39 +220,37 @@ impl AsyncWrite for PollDataChannel {
     /// We can therefore create a callback future called `onbufferedamountlow_fut` to listen for `bufferedamountlow` event and wake the task
     /// The default `bufferedAmountLowThreshold` value is 0.
     fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-        debug!("poll_flush");
-
-        // if bufferedamountlow empty,return ready
-        if self.data_channel.buffered_amount() == 0 {
-            debug!("0 buffered_amount");
-            return Poll::Ready(Ok(()));
-        }
-
-        debug!(
-            "buffered_amount is {:?}",
+        log::debug!(
+            "poll_flush buffered_amount is {:?}",
             self.data_channel.buffered_amount()
         );
+        // if bufferedamountlow empty, return ready
+        if self.data_channel.buffered_amount() == 0 {
+            return Poll::Ready(Ok(()));
+        }
 
         // Otherwise, wait for the event to occur, so poll on onbufferedamountlow_fut
         match self.onbufferedamountlow_fut.poll_unpin(cx) {
             Poll::Ready(()) => {
-                debug!("flushed");
+                log::debug!("flushed");
                 Poll::Ready(Ok(()))
             }
             Poll::Pending => {
-                debug!("pending");
+                log::debug!("pending");
                 Poll::Pending
             }
         }
     }
 
+    /// Initiates or attempts to shut down this writer,
+    /// returning success when the connection callback returns has completely shut down.
     fn poll_close(
-        self: std::pin::Pin<&mut Self>,
+        mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<std::result::Result<(), std::io::Error>> {
-        // close the data channel
-        debug!("poll_close");
+        log::debug!("poll_close");
         self.data_channel.close();
+        ready!(self.onclose_fut.poll_unpin(cx));
         Poll::Ready(Ok(()))
     }
 }
