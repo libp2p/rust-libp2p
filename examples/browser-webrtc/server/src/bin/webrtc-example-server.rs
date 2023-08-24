b02a560b0519eb9ae8ee50f7a69cfd02256ec596
Doug A <douganderson444@gmail.com>
2023-08-24 14:05:39 -0300

    mv env vars to `main.rs`

--- examples/browser-webrtc/server/src/bin/webrtc-example-server.rs
+++ examples/browser-webrtc/server/src/bin/webrtc-example-server.rs
@@ -1,5 +1,12 @@
+use std::env;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    env::set_var(
+        "RUST_LOG",
+        "debug,libp2p_webrtc,libp2p_ping=debug,webrtc_sctp=off,webrtc_mdns=off,webrtc_ice=off",
+    );
+
     env_logger::init();
 
     webrtc_example_server::start().await?;
