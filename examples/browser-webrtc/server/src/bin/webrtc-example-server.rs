3e7865ef4cf0c5edab5a76397b5d80a3d0c63eaf
Thomas Eizinger <thomas@eizinger.io>
2023-08-31 21:00:49 +1000

    Init logger directly instead of setting var

--- examples/browser-webrtc/server/src/bin/webrtc-example-server.rs
+++ examples/browser-webrtc/server/src/bin/webrtc-example-server.rs
@@ -1,13 +1,9 @@
-use std::env;
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    env::set_var(
-        "RUST_LOG",
-        "webrtc_example_server=debug,libp2p_webrtc=info,libp2p_ping=debug",
-    );
-
-    env_logger::init();
+    env_logger::builder()
+        .parse_filters("webrtc_example_server=debug,libp2p_webrtc=info,libp2p_ping=debug")
+        .parse_default_env()
+        .init();
 
     webrtc_example_server::start().await?;
 
