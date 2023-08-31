3a1327847d2debf2023af47cf576c43ced97a8a2
Thomas Eizinger <thomas@eizinger.io>
2023-08-31 21:39:08 +1000

    Use `thiserror`

--- examples/browser-webrtc/client/src/error.rs
+++ examples/browser-webrtc/client/src/error.rs
@@ -1,27 +1,12 @@
 use libp2p::ping::Failure;
 use libp2p::{multiaddr, swarm};
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub(crate) enum PingerError {
-    Ping(Failure),
-    MultiaddrParse(multiaddr::Error),
-    Dial(swarm::DialError),
-}
-
-impl From<Failure> for PingerError {
-    fn from(f: Failure) -> Self {
-        PingerError::Ping(f)
-    }
-}
-
-impl From<multiaddr::Error> for PingerError {
-    fn from(err: multiaddr::Error) -> Self {
-        PingerError::MultiaddrParse(err)
-    }
-}
-
-impl From<swarm::DialError> for PingerError {
-    fn from(err: swarm::DialError) -> Self {
-        PingerError::Dial(err)
-    }
+    #[error("failed to ping node")]
+    Ping(#[from] Failure),
+    #[error("failed to parse address")]
+    MultiaddrParse(#[from] multiaddr::Error),
+    #[error("failed to dial node")]
+    Dial(#[from] swarm::DialError),
 }
