7a790970b6995b3e79037ba3a5a225c94465645f
Thomas Eizinger <thomas@eizinger.io>
2023-09-12 14:30:47 +1000

    Update deprecation notice

--- swarm/src/lib.rs
+++ swarm/src/lib.rs
@@ -68,7 +68,7 @@ pub mod dial_opts;
 pub mod dummy;
 pub mod handler;
 #[deprecated(
-    note = "Configure an appropriate idle connection timeout via `SwarmBuilder::idle_connection_timeout` instead."
+    note = "Configure an appropriate idle connection timeout via `SwarmBuilder::idle_connection_timeout` instead. To keep connections alive 'forever', use `Duration::from_secs(u64::MAX)`."
 )]
 pub mod keep_alive;
 mod listen_opts;
