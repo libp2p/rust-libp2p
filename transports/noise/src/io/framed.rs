5627360be9e9ba3f1010ec501cbf42d67a700c73
Thomas Eizinger <thomas@eizinger.io>
2023-09-29 12:59:44 +1000

    Go back to reusing buffers

--- transports/noise/src/io/framed.rs
+++ transports/noise/src/io/framed.rs
@@ -27,7 +27,6 @@ use super::handshake::proto;
 use crate::{protocol::PublicKey, Error};
 use asynchronous_codec::{Decoder, Encoder};
 use bytes::{Buf, Bytes, BytesMut};
-use log::{debug, error};
 use quick_protobuf::{BytesReader, MessageRead, MessageWrite, Writer};
 use std::io;
 use std::mem::size_of;
@@ -46,47 +45,20 @@ static_assertions::const_assert! {
 /// encoding and decoding length-delimited session messages.
 pub(crate) struct Codec<S> {
     session: S,
+
+    // We reuse write and encryption buffers across multiple messages to avoid reallocations.
+    // We cannot reuse read and decryption buffers because we cannot return borrowed data.
+    write_buffer: BytesMut,
+    encrypt_buffer: BytesMut,
 }
 
-impl<S: SessionState> Codec<S> {
+impl<S> Codec<S> {
     pub(crate) fn new(session: S) -> Self {
-        Codec { session }
-    }
-
-    fn encode_bytes(&mut self, item: &[u8], dst: &mut BytesMut) -> Result<(), io::Error> {
-        let mut encrypt_buffer = BytesMut::zeroed(item.len() + EXTRA_ENCRYPT_SPACE);
-
-        let n = match self.session.write_message(item, &mut encrypt_buffer) {
-            Ok(n) => n,
-            Err(e) => {
-                error!("encryption error: {:?}", e);
-                return Err(io::ErrorKind::InvalidData.into());
-            }
-        };
-
-        let msg = encrypt_buffer.split_to(n).freeze();
-
-        encode_length_prefixed(&msg, dst);
-
-        Ok(())
-    }
-
-    fn decode_bytes(&mut self, src: &mut BytesMut) -> Result<Option<Bytes>, io::Error> {
-        let bytes = match decode_length_prefixed(src)? {
-            Some(b) => b,
-            None => return Ok(None),
-        };
-
-        let mut decrypt_buffer = BytesMut::zeroed(bytes.len());
-        let n = match self.session.read_message(&bytes, &mut decrypt_buffer) {
-            Ok(n) => n,
-            Err(e) => {
-                debug!("decryption error {e}");
-                return Err(io::ErrorKind::InvalidData.into());
-            }
-        };
-
-        Ok(Some(decrypt_buffer.split_to(n).freeze()))
+        Codec {
+            session,
+            write_buffer: BytesMut::default(),
+            encrypt_buffer: BytesMut::default(),
+        }
     }
 }
 
@@ -131,28 +103,39 @@ impl Encoder for Codec<snow::HandshakeState> {
     type Item<'a> = &'a proto::NoiseHandshakePayload;
 
     fn encode(&mut self, item: Self::Item<'_>, dst: &mut BytesMut) -> Result<(), Self::Error> {
-        let mut write_buffer = BytesMut::zeroed(item.get_size());
+        let item_size = item.get_size();
 
-        let mut writer = Writer::new(&mut write_buffer[..]);
+        self.write_buffer.resize(item_size, 0);
+        let mut writer = Writer::new(&mut self.write_buffer[..item_size]);
         item.write_message(&mut writer)
             .expect("Protobuf encoding to succeed");
 
-        self.encode_bytes(&write_buffer.split_to(item.get_size()).freeze(), dst)
+        encrypt(
+            &self.write_buffer[..item_size],
+            dst,
+            &mut self.encrypt_buffer,
+            |item, buffer| self.session.write_message(item, buffer),
+        )?;
+
+        Ok(())
     }
 }
+
 impl Decoder for Codec<snow::HandshakeState> {
     type Error = io::Error;
     type Item = proto::NoiseHandshakePayload;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        let bytes = match self.decode_bytes(src)? {
-            Some(bytes) => bytes,
+        let cleartext = match decrypt(src, |ciphertext, decrypt_buffer| {
+            self.session.read_message(ciphertext, decrypt_buffer)
+        })? {
             None => return Ok(None),
+            Some(cleartext) => cleartext,
         };
 
-        let mut reader = BytesReader::from_bytes(&bytes[..]);
+        let mut reader = BytesReader::from_bytes(&cleartext[..]);
         let pb =
-            proto::NoiseHandshakePayload::from_reader(&mut reader, &bytes[..]).map_err(|_| {
+            proto::NoiseHandshakePayload::from_reader(&mut reader, &cleartext).map_err(|_| {
                 io::Error::new(
                     io::ErrorKind::InvalidData,
                     "Failed decoding handshake payload",
@@ -168,53 +151,64 @@ impl Encoder for Codec<snow::TransportState> {
     type Item<'a> = &'a [u8];
 
     fn encode(&mut self, item: Self::Item<'_>, dst: &mut BytesMut) -> Result<(), Self::Error> {
-        self.encode_bytes(item, dst)
+        encrypt(item, dst, &mut self.encrypt_buffer, |item, buffer| {
+            self.session.write_message(item, buffer)
+        })
     }
 }
+
 impl Decoder for Codec<snow::TransportState> {
     type Error = io::Error;
     type Item = Bytes;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        self.decode_bytes(src)
+        decrypt(src, |ciphertext, decrypt_buffer| {
+            self.session.read_message(ciphertext, decrypt_buffer)
+        })
     }
 }
 
-/// A stateful context in which Noise protocol messages can be read and written.
-pub(crate) trait SessionState {
-    /// Decrypt the payload `msg` with the session state and read the result to `buf`.
-    fn read_message(&mut self, msg: &[u8], buf: &mut [u8]) -> Result<usize, snow::Error>;
+/// Encrypts the given cleartext to `dst`.
+///
+/// This is a standalone function to allow us reusing the `encrypt_buffer` and to use to across different session states of te noise protocol.
+fn encrypt(
+    cleartext: &[u8],
+    dst: &mut BytesMut,
+    encrypt_buffer: &mut BytesMut,
+    encrypt_fn: impl FnOnce(&[u8], &mut [u8]) -> Result<usize, snow::Error>,
+) -> io::Result<()> {
+    encrypt_buffer.resize(cleartext.len() + EXTRA_ENCRYPT_SPACE, 0);
+    let n = encrypt_fn(cleartext, encrypt_buffer).map_err(into_io_error)?;
+    encode_length_prefixed(&encrypt_buffer[..n], dst);
 
-    /// Encrypt the payload `msg` with the session state and write the result to `buf`.
-    fn write_message(&mut self, msg: &[u8], buf: &mut [u8]) -> Result<usize, snow::Error>;
+    Ok(())
 }
 
-impl SessionState for snow::HandshakeState {
-    fn read_message(&mut self, msg: &[u8], buf: &mut [u8]) -> Result<usize, snow::Error> {
-        self.read_message(msg, buf)
-    }
+fn decrypt(
+    ciphertext: &mut BytesMut,
+    decrypt_fn: impl FnOnce(&[u8], &mut [u8]) -> Result<usize, snow::Error>,
+) -> io::Result<Option<Bytes>> {
+    let ciphertext = match decode_length_prefixed(ciphertext)? {
+        Some(b) => b,
+        None => return Ok(None),
+    };
 
-    fn write_message(&mut self, msg: &[u8], buf: &mut [u8]) -> Result<usize, snow::Error> {
-        self.write_message(msg, buf)
-    }
+    let mut decrypt_buffer = BytesMut::zeroed(ciphertext.len());
+    let n = decrypt_fn(&ciphertext, &mut decrypt_buffer).map_err(into_io_error)?;
+
+    Ok(Some(decrypt_buffer.split_to(n).freeze()))
 }
 
-impl SessionState for snow::TransportState {
-    fn read_message(&mut self, msg: &[u8], buf: &mut [u8]) -> Result<usize, snow::Error> {
-        self.read_message(msg, buf)
-    }
-
-    fn write_message(&mut self, msg: &[u8], buf: &mut [u8]) -> Result<usize, snow::Error> {
-        self.write_message(msg, buf)
-    }
+fn into_io_error(err: snow::Error) -> io::Error {
+    io::Error::new(io::ErrorKind::InvalidData, err)
 }
 
 const U16_LENGTH: usize = size_of::<u16>();
 
-fn encode_length_prefixed(src: &Bytes, dst: &mut BytesMut) {
+fn encode_length_prefixed(src: &[u8], dst: &mut BytesMut) {
     dst.reserve(U16_LENGTH + src.len());
     dst.extend_from_slice(&(src.len() as u16).to_be_bytes());
-    dst.extend_from_slice(&src);
+    dst.extend_from_slice(src);
 }
 
 fn decode_length_prefixed(src: &mut BytesMut) -> Result<Option<Bytes>, io::Error> {
