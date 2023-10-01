bce012d9855e5c2207935b703cdcd1eb8ee83bf5
Dave Huseby <dwh@linuxprogrammer.org>
2023-10-01 08:19:12 -0600

    fixed the broken ci tests
    Signed-off-by: Dave Huseby <dwh@linuxprogrammer.org>

--- protocols/kad/src/behaviour/test.rs
+++ protocols/kad/src/behaviour/test.rs
@@ -1054,7 +1054,7 @@ fn exceed_jobs_max_queries() {
                             result: QueryResult::GetClosestPeers(Ok(r)),
                             ..
                         }) => break assert!(r.peers.is_empty()),
-                        SwarmEvent::Behaviour(KademliaEvent::ModeChanged { .. }) => {}
+                        SwarmEvent::Behaviour(Event::ModeChanged { .. }) => {}
                         SwarmEvent::Behaviour(e) => panic!("Unexpected event: {e:?}"),
                         _ => {}
                     }
@@ -1378,7 +1378,7 @@ fn get_providers_single() {
                     result: QueryResult::StartProviding(Ok(_)),
                     ..
                 }) => {}
-                SwarmEvent::Behaviour(KademliaEvent::ModeChanged { .. }) => {}
+                SwarmEvent::Behaviour(Event::ModeChanged { .. }) => {}
                 SwarmEvent::Behaviour(e) => panic!("Unexpected event: {e:?}"),
                 _ => {}
             }
