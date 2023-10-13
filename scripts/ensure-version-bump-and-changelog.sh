2a68e08e452315cc75a4c0f6ed201de2cf3ca58f
Thomas Eizinger <thomas@eizinger.io>
2023-10-13 11:23:46 +1100

    Don't lint for unreleased versions if no code was touched

--- scripts/ensure-version-bump-and-changelog.sh
+++ scripts/ensure-version-bump-and-changelog.sh
@@ -13,19 +13,24 @@ CHANGELOG_DIFF=$(git diff "$HEAD_SHA".."$MERGE_BASE" --name-only -- "$DIR_TO_CRA
 VERSION_IN_CHANGELOG=$(awk -F' ' '/^## [0-9]+\.[0-9]+\.[0-9]+/{print $2; exit}' "$DIR_TO_CRATE/CHANGELOG.md")
 VERSION_IN_MANIFEST=$(cargo metadata --format-version=1 --no-deps | jq -e -r '.packages[] | select(.name == "'"$CRATE"'") | .version')
 
-# Ensure CHANGELOG is updated if any files are touched.
-if [ -n "$DIFF_TO_MASTER" ] && [ -z "$CHANGELOG_DIFF" ]; then
-    echo "Files in $DIR_TO_CRATE have changed, please write a changelog entry in $DIR_TO_CRATE/CHANGELOG.md"
-    exit 1
-fi
-
-# Ensure version in Cargo.toml and CHANGELOG are in sync.
+# First, ensure version in Cargo.toml and CHANGELOG are in sync. This should always hold, regardless of whether the code in the crate was modified.
 if [[ "$VERSION_IN_CHANGELOG" != "$VERSION_IN_MANIFEST" ]]; then
     echo "Version in Cargo.toml ($VERSION_IN_MANIFEST) does not match version in CHANGELOG ($VERSION_IN_CHANGELOG)"
     exit 1
 fi
 
-# Ensure version used in manifest has not been released yet.
+# If the crate wasn't touched in this PR, exit early.
+if [ -z "$DIFF_TO_MASTER" ]; then
+  exit 0;
+fi
+
+# Code was touched, ensure changelog is updated too.
+if [ -z "$CHANGELOG_DIFF" ]; then
+    echo "Files in $DIR_TO_CRATE have changed, please write a changelog entry in $DIR_TO_CRATE/CHANGELOG.md"
+    exit 1
+fi
+
+# Code was touched, ensure the version used in the manifest hasn't been released yet.
 if git tag | grep -q "^$CRATE-v${VERSION_IN_MANIFEST}$"; then
     echo "v$VERSION_IN_MANIFEST of '$CRATE' has already been released, please bump the version."
     exit 1
