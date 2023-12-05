1f43a6589774564a2aad5c84c684401dc61f925d
Eitan Seri-Levi <eserilev@ucsc.edu>
2023-12-05 09:39:44 -0800

    framework of escape hatch example

--- examples/escape-hatch/src/lib.rs
+++ examples/escape-hatch/src/lib.rs
@@ -8,6 +8,17 @@ use futures_timer::Delay;
 
 pub const PROTOCOL_NAME: StreamProtocol = StreamProtocol::new("/ipfs/ping/1.0.0");
 
+
+pub enum Error {
+	NotConnected(PeerId),
+	UnsupportedProtocol,
+	Io(io::Error)
+}
+
+pub enum Event {
+	InboundStreamFailed(io::Error),
+}
+
 /// A behaviour that manages requests to open new streams which are directly handed to the user.
 pub struct Behaviour {
     /// Config
@@ -46,6 +57,10 @@ impl IncomingStreams {
     }
 
     pub fn poll_next(&mut self) -> Poll<(PeerId, Stream)> {
+        if let Some(e) = self.events.pop_back() {
+        } else {
+        }
+
         todo!()
     }
 }
@@ -71,64 +86,6 @@ impl Behaviour {
 
 impl Control {
     pub async fn open_stream(&self, peer: PeerId) -> Result<Stream, Error> {
- 
-        peer
-        let stream = self.connection.poll_next_inbound(cx)
-            .transpose()
-            .map_err(Error)?
-            .map(Stream)
-            .ok_or(Error(ConnectionError::Closed))?;
-
         todo!()
-
-        // Stream::from(value);
-
-        // Ok(())
-    }
-}
-
-pub enum Error {
-    NotConnected(PeerId),
-    UnsupportedProtocol,
-    Io(io::Error),
-}
-
-pub enum Event {
-    /// A new stream is being opened
-    OpenStream,
-
-    /// An inbound stream failed
-    InboundStreamFailed(io::Error),
-}
-
-/// A wrapper around send that enforces a time out.
-async fn send_ping(stream: Stream, timeout: Duration) -> Result<(Stream, Duration), Error> {
-    let ping = send(stream);
-    futures::pin_mut!(ping);
-
-    match future::select(ping, Delay::new(timeout)).await {
-        Either::Left((Ok((stream, rtt)), _)) => Ok((stream, rtt)),
-        Either::Left((Err(e), _)) =>todo!(),
-        Either::Right(((), _)) => todo!(),
-    }
-}
-
-pub async fn send<S>(mut stream: S) -> io::Result<(S, Duration)>
-where
-    S: AsyncRead + AsyncWrite + Unpin,
-{
-    let payload: [u8; 32] = thread_rng().sample(distributions::Standard);
-    stream.write_all(&payload).await?;
-    stream.flush().await?;
-    let started = Instant::now();
-    let mut recv_payload = [0u8; 32];
-    stream.read_exact(&mut recv_payload).await?;
-    if recv_payload == payload {
-        Ok((stream, started.elapsed()))
-    } else {
-        Err(io::Error::new(
-            io::ErrorKind::InvalidData,
-            "Payload mismatch",
-        ))
     }
 }
--- examples/escape-hatch/src/main.rs
+++ examples/escape-hatch/src/main.rs
@@ -14,35 +14,3 @@ async fn main() {
     let stream_protocol = StreamProtocol::new("/escape-hatch/");
     let (behaviour, control, incoming_stream) = Behaviour::new(stream_protocol);
 }
-
-
-
-
-async fn test() {
-
-    let mem_addr = multiaddr![Memory(thread_rng().gen::<u64>())];
-    let mut transport = MemoryTransport::new().boxed();
-    transport.listen_on(ListenerId::next(), mem_addr).unwrap();
-
-    let listener_addr = transport
-        .select_next_some()
-        .now_or_never()
-        .and_then(|ev| ev.into_new_address())
-        .expect("MemoryTransport not listening on an address!");
-
-    async_std::task::spawn(async move {
-        let transport_event = transport.next().await.unwrap();
-        let (listener_upgrade, _) = transport_event.into_incoming().unwrap();
-        let conn = listener_upgrade.await.unwrap();
-        // recv_ping(conn).await.unwrap();
-    });
-
-    async_std::task::block_on(async move {
-        let c = MemoryTransport::new()
-            .dial(listener_addr)
-            .unwrap()
-            .await
-            .unwrap();
-        // let (_, rtt) = send_ping(c).await.unwrap();
-    });
-}
