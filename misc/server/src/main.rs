1a7599f09a10e46584900c6d244e4e376f898394
François RIBEAU <francois.ribeau.external@stormshield.eu>
2023-12-05 12:41:42 +0100

    feat(identify): add connection_id in event

--- misc/server/src/main.rs
+++ misc/server/src/main.rs
@@ -154,6 +154,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                             protocols,
                             ..
                         },
+                    ..
                 } = e
                 {
                     if protocols.iter().any(|p| *p == kad::PROTOCOL_NAME) {
