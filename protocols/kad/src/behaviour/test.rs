f0e563e74954fcd34135f9446d2a4f4c3b5deeda
PanGan21 <ganelis.pan@gmail.com>
2023-12-05 12:48:08 +0200

    satisfy clippy

--- protocols/kad/src/behaviour/test.rs
+++ protocols/kad/src/behaviour/test.rs
@@ -174,7 +174,7 @@ fn bootstrap() {
         let num_group = rng.gen_range(1..(num_total % K_VALUE.get()) + 2);
 
         let mut cfg = Config::default();
-        cfg.bootstrap_interval = None;
+        cfg.set_bootstrap_interval(None);
         if rng.gen() {
             cfg.disjoint_query_paths(true);
         }
@@ -254,7 +254,7 @@ fn query_iter() {
     fn run(rng: &mut impl Rng) {
         let num_total = rng.gen_range(2..20);
         let mut config = Config::default();
-        config.bootstrap_interval = None;
+        config.set_bootstrap_interval(None);
         let mut swarms = build_connected_nodes_with_config(num_total, 1, config)
             .into_iter()
             .map(|(_a, s)| s)
@@ -503,7 +503,7 @@ fn put_record() {
 
         let mut config = Config::default();
         config.set_replication_factor(replication_factor);
-        config.bootstrap_interval = None;
+        config.set_bootstrap_interval(None);
         if rng.gen() {
             config.disjoint_query_paths(true);
         }
@@ -873,7 +873,7 @@ fn add_provider() {
 
         let mut config = Config::default();
         config.set_replication_factor(replication_factor);
-        config.bootstrap_interval = None;
+        config.set_bootstrap_interval(None);
         if rng.gen() {
             config.disjoint_query_paths(true);
         }
@@ -1099,7 +1099,7 @@ fn disjoint_query_does_not_finish_before_all_paths_did() {
     config.disjoint_query_paths(true);
     // I.e. setting the amount disjoint paths to be explored to 2.
     config.set_parallelism(NonZeroUsize::new(2).unwrap());
-    config.bootstrap_interval = None;
+    config.set_bootstrap_interval(None);
 
     let mut alice = build_node_with_config(config);
     let mut trudy = build_node(); // Trudy the intrudor, an adversary.
