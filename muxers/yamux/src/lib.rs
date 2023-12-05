73a3f0761539f2def106846d1c1b8777d7abaf81
Max Inden <mail@max-inden.de>
2023-12-05 18:36:36 +0100

    Fix doc link

--- muxers/yamux/src/lib.rs
+++ muxers/yamux/src/lib.rs
@@ -42,7 +42,7 @@ pub struct Muxer<C> {
     connection: Either<yamux012::Connection<C>, yamux013::Connection<C>>,
     /// Temporarily buffers inbound streams in case our node is performing backpressure on the remote.
     ///
-    /// The only way how yamux can make progress is by calling [`yamux::Connection::poll_next_inbound`]. However, the
+    /// The only way how yamux can make progress is by calling [`yamux013::Connection::poll_next_inbound`]. However, the
     /// [`StreamMuxer`] interface is designed to allow a caller to selectively make progress via
     /// [`StreamMuxer::poll_inbound`] and [`StreamMuxer::poll_outbound`] whilst the more general
     /// [`StreamMuxer::poll`] is designed to make progress on existing streams etc.
