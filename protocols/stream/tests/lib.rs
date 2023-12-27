3c3ce7a6d599b94e83d7c5126c1a9a72b7f3a96a
Thomas Eizinger <thomas@eizinger.io>
2023-12-27 21:01:33 +1100

    Add failing test

--- protocols/stream/tests/lib.rs
+++ protocols/stream/tests/lib.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use futures::{AsyncReadExt as _, AsyncWriteExt as _, StreamExt as _};
 use libp2p_stream as stream;
 use libp2p_swarm::{StreamProtocol, Swarm};
@@ -39,4 +41,39 @@ async fn dropping_incoming_streams_deregisters() {
 
     let error = peer_control.open_stream().await.unwrap_err();
     assert!(matches!(error, OpenStreamError::UnsupportedProtocol));
+}
+
+#[tokio::test]
+async fn peer_control_keeps_connection_alive() {
+    let mut swarm1 = Swarm::new_ephemeral(|_| stream::Behaviour::default());
+    let mut swarm2 = Swarm::new_ephemeral(|_| stream::Behaviour::default());
+
+    let mut control = swarm1.behaviour_mut().new_control(PROTOCOL);
+    let mut incoming = swarm2.behaviour_mut().accept(PROTOCOL).unwrap();
+
+    swarm2.listen().with_memory_addr_external().await;
+    swarm1.connect(&mut swarm2).await;
+
+    let swarm2_peer_id = *swarm2.local_peer_id();
+
+    tokio::spawn(async move {
+        while let Some((_, mut stream)) = incoming.next().await {
+            stream.write_all(&[42]).await.unwrap();
+            stream.close().await.unwrap();
+        }
+    });
+    tokio::spawn(swarm1.loop_on_next());
+    tokio::spawn(swarm2.loop_on_next());
+
+    let mut peer_control = control.peer(swarm2_peer_id).await.unwrap();
+
+    // The idle timeout in `new_ephemeral` is 5 seconds.
+    // Hence, if the keep-alive using `PeerControl` does not work, we would fail after this.
+    tokio::time::sleep(Duration::from_secs(6)).await;
+
+    let mut stream = peer_control.open_stream().await.unwrap();
+
+    let mut buf = [0u8; 1];
+    stream.read_exact(&mut buf).await.unwrap();
+    assert_eq!([42], buf);
 }
