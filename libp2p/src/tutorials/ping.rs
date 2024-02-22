4fd4a95b6f1b6baee74bd9b69098bd878d5c6c9f
João Oliveira <hello@jxs.pt>
2024-02-22 19:51:59 +0000

    Update libp2p/src/tutorials/ping.rs

--- libp2p/src/tutorials/ping.rs
+++ libp2p/src/tutorials/ping.rs
@@ -267,7 +267,7 @@
 //!             libp2p::yamux::Config::default,
 //!         )?
 //!         .with_behaviour(|_| ping::Behaviour::default())?
-//!         .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX))) // Allows us to observe pings for a long time.
+//!         .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX))) // Allows us to observe pings indefinitely..
 //!         .build();
 //!
 //!     // Tell the swarm to listen on all interfaces and a random, OS-assigned
