a3692815c6cd7d65d45539ee69652786d770d276
drHuangMHT <DrHuangMHT@outlook.com>
2024-04-01 16:10:33 +0800

    formatting and style consistency

--- examples/metrics/src/http_service.rs
+++ examples/metrics/src/http_service.rs
@@ -27,20 +27,24 @@ use prometheus_client::encoding::text::encode;
 use prometheus_client::registry::Registry;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
+use tokio::net::TcpListener;
 
 const METRICS_CONTENT_TYPE: &str = "application/openmetrics-text;charset=utf-8;version=1.0.0";
 
 pub(crate) async fn metrics_server(registry: Registry) -> Result<(), std::io::Error> {
     // Serve on localhost.
     let addr: SocketAddr = ([127, 0, 0, 1], 0).into();
+    let service = MetricService::new(registry);
     let server = Router::new()
         .route("/metrics", get(respond_with_metrics))
         .fallback(respond_with_404_not_found)
-        .with_state(MetricService::new(registry));
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    let local_addr = listener.local_addr()?;
+        .with_state(service);
+    let tcp_listener = TcpListener::bind(addr).await.unwrap();
+    let local_addr = tcp_listener.local_addr()?;
     tracing::info!(metrics_server=%format!("http://{}/metrics", local_addr));
-    axum::serve(listener, server.into_make_service()).await.unwrap();
+    axum::serve(tcp_listener, server.into_make_service())
+        .await
+        .unwrap();
     Ok(())
 }
 
