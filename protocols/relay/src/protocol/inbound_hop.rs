6c4a02e8b97433d7b1dec88fd3e6a2a8b2723800
Prabhat1308 <prabhatverma329@gmail.com>
2024-07-03 01:52:26 +0530

    add resource limits to CircuitReq
    Signed-off-by: Prabhat1308 <prabhatverma329@gmail.com>

--- protocols/relay/src/protocol/inbound_hop.rs
+++ protocols/relay/src/protocol/inbound_hop.rs
@@ -115,6 +115,8 @@ impl ReservationReq {
 pub struct CircuitReq {
     dst: PeerId,
     substream: Framed<Stream, quick_protobuf_codec::Codec<proto::HopMessage>>,
+    max_circuit_duration: Duration,
+    max_circuit_bytes: u64,
 }
 
 impl CircuitReq {
@@ -127,7 +129,15 @@ impl CircuitReq {
             type_pb: proto::HopMessageType::STATUS,
             peer: None,
             reservation: None,
-            limit: None,
+            limit: Some(proto::Limit {
+                duration: Some(
+                    self.max_circuit_duration
+                        .as_secs()
+                        .try_into()
+                        .expect("`max_circuit_duration` not to exceed `u32::MAX`."),
+                ),
+                data: Some(self.max_circuit_bytes),
+            }),
             status: Some(proto::Status::OK),
         };
 
