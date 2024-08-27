86cb5a8e5ab12a702a06dc34f99938c1720e955c
David E. Perez Negron R. <david@nethunters.xyz>
2024-08-26 23:06:07 -0600

    refactor: use tokio instead of async-std in the distributed-key-value-store example and remove unnecesary dependencies

--- examples/distributed-key-value-store/src/main.rs
+++ examples/distributed-key-value-store/src/main.rs
@@ -20,8 +20,8 @@
 
 #![doc = include_str!("../README.md")]
 
-use async_std::io;
-use futures::{prelude::*, select};
+use tokio::{io, io::AsyncBufReadExt, select};
+use futures::stream::StreamExt;
 use libp2p::kad;
 use libp2p::kad::store::MemoryStore;
 use libp2p::kad::Mode;
@@ -34,7 +34,7 @@ use std::error::Error;
 use std::time::Duration;
 use tracing_subscriber::EnvFilter;
 
-#[async_std::main]
+#[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let _ = tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
@@ -44,11 +44,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     #[derive(NetworkBehaviour)]
     struct Behaviour {
         kademlia: kad::Behaviour<MemoryStore>,
-        mdns: mdns::async_io::Behaviour,
+        mdns: mdns::tokio::Behaviour,
     }
 
     let mut swarm = libp2p::SwarmBuilder::with_new_identity()
-        .with_async_std()
+        .with_tokio()
         .with_tcp(
             tcp::Config::default(),
             noise::Config::new,
@@ -60,7 +60,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     key.public().to_peer_id(),
                     MemoryStore::new(key.public().to_peer_id()),
                 ),
-                mdns: mdns::async_io::Behaviour::new(
+                mdns: mdns::tokio::Behaviour::new(
                     mdns::Config::default(),
                     key.public().to_peer_id(),
                 )?,
@@ -72,7 +72,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     swarm.behaviour_mut().kademlia.set_mode(Some(Mode::Server));
 
     // Read full lines from stdin
-    let mut stdin = io::BufReader::new(io::stdin()).lines().fuse();
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
 
     // Listen on all interfaces and whatever port the OS assigns.
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
@@ -80,7 +80,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Kick it off.
     loop {
         select! {
-        line = stdin.select_next_some() => handle_input_line(&mut swarm.behaviour_mut().kademlia, line.expect("Stdin not to close")),
+        line = stdin.next_line() => handle_input_line(&mut swarm.behaviour_mut().kademlia,
+        line.expect("Stdin not to close").expect("EOL")),
         event = swarm.select_next_some() => match event {
             SwarmEvent::NewListenAddr { address, .. } => {
                 println!("Listening in {address:?}");
@@ -142,6 +143,68 @@ async fn main() -> Result<(), Box<dyn Error>> {
             }
             _ => {}
         }
+        //line = stdin.select_next_some() => handle_input_line(&mut swarm.behaviour_mut().kademlia, line.expect("Stdin not to close")),
+        //event = swarm.select_next_some() => match event {
+        //    SwarmEvent::NewListenAddr { address, .. } => {
+        //        println!("Listening in {address:?}");
+        //    },
+        //    SwarmEvent::Behaviour(BehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+        //        for (peer_id, multiaddr) in list {
+        //            swarm.behaviour_mut().kademlia.add_address(&peer_id, multiaddr);
+        //        }
+        //    }
+        //    SwarmEvent::Behaviour(BehaviourEvent::Kademlia(kad::Event::OutboundQueryProgressed { result, ..})) => {
+        //        match result {
+        //            kad::QueryResult::GetProviders(Ok(kad::GetProvidersOk::FoundProviders { key, providers, .. })) => {
+        //                for peer in providers {
+        //                    println!(
+        //                        "Peer {peer:?} provides key {:?}",
+        //                        std::str::from_utf8(key.as_ref()).unwrap()
+        //                    );
+        //                }
+        //            }
+        //            kad::QueryResult::GetProviders(Err(err)) => {
+        //                eprintln!("Failed to get providers: {err:?}");
+        //            }
+        //            kad::QueryResult::GetRecord(Ok(
+        //                kad::GetRecordOk::FoundRecord(kad::PeerRecord {
+        //                    record: kad::Record { key, value, .. },
+        //                    ..
+        //                })
+        //            )) => {
+        //                println!(
+        //                    "Got record {:?} {:?}",
+        //                    std::str::from_utf8(key.as_ref()).unwrap(),
+        //                    std::str::from_utf8(&value).unwrap(),
+        //                );
+        //            }
+        //            kad::QueryResult::GetRecord(Ok(_)) => {}
+        //            kad::QueryResult::GetRecord(Err(err)) => {
+        //                eprintln!("Failed to get record: {err:?}");
+        //            }
+        //            kad::QueryResult::PutRecord(Ok(kad::PutRecordOk { key })) => {
+        //                println!(
+        //                    "Successfully put record {:?}",
+        //                    std::str::from_utf8(key.as_ref()).unwrap()
+        //                );
+        //            }
+        //            kad::QueryResult::PutRecord(Err(err)) => {
+        //                eprintln!("Failed to put record: {err:?}");
+        //            }
+        //            kad::QueryResult::StartProviding(Ok(kad::AddProviderOk { key })) => {
+        //                println!(
+        //                    "Successfully put provider record {:?}",
+        //                    std::str::from_utf8(key.as_ref()).unwrap()
+        //                );
+        //            }
+        //            kad::QueryResult::StartProviding(Err(err)) => {
+        //                eprintln!("Failed to put provider record: {err:?}");
+        //            }
+        //            _ => {}
+        //        }
+        //    }
+        //    _ => {}
+        //}
         }
     }
 }
