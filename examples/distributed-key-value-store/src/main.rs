5f21116345478ccc8ecc3e4f1d85137487d80f46
David E. Perez Negron R. <david@nethunters.xyz>
2024-08-26 23:24:00 -0600

    cleanup comments and format

--- examples/distributed-key-value-store/src/main.rs
+++ examples/distributed-key-value-store/src/main.rs
@@ -20,7 +20,6 @@
 
 #![doc = include_str!("../README.md")]
 
-use tokio::{io, io::AsyncBufReadExt, select};
 use futures::stream::StreamExt;
 use libp2p::kad;
 use libp2p::kad::store::MemoryStore;
@@ -32,6 +31,7 @@ use libp2p::{
 };
 use std::error::Error;
 use std::time::Duration;
+use tokio::{io, io::AsyncBufReadExt, select};
 use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
@@ -143,68 +143,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
             }
             _ => {}
         }
-        //line = stdin.select_next_some() => handle_input_line(&mut swarm.behaviour_mut().kademlia, line.expect("Stdin not to close")),
-        //event = swarm.select_next_some() => match event {
-        //    SwarmEvent::NewListenAddr { address, .. } => {
-        //        println!("Listening in {address:?}");
-        //    },
-        //    SwarmEvent::Behaviour(BehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
-        //        for (peer_id, multiaddr) in list {
-        //            swarm.behaviour_mut().kademlia.add_address(&peer_id, multiaddr);
-        //        }
-        //    }
-        //    SwarmEvent::Behaviour(BehaviourEvent::Kademlia(kad::Event::OutboundQueryProgressed { result, ..})) => {
-        //        match result {
-        //            kad::QueryResult::GetProviders(Ok(kad::GetProvidersOk::FoundProviders { key, providers, .. })) => {
-        //                for peer in providers {
-        //                    println!(
-        //                        "Peer {peer:?} provides key {:?}",
-        //                        std::str::from_utf8(key.as_ref()).unwrap()
-        //                    );
-        //                }
-        //            }
-        //            kad::QueryResult::GetProviders(Err(err)) => {
-        //                eprintln!("Failed to get providers: {err:?}");
-        //            }
-        //            kad::QueryResult::GetRecord(Ok(
-        //                kad::GetRecordOk::FoundRecord(kad::PeerRecord {
-        //                    record: kad::Record { key, value, .. },
-        //                    ..
-        //                })
-        //            )) => {
-        //                println!(
-        //                    "Got record {:?} {:?}",
-        //                    std::str::from_utf8(key.as_ref()).unwrap(),
-        //                    std::str::from_utf8(&value).unwrap(),
-        //                );
-        //            }
-        //            kad::QueryResult::GetRecord(Ok(_)) => {}
-        //            kad::QueryResult::GetRecord(Err(err)) => {
-        //                eprintln!("Failed to get record: {err:?}");
-        //            }
-        //            kad::QueryResult::PutRecord(Ok(kad::PutRecordOk { key })) => {
-        //                println!(
-        //                    "Successfully put record {:?}",
-        //                    std::str::from_utf8(key.as_ref()).unwrap()
-        //                );
-        //            }
-        //            kad::QueryResult::PutRecord(Err(err)) => {
-        //                eprintln!("Failed to put record: {err:?}");
-        //            }
-        //            kad::QueryResult::StartProviding(Ok(kad::AddProviderOk { key })) => {
-        //                println!(
-        //                    "Successfully put provider record {:?}",
-        //                    std::str::from_utf8(key.as_ref()).unwrap()
-        //                );
-        //            }
-        //            kad::QueryResult::StartProviding(Err(err)) => {
-        //                eprintln!("Failed to put provider record: {err:?}");
-        //            }
-        //            _ => {}
-        //        }
-        //    }
-        //    _ => {}
-        //}
         }
     }
 }
