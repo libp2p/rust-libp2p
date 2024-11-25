d8e39578a7b092fe8ca42e46d22636aba63c3780
João Oliveira <hello@jxs.pt>
2024-11-25 12:02:42 +0000

    add dots to peer_score comments

--- protocols/gossipsub/src/peer_score.rs
+++ protocols/gossipsub/src/peer_score.rs
@@ -337,14 +337,14 @@ impl PeerScore {
             }
         }
 
-        // P7: behavioural pattern penalty
+        // P7: behavioural pattern penalty.
         if peer_stats.behaviour_penalty > self.params.behaviour_penalty_threshold {
             let excess = peer_stats.behaviour_penalty - self.params.behaviour_penalty_threshold;
             let p7 = excess * excess;
             score += p7 * self.params.behaviour_penalty_weight;
         }
 
-        // Slow peer weighting
+        // Slow peer weighting.
         if peer_stats.slow_peer_penalty > self.params.slow_peer_threshold {
             let excess = peer_stats.slow_peer_penalty - self.params.slow_peer_threshold;
             score += excess * self.params.slow_peer_weight;
