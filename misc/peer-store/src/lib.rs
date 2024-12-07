fbc134461e75ce9e92bf32daff76b985fad7e8c3
drHuangMHT <DrHuangMHT@outlook.com>
2024-12-07 15:34:57 +0800

    formatting

--- misc/peer-store/src/lib.rs
+++ misc/peer-store/src/lib.rs
@@ -7,18 +7,18 @@ pub use behaviour::{Behaviour, Event};
 use libp2p_core::Multiaddr;
 pub use store::{MemoryStore, Store};
 
-pub struct AddressRecord<'a>{
+pub struct AddressRecord<'a> {
     address: &'a Multiaddr,
     last_seen: &'a SystemTime,
 }
-impl<'a> AddressRecord<'a>{
+impl<'a> AddressRecord<'a> {
     /// The address of this record.
-    pub fn address(&self)->&Multiaddr{
+    pub fn address(&self) -> &Multiaddr {
         self.address
     }
     /// How much time has passed since the address is last reported wrt. current time.  
     /// This may fail because of system time change.
-    pub fn last_seen(&self)->Result<std::time::Duration, std::time::SystemTimeError>{
+    pub fn last_seen(&self) -> Result<std::time::Duration, std::time::SystemTimeError> {
         self.last_seen.elapsed()
     }
 }
