7add7b7be2f2c13702c552a9e6599226dc6416c3
drHuangMHT <DrHuangMHT@outlook.com>
2024-12-09 22:29:29 +0800

    reorder cfg flags

--- protocols/gossipsub/src/rpc.rs
+++ protocols/gossipsub/src/rpc.rs
@@ -96,14 +96,14 @@ impl Sender {
         sender.try_send(rpc).map_err(|err| err.into_inner())
     }
 
-    #[cfg(feature = "metrics")]
     /// Returns the current size of the priority queue.
+    #[cfg(feature = "metrics")]
     pub(crate) fn priority_queue_len(&self) -> usize {
         self.len.load(Ordering::Relaxed)
     }
 
-    #[cfg(feature = "metrics")]
     /// Returns the current size of the non-priority queue.
+    #[cfg(feature = "metrics")]
     pub(crate) fn non_priority_queue_len(&self) -> usize {
         self.non_priority_sender.len()
     }
