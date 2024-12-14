05f66ad73bf6a40bd54e40164ddc5ee3cd1a0067
kamuik16 <krishang.s@ahduni.edu.in>
2024-12-14 16:00:05 +0530

    chore: add description

--- misc/logging/src/lib.rs
+++ misc/logging/src/lib.rs
@@ -1,9 +1,12 @@
 pub use tracing_subscriber::EnvFilter;
 
+/// Initializes logging with the default environment filter (`RUST_LOG`).
 pub fn with_default_env_filter() {
     with_env_filter(EnvFilter::from_default_env());
 }
 
+/// Initializes logging with a custom environment filter.
+/// Logs are written to standard error (`stderr`).
 pub fn with_env_filter(filter: impl Into<EnvFilter>) {
     let _ = tracing_subscriber::fmt()
         .with_env_filter(filter)
