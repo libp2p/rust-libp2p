f41e8fa149ea9490f7d1093b9242d62dcf65702a
dgarus <garus.d.g@gmail.com>
2024-12-20 11:28:05 +0300

    mod transport

--- transports/webtransport/src/transport.rs
+++ transports/webtransport/src/transport.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
+use std::fmt;
 use std::future::Pending;
-use std::net::{IpAddr, SocketAddr};
+use std::net::SocketAddr;
 use std::pin::Pin;
 use std::sync::Arc;
 use std::task::{Context, Poll, Waker};
@@ -8,23 +9,16 @@ use std::time::Duration;
 
 use futures::{prelude::*, stream::SelectAll};
 use futures::future::BoxFuture;
-use if_watch::IfEvent;
-use if_watch::tokio::IfWatcher;
-use wtransport::config::TlsServerConfig;
-use wtransport::endpoint::{endpoint_side, IncomingSession};
-use wtransport::error::ConnectionError;
-use wtransport::ServerConfig;
+use wtransport::{config::TlsServerConfig, ServerConfig};
+use wtransport::endpoint::{Endpoint, endpoint_side::Server, IncomingSession};
 
-use libp2p_core::{Multiaddr, Transport};
-use libp2p_core::multiaddr::Protocol;
-use libp2p_core::multihash::Multihash;
-use libp2p_core::muxing::StreamMuxerBox;
+use libp2p_core::{Multiaddr, multiaddr::Protocol, multihash::Multihash, Transport};
 use libp2p_core::transport::{DialOpts, ListenerId, TransportError, TransportEvent};
 use libp2p_identity::{Keypair, PeerId};
 
-use crate::Connecting;
 use crate::certificate::CertHash;
 use crate::config::Config;
+use crate::Connecting;
 use crate::connection::Connection;
 use crate::Error;
 
@@ -66,6 +60,7 @@ impl GenTransport {
         (SocketAddr, Option<PeerId>),
         TransportError<<Self as Transport>::Error>,
     > {
+        //todo rewrite: addr.clone() should be avoided.
         let (socket_addr, peer_id) = multiaddr_to_socketaddr(&addr)
             .ok_or_else(|| TransportError::MultiaddrNotSupported(addr.clone()))?;
         if check_unspecified_addr && (socket_addr.port() == 0 || socket_addr.ip().is_unspecified())
@@ -85,7 +80,7 @@ impl Transport for GenTransport {
     fn listen_on(&mut self, id: ListenerId, addr: Multiaddr) -> Result<(), TransportError<Self::Error>> {
         let (socket_addr, _peer_id) = self.remote_multiaddr_to_socketaddr(addr, false)?;
         let config = ServerConfig::builder()
-            .with_bind_address(socket_addr.clone())
+            .with_bind_address(socket_addr)
             .with_custom_tls(self.server_tls_config.clone())
             .build();
 
@@ -96,7 +91,7 @@ impl Transport for GenTransport {
         let handshake_timeout = self.handshake_timeout.clone();
 
         let listener = Listener::new(
-            id, socket_addr, Arc::new(endpoint), keypair, cert_hashes, handshake_timeout,
+            id, endpoint, keypair, cert_hashes, handshake_timeout,
         )?;
         self.listeners.push(listener);
 
@@ -108,15 +103,37 @@ impl Transport for GenTransport {
     }
 
     fn remove_listener(&mut self, id: ListenerId) -> bool {
-        todo!()
+        if let Some(listener) = self.listeners.iter_mut().find(|l| l.listener_id == id) {
+            // Close the listener, which will eventually finish its stream.
+            // `SelectAll` removes streams once they are finished.
+            listener.close(Ok(()));
+            true
+        } else {
+            false
+        }
     }
 
     fn dial(&mut self, _addr: Multiaddr, _opts: DialOpts) -> Result<Self::Dial, TransportError<Self::Error>> {
         panic!("Dial operation is not supported!")
     }
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<TransportEvent<Self::ListenerUpgrade, Self::Error>> {
-        todo!()
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>)
+            -> Poll<TransportEvent<Self::ListenerUpgrade, Self::Error>>
+    {
+        if let Poll::Ready(Some(ev)) = self.listeners.poll_next_unpin(cx) {
+            return Poll::Ready(ev);
+        }
+
+        match &self.waker {
+            None => self.waker = Some(cx.waker().clone()),
+            Some(waker) => {
+                if !waker.will_wake(cx.waker()) {
+                    self.waker = Some(cx.waker().clone());
+                }
+            }
+        };
+
+        Poll::Pending
     }
 }
 
@@ -125,15 +142,11 @@ struct Listener {
     /// Id of the listener.
     listener_id: ListenerId,
     /// Endpoint
-    endpoint: Arc<wtransport::Endpoint<endpoint_side::Server>>,
+    endpoint: Arc<Endpoint<Server>>,
     /// A future to poll new incoming connections.
     accept: BoxFuture<'static, IncomingSession>,
     /// Timeout for connection establishment on inbound connections.
     handshake_timeout: Duration,
-    /// Watcher for network interface changes.
-    ///
-    /// None if we are only listening on a single interface.
-    if_watcher: Option<IfWatcher>,
     /// Whether the listener was closed and the stream should terminate.
     is_closed: bool,
     /// Pending event to reported.
@@ -141,55 +154,52 @@ struct Listener {
     /// The stream must be to awaken after it has been closed to deliver the last event.
     close_listener_waker: Option<Waker>,
 
-    listening_addresses: HashSet<IpAddr>,
-
     keypair: Keypair,
+
     cert_hashes: Vec<CertHash>,
 }
 
 impl Listener {
     fn new(
         listener_id: ListenerId,
-        socket_addr: SocketAddr,
-        endpoint: Arc<wtransport::Endpoint<endpoint_side::Server>>,
+        endpoint: Endpoint<Server>,
         keypair: &Keypair,
         cert_hashes: &Vec<CertHash>,
         handshake_timeout: Duration, ) -> Result<Self, Error> {
-        let if_watcher;
-        let pending_event;
-        let mut listening_addresses = HashSet::new();
-        if socket_addr.ip().is_unspecified() {
-            if_watcher = Some(if_watch::tokio::IfWatcher::new()?);
-            pending_event = None;
-        } else {
-            if_watcher = None;
-            listening_addresses.insert(socket_addr.ip());
-            let ma = socketaddr_to_multiaddr(&socket_addr, cert_hashes);
-            pending_event = Some(TransportEvent::NewAddress {
-                listener_id,
-                listen_addr: ma,
-            })
-        }
+        let endpoint = Arc::new(endpoint);
+        let c_endpoint = Arc::clone(&endpoint);
+        let accept = async move { c_endpoint.accept().await }.boxed();
 
-
-        let endpoint_c = Arc::clone(&endpoint);
-        let accept = async move { endpoint_c.accept().await }.boxed();
-
-        Ok(
-            Listener {
+        Ok(Listener {
                 listener_id,
                 endpoint,
                 accept,
                 handshake_timeout,
-                if_watcher,
                 is_closed: false,
-                pending_event,
+                pending_event: None,
                 close_listener_waker: None,
-                listening_addresses,
                 keypair: keypair.clone(),
                 cert_hashes: cert_hashes.clone(),
-            }
-        )
+        })
+    }
+
+    /// Report the listener as closed in a [`TransportEvent::ListenerClosed`] and
+    /// terminate the stream.
+    fn close(&mut self, reason: Result<(), Error>) {
+        if self.is_closed {
+            return;
+        }
+        self.endpoint.close(From::from(0u32), &[]);
+        self.pending_event = Some(TransportEvent::ListenerClosed {
+            listener_id: self.listener_id,
+            reason,
+        });
+        self.is_closed = true;
+
+        // Wake the stream to deliver the last event.
+        if let Some(waker) = self.close_listener_waker.take() {
+            waker.wake();
+        }
     }
 
     fn socket_addr(&self) -> SocketAddr {
@@ -199,63 +209,12 @@ impl Listener {
     }
 
     fn noise_config(&self) -> libp2p_noise::Config {
-        let mut res = libp2p_noise::Config::new(&self.keypair).expect("Getting a noise config");
-        // let mut set = HashSet::with_capacity(self.cert_hashes.len());
+        let res = libp2p_noise::Config::new(&self.keypair).expect("Getting a noise config");
         let set = self.cert_hashes.iter().cloned().collect::<HashSet<_>>();
         let res = res.with_webtransport_certhashes(set);
 
         res
     }
-
-
-    /* todo poll_if_addr
-    /// Poll for a next If Event.
-    fn poll_if_addr(&mut self, cx: &mut Context<'_>) -> Poll<<Self as Stream>::Item> {
-        let endpoint_addr = self.socket_addr();
-        let Some(if_watcher) = self.if_watcher.as_mut() else {
-            return Poll::Pending;
-        };
-        loop {
-            match ready!(P::poll_if_event(if_watcher, cx)) {
-                Ok(IfEvent::Up(inet)) => {
-                    if let Some(listen_addr) =
-                        ip_to_listenaddr(&endpoint_addr, inet.addr(), self.version)
-                    {
-                        tracing::debug!(
-                            address=%listen_addr,
-                            "New listen address"
-                        );
-                        self.listening_addresses.insert(inet.addr());
-                        return Poll::Ready(TransportEvent::NewAddress {
-                            listener_id: self.listener_id,
-                            listen_addr,
-                        });
-                    }
-                }
-                Ok(IfEvent::Down(inet)) => {
-                    if let Some(listen_addr) =
-                        ip_to_listenaddr(&endpoint_addr, inet.addr(), self.version)
-                    {
-                        tracing::debug!(
-                            address=%listen_addr,
-                            "Expired listen address"
-                        );
-                        self.listening_addresses.remove(&inet.addr());
-                        return Poll::Ready(TransportEvent::AddressExpired {
-                            listener_id: self.listener_id,
-                            listen_addr,
-                        });
-                    }
-                }
-                Err(err) => {
-                    return Poll::Ready(TransportEvent::ListenerError {
-                        listener_id: self.listener_id,
-                        error: err.into(),
-                    })
-                }
-            }
-        }
-    }*/
 }
 
 impl Stream for Listener {
@@ -269,30 +228,15 @@ impl Stream for Listener {
             if self.is_closed {
                 return Poll::Ready(None);
             }
-            /* todo poll_if_addr
-            if let Poll::Ready(event) = self.poll_if_addr(cx) {
-                return Poll::Ready(Some(event));
-            }*/
 
             match self.accept.poll_unpin(cx) {
                 Poll::Ready(incoming_session) => {
-                    let endpoint = self.endpoint.clone();
+                    let endpoint = Arc::clone(&self.endpoint);
                     self.accept = async move { endpoint.accept().await }.boxed();
-
-                    /*let connecting = match session_request.accept() {
-                        Ok(connecting) => connecting,
-                        Err(error) => {
-                            return Poll::Ready(Some(TransportEvent::ListenerError {
-                                listener_id: self.listener_id,
-                                error: Error::Connection(crate::ConnectionError(error)),
-                            }))
-                        }
-                    };*/
-
-                    let local_addr = socketaddr_to_multiaddr(&self.socket_addr(), &self.cert_hashes);
+                    let mut local_addr = socketaddr_to_multiaddr(&self.socket_addr());
+                    local_addr = add_hashes(local_addr, &self.cert_hashes);
                     let remote_addr = incoming_session.remote_address();
-                    //todo нафига к удаленному адресу добавлять хэши сертификатов?
-                    let send_back_addr = socketaddr_to_multiaddr(&remote_addr, &self.cert_hashes);
+                    let send_back_addr = socketaddr_to_multiaddr(&remote_addr);
                     let noise = self.noise_config();
 
                     let event = TransportEvent::Incoming {
@@ -306,38 +250,61 @@ impl Stream for Listener {
                 Poll::Pending => {}
             };
 
-            self.close_listener_waker = Some(cx.waker().clone());
+            match &self.close_listener_waker {
+                None => self.close_listener_waker = Some(cx.waker().clone()),
+                Some(waker) => {
+                    if !waker.will_wake(cx.waker()) {
+                        self.close_listener_waker = Some(cx.waker().clone())
+                    }
+                },
+            }
 
             return Poll::Pending;
         }
     }
 }
 
+impl fmt::Debug for Listener {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Listener")
+            .field("listener_id", &self.listener_id)
+            .field("handshake_timeout", &self.handshake_timeout)
+            .field("is_closed", &self.is_closed)
+            .field("pending_event", &self.pending_event)
+            .finish()
+    }
+}
+
 /// Turns an IP address and port into the corresponding WebTransport multiaddr.
-fn socketaddr_to_multiaddr(socket_addr: &SocketAddr, hashes: &Vec<Multihash<64>>) -> Multiaddr {
-    let mut vec = hashes.clone();
-    let mut res = Multiaddr::empty()
+fn socketaddr_to_multiaddr(socket_addr: &SocketAddr) -> Multiaddr {
+    Multiaddr::empty()
         .with(socket_addr.ip().into())
         .with(Protocol::Udp(socket_addr.port()))
         .with(Protocol::QuicV1)
         .with(Protocol::WebTransport)
-        .with(Protocol::Certhash(
-            vec.pop().expect("Gets the last element"),
-        ));
-    if !vec.is_empty() {
-        res = res.with(Protocol::Certhash(
-            vec.pop().expect("Gets the last element"),
-        ));
-    };
+}
 
-    res
+fn add_hashes(m_addr: Multiaddr, hashes: &Vec<Multihash<64>>) -> Multiaddr {
+    if !hashes.is_empty() {
+        let mut vec = hashes.clone();
+        let mut res = m_addr.with(Protocol::Certhash(
+            vec.pop().expect("Gets the last element"),
+        ));
+        if !vec.is_empty() {
+            res = res.with(Protocol::Certhash(
+                vec.pop().expect("Gets the last element"),
+            ));
+        };
+
+        res
+    } else {
+        m_addr
+    }
 }
 
 /// Tries to turn a Webtransport multiaddress into a UDP [`SocketAddr`]. Returns None if the format
 /// of the multiaddr is wrong.
-fn multiaddr_to_socketaddr(
-    addr: &Multiaddr,
-) -> Option<(SocketAddr, Option<PeerId>)> {
+fn multiaddr_to_socketaddr(addr: &Multiaddr) -> Option<(SocketAddr, Option<PeerId>)> {
     let mut iter = addr.iter();
     let proto1 = iter.next()?;
     let proto2 = iter.next()?;
@@ -385,10 +352,22 @@ fn multiaddr_to_socketaddr(
 }
 
 #[cfg(test)]
-// #[cfg(any(feature = "async-std", feature = "tokio"))]
 mod test {
+    use time::ext::NumericalDuration;
+    use time::OffsetDateTime;
+
+    use crate::certificate::Certificate;
+
+    use super::*;
+
     #[test]
     fn test() {
-        println!("hi!")
+        let keypair = Keypair::generate_ed25519();
+        let not_before = OffsetDateTime::now_utc().checked_sub(1.days()).unwrap();
+        let cert = Certificate::generate(&keypair, not_before).expect("Generate certificate");
+
+        let config = Config::new(&keypair, cert);
+        let mut transport = GenTransport::new(config);
+        // todo write few tests: closing a listener, addresses parsing and so on
     }
 }
