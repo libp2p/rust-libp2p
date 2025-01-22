2ceb92f780d062a4088e3c157fd8d7e5abf58455
dgarus <garus.d.g@gmail.com>
2025-01-22 11:29:18 +0300

    fixed clippy errors

--- transports/webtransport/tests/smoke.rs
+++ transports/webtransport/tests/smoke.rs
@@ -1,22 +1,24 @@
-mod client;
-
-use crate::client::WtClient;
 use futures::channel::mpsc;
 use futures::stream::StreamExt;
 use futures::{future, AsyncReadExt, AsyncWriteExt, SinkExt};
 use libp2p_core::multiaddr::Protocol;
 use libp2p_core::muxing::{StreamMuxerBox, StreamMuxerExt};
 use libp2p_core::transport::{Boxed, ListenerId, TransportEvent};
-use libp2p_core::{Multiaddr, Transport};
+use libp2p_core::upgrade::OutboundConnectionUpgrade;
+use libp2p_core::{Multiaddr, Transport, UpgradeInfo};
 use libp2p_identity::{Keypair, PeerId};
 use libp2p_webtransport as webtransport;
-use libp2p_webtransport::{CertHash, Certificate};
+use libp2p_webtransport::{CertHash, Certificate, Stream};
 use std::collections::HashSet;
+use std::io;
 use std::net::SocketAddr;
+use std::sync::Arc;
 use std::time::Duration;
 use time::ext::NumericalDuration;
 use time::OffsetDateTime;
 use tracing_subscriber::EnvFilter;
+use wtransport::error::{ConnectingError, ConnectionError, StreamOpeningError};
+use wtransport::{ClientConfig, Connection, Endpoint};
 
 #[tokio::test]
 async fn smoke() {
@@ -133,4 +135,104 @@ fn generate_keypair_and_certificate() -> (Keypair, webtransport::Certificate, Ha
     certhashes.insert(hash);
 
     (keypair, cert, certhashes)
+}
+
+#[derive(Debug, thiserror::Error)]
+enum ClientError {
+    #[error(transparent)]
+    Io(#[from] io::Error),
+
+    #[error(transparent)]
+    Connecting(#[from] ConnectingError),
+
+    #[error(transparent)]
+    Connection(#[from] ConnectionError),
+
+    #[error(transparent)]
+    Stream(#[from] StreamOpeningError),
+}
+
+struct WtClient {
+    url: String,
+    remote_peer_id: PeerId,
+    keypair: Keypair,
+    certhashes: HashSet<CertHash>,
+    connection: Option<Arc<Connection>>,
+}
+
+impl WtClient {
+    fn new(url: String, remote_peer_id: PeerId, certhashes: HashSet<CertHash>) -> Self {
+        let keypair = Keypair::generate_ed25519();
+        WtClient {
+            url,
+            remote_peer_id,
+            keypair,
+            certhashes,
+            connection: None,
+        }
+    }
+
+    async fn connect(&mut self) -> Result<Stream, ClientError> {
+        let client_tls = libp2p_tls::make_webtransport_client_config(
+            Some(self.remote_peer_id),
+            alpn_protocols(),
+        );
+
+        let config = ClientConfig::builder()
+            .with_bind_default()
+            .with_custom_tls(client_tls)
+            .build();
+
+        tracing::debug!("Connecting to {}", self.url.as_str());
+
+        let con = Endpoint::client(config)?.connect(self.url.as_str()).await?;
+        self.connection = Some(Arc::new(con));
+
+        tracing::debug!("Connection is established to {}", self.url.as_str());
+
+        self.authenticate().await?;
+        let stream = self.create_bidirectional_stream().await?;
+
+        Ok(stream)
+    }
+
+    async fn create_bidirectional_stream(&mut self) -> Result<Stream, ClientError> {
+        match &self.connection {
+            None => {
+                panic!("There is no open connection!")
+            }
+            Some(arc_con) => {
+                let con = Arc::clone(arc_con);
+                let opening_bi_stream = con.open_bi().await?;
+
+                tracing::debug!("Opening bi stream is created");
+
+                let (send, recv) = opening_bi_stream.await?;
+                let stream = webtransport::Stream::new(send, recv);
+
+                tracing::debug!("Stream is created");
+
+                Ok(stream)
+            }
+        }
+    }
+
+    async fn authenticate(&mut self) -> Result<(), ClientError> {
+        let stream = self.create_bidirectional_stream().await?;
+        let mut noise = libp2p_noise::Config::new(&self.keypair).unwrap();
+
+        if !self.certhashes.is_empty() {
+            noise = noise.with_webtransport_certhashes(self.certhashes.clone());
+        }
+
+        tracing::debug!("Noise upgrade_outbound");
+        let info = noise.protocol_info().next().unwrap_or_default();
+        let _ = noise.upgrade_outbound(stream, info).await.unwrap();
+
+        Ok(())
+    }
+}
+
+fn alpn_protocols() -> Vec<Vec<u8>> {
+    vec![b"libp2p".to_vec(), b"h3".to_vec()]
 }
