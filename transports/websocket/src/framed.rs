77d9a8ff0eac34dc0870919eb555e7a899710d8a
shiyasmohd <shiyasjaseena124@gmail.com>
2025-02-20 08:17:07 +0530

    chore(websocket): add deprecate msg for WsConfig in framed.rs

--- transports/websocket/src/framed.rs
+++ transports/websocket/src/framed.rs
@@ -52,6 +52,9 @@ const MAX_DATA_SIZE: usize = 256 * 1024 * 1024;
 /// A Websocket transport whose output type is a [`Stream`] and [`Sink`] of
 /// frame payloads which does not implement [`AsyncRead`] or
 /// [`AsyncWrite`]. See [`crate::Config`] if you require the latter.
+#[deprecated = "Use `Config` instead"]
+pub type WsConfig<T> = Config<T>;
+
 #[derive(Debug)]
 pub struct Config<T> {
     transport: Arc<Mutex<T>>,
