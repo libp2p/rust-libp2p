794102b1b8ca3857673d917806f1a87328678908
kamuik16 <krishang.s@ahduni.edu.in>
2025-05-27 14:51:17 +0530

    chore: use tokio instead of async_std

--- swarm/tests/connection_close.rs
+++ swarm/tests/connection_close.rs
@@ -12,7 +12,7 @@ use libp2p_swarm::{
 };
 use libp2p_swarm_test::SwarmExt;
 
-#[async_std::test]
+#[tokio::test]
 async fn sends_remaining_events_to_behaviour_on_connection_close() {
     let mut swarm1 = Swarm::new_ephemeral(|_| Behaviour::new(3));
     let mut swarm2 = Swarm::new_ephemeral(|_| Behaviour::new(3));
