8ef97dc6f03ceb7792144c9430877c642a95754d
Surkov, Kirill <kirill.surkov@intel.com>
2025-09-03 18:04:51 +0300

    Add a comment

--- identity/build.rs
+++ identity/build.rs
@@ -1,5 +1,6 @@
 fn main() {
     cfg_aliases::cfg_aliases! {
+        // Only native targets and `wasm32-unknown-unknown` are supported by `ring` crate.
         rsa_supported: { any(not(target_arch = "wasm32"), target_os = "unknown") }
     }
 }
