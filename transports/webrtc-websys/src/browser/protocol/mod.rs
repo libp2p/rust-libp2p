a9d98acd7fef937df905e8b469fda9306b7ad1ab
Elijah Hampton <elijahhamptonj@proton.me>
2025-10-30 14:50:47 -0400

    Rebase complete

--- transports/webrtc-websys/src/browser/protocol/mod.rs
+++ transports/webrtc-websys/src/browser/protocol/mod.rs
@@ -3,6 +3,8 @@ pub(crate) mod signaling;
 pub(crate) mod upgrade;
 pub(crate) mod protocol;
 
+pub(crate) use self::signaling::ConnectionCallbacks;
+
 pub use protocol::{SIGNALING_PROTOCOL_ID, SIGNALING_STREAM_PROTOCOL};
-pub use signaling::{ConnectionCallbacks, ProtocolHandler, Signaling};
+pub use signaling::{ProtocolHandler, Signaling};
 pub use upgrade::SignalingProtocolUpgrade;
