f9607883f00ca9474e3143805e195054986702a7
momosh <xtopala+cryptogit@gmail.com>
2025-11-04 20:41:04 +0100

    revert clippy and intra-doc changes

--- protocols/kad/src/kbucket/key.rs
+++ protocols/kad/src/kbucket/key.rs
@@ -18,7 +18,6 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 #![allow(clippy::manual_div_ceil)]
-#![allow(clippy::double_parens)]
 
 use std::{
     borrow::Borrow,
