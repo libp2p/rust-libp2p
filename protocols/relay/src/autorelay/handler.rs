aa31f50863d8dc9d7e30b63ad672a17b5468fc20
Darius Clark <darius.clark@proton.me>
2026-06-04 06:53:32 -0500

    chore: use `ConnectionEvent::RemoteProtocolChange` only

--- protocols/relay/src/autorelay/handler.rs
+++ protocols/relay/src/autorelay/handler.rs
@@ -77,36 +77,30 @@ impl ConnectionHandler for Handler {
             Self::OutboundOpenInfo,
         >,
     ) {
-        match event {
-            ConnectionEvent::RemoteProtocolsChange(protocol)
-            | ConnectionEvent::LocalProtocolsChange(protocol) => {
-                let change = self.supported_protocol.on_protocols_change(protocol);
-                if change {
-                    let valid = self
-                        .supported_protocol
-                        .iter()
-                        .any(|proto| HOP_PROTOCOL_NAME.eq(proto));
+        if let ConnectionEvent::RemoteProtocolsChange(protocol) = event {
+            let change = self.supported_protocol.on_protocols_change(protocol);
+            if change {
+                let valid = self
+                    .supported_protocol
+                    .iter()
+                    .any(|proto| HOP_PROTOCOL_NAME.eq(proto));
 
-                    match (valid, self.supported) {
-                        (true, false) => {
-                            self.supported = true;
-                            self.events
-                                .push_back(ConnectionHandlerEvent::NotifyBehaviour(Out::Supported));
-                        }
-                        (false, true) => {
-                            self.supported = false;
-                            self.blacklist_timer = None;
-                            self.events
-                                .push_back(ConnectionHandlerEvent::NotifyBehaviour(
-                                    Out::Unsupported,
-                                ));
-                        }
-                        (true, true) => {}
-                        _ => {}
+                match (valid, self.supported) {
+                    (true, false) => {
+                        self.supported = true;
+                        self.events
+                            .push_back(ConnectionHandlerEvent::NotifyBehaviour(Out::Supported));
                     }
+                    (false, true) => {
+                        self.supported = false;
+                        self.blacklist_timer = None;
+                        self.events
+                            .push_back(ConnectionHandlerEvent::NotifyBehaviour(Out::Unsupported));
+                    }
+                    (true, true) => {}
+                    _ => {}
                 }
             }
-            _ => {}
         }
     }
 
